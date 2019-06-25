0551b114dcfe32a6677ab3960d936ee489656cbe
Patrick Riley <priley86@gmail.com>
2019-06-25 12:30:46 -0400

    feat(rhelGraphCard): issues/5 add StackChart UI components and responsive CSS (#13)

--- src/components/rhelView/rhelView.js
+++ src/components/rhelView/rhelView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
-import { Button, PageSection } from '@patternfly/react-core';
+import { PageSection } from '@patternfly/react-core';
 import { connectRouter } from '../../redux';
 import RhelGraphCard from '../rhelGraphCard/rhelGraphCard';
 
@@ -15,13 +15,6 @@ class RhelView extends React.Component {
         </PageHeader>
         <PageSection>
           <RhelGraphCard />
-          <p>Lorem ipsum dolor sit...</p>
-          <p>
-            <Button variant="primary"> PF-Next Primary Button </Button>
-            <Button variant="secondary"> PF-Next Secondary Button </Button>
-            <Button variant="tertiary"> PF-Next Tertiary Button </Button>
-            <Button variant="danger"> PF-Next Danger Button </Button>
-          </p>
         </PageSection>
       </React.Fragment>
     );
