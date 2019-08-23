b2368dcc85142395b21f176c62b6165cc3af67bc
CD Cabrera <ccabrera@redhat.com>
2019-08-23 18:55:50 -0400

    chore(build): npm updates

--- src/services/config.js
+++ src/services/config.js
@@ -1,10 +1,7 @@
-const serviceConfig = (passedConfig = {}) =>
-  Object.assign(
-    {
-      headers: {},
-      timeout: process.env.REACT_APP_AJAX_TIMEOUT
-    },
-    passedConfig
-  );
+const serviceConfig = (passedConfig = {}) => ({
+  headers: {},
+  timeout: process.env.REACT_APP_AJAX_TIMEOUT,
+  ...passedConfig
+});
 
 export { serviceConfig as default, serviceConfig };
