7922276df809bc8477c22a97dc235799e0f964c1
CD Cabrera <ccabrera@redhat.com>
2019-09-10 16:02:52 -0400

    fix(build): issues/97 branching and integration
    * expanded ci build stages
    * leverage stage names to determine beta path deployment

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 #
 #
+# CI/Dev beta release for "ci-beta" and "qa-beta" branches, based on deploy stage name
+#
+releaseDev()
+{
+  if [[ "${TRAVIS_BRANCH}" = "ci" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
+    printf "${YELLOW}PUSHING ci-beta${NOCOLOR}\n"
+    rm -rf ./build/.git
+    .travis/release.sh "ci-beta"
+    printf "${GREEN}COMPLETED ci-beta${NOCOLOR}\n"
+
+    printf "${YELLOW}PUSHING qa-beta${NOCOLOR}\n"
+    rm -rf ./build/.git
+    .travis/release.sh "qa-beta"
+    printf "${GREEN}COMPLETED qa-beta${NOCOLOR}\n"
+  fi
+}
+#
+#
+# Prod release for "master" branch based on deploy stage name.
+#
+releaseProd()
+{
+  if [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
+    printf "${YELLOW}PUSHING prod-beta${NOCOLOR}\n"
+    rm -rf ./build/.git
+    .travis/release.sh "prod-beta"
+    printf "${GREEN}COMPLETED prod-beta${NOCOLOR}\n"
+  fi
+
+  if [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]];  then
+    printf "${YELLOW}PUSHING prod-stable${NOCOLOR}\n"
+    rm -rf ./build/.git
+    .travis/release.sh "prod-stable"
+    printf "${GREEN}COMPLETED prod-stable${NOCOLOR}\n"
+  fi
+}
+#
+#
 # main()
 #
 {
   set -e
   set -x
 
-  if [ "${TRAVIS_BRANCH}" = "ci" ]; then
-    echo "PUSHING ci-beta"
-    rm -rf ./build/.git
-    .travis/release.sh "ci-beta"
+  BLUE="\e[34m"
+  RED="\e[31m"
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  NOCOLOR="\e[39m"
 
-    echo "PUSHING qa-beta"
-    rm -rf ./build/.git
-    .travis/release.sh "qa-beta"
-  fi
-
-  if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    echo "PUSHING prod-beta"
-    rm -rf ./build/.git
-    .travis/release.sh "prod-beta"
-
-    echo "PUSHING prod-stable"
-    rm -rf ./build/.git
-    .travis/release.sh "prod-stable"
-  fi
+  releaseDev
+  releaseProd
 }
--- scripts/pre.sh
+++ scripts/pre.sh
@@ -6,17 +6,18 @@
 deployPaths()
 {
   local CI_BRANCH=$1
+  local CI_BUILD_STAGE=$2
 
   DEPLOY_PATH_PREFIX=""
 
-  if [[ $CI_BRANCH == *"beta"* ]] || [[ $CI_BRANCH == *"master"* ]]; then
+  if [[ $CI_BUILD_STAGE == *"Beta"* ]]; then
     DEPLOY_PATH_PREFIX=/beta
   fi
 
   echo UI_DEPLOY_PATH_PREFIX="$DEPLOY_PATH_PREFIX" >> ./.env.production.local
 
-  echo "Deploy config for branch \"${CI_BRANCH}\"..."
-  echo "Deploy path prefix ... UI_DEPLOY_PATH_PREFIX=$DEPLOY_PATH_PREFIX"
+  echo "\"${CI_BUILD_STAGE}\" build stage config for branch \"${CI_BRANCH}\"..."
+  printf "Deploy path prefix ... ${GREEN}UI_DEPLOY_PATH_PREFIX=$DEPLOY_PATH_PREFIX${NOCOLOR}\n"
 }
 #
 #
@@ -25,7 +26,7 @@ deployPaths()
 version()
 {
   UI_VERSION="$(node -p 'require(`./package.json`).version').$(git rev-parse --short HEAD)"
-  echo "Version... UI_VERSION=$UI_VERSION"
+  printf "Version... ${GREEN}UI_VERSION=$UI_VERSION${NOCOLOR}\n"
   echo UI_VERSION="$UI_VERSION" >> ./.env.production.local
 }
 #
@@ -44,9 +45,15 @@ clean()
 # main()
 #
 {
+  BLUE="\e[34m"
+  RED="\e[31m"
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  NOCOLOR="\e[39m"
+
   clean
   version
 
   # see .travis.yml globals
-  deployPaths ${BRANCH:-local}
+  deployPaths "${BRANCH:-local}" "${BUILD_STAGE:-Local Deploy}"
 }
