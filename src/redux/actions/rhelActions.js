80f6ef5148ad3d1d339d53fd16841835d9d82e86
CD Cabrera <ccabrera@redhat.com>
2019-10-02 17:15:19 -0400

    feat(notifications): issues/11 toast notifications (#107)
    * build, platform notifications package
    * index, add notifications component
    * rhelActions, notifications metadata
    * middleware, notifications options-settings
    * redux reducers, notifications reducer
    * scss, notifications stylesheet

--- src/redux/actions/rhelActions.js
+++ src/redux/actions/rhelActions.js
@@ -4,7 +4,16 @@ import rhelServices from '../../services/rhelServices';
 const getGraphReports = (query = {}) => dispatch =>
   dispatch({
     type: rhelTypes.GET_GRAPH_REPORT,
-    payload: rhelServices.getGraphReportsRhsm(query)
+    payload: rhelServices.getGraphReportsRhsm(query),
+    meta: {
+      notifications: {
+        rejected: {
+          variant: 'info',
+          title: 'RHSM connection has failed',
+          description: 'Product ID Red Hat Enterprise Linux'
+        }
+      }
+    }
   });
 
 export { getGraphReports as default, getGraphReports };
