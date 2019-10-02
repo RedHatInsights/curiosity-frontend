8c9b3de679d16d526a919d22cfe430b1492ff5f5
CD Cabrera <ccabrera@redhat.com>
2019-10-02 17:15:19 -0400

    feat(notifications): issues/11 toast notifications (#107)
    * build, platform notifications package
    * index, add notifications component
    * rhelActions, notifications metadata
    * middleware, notifications options-settings
    * redux reducers, notifications reducer
    * scss, notifications stylesheet

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { notifications } from '@redhat-cloud-services/frontend-components-notifications';
 import rhelGraphReducer from './rhelGraphReducer';
 import rhelViewReducer from './rhelViewReducer';
 import userReducer from './userReducer';
 
 const reducers = {
+  notifications,
   rhelGraph: rhelGraphReducer,
   rhelView: rhelViewReducer,
   user: userReducer
