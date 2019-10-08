f1ec4f3edb64a4ac1a478d6bd5c54fbceecca08d
CD Cabrera <ccabrera@redhat.com>
2019-10-08 17:44:22 -0400

    fix(i18n): combine i18n integration and unit test
    * combined i18n tests to allow for easier snapshot updates

--- src/components/i18n/__tests__/i18n.test.js
+++ src/components/i18n/__tests__/i18n.test.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import _get from 'lodash/get';
+import { GettextExtractor, JsExtractors } from 'gettext-extractor';
 import { I18n, translate } from '../i18n';
+import enLocales from '../../../../public/locales/en-US';
+
+const textExtractor = () => {
+  const extractor = new GettextExtractor();
+  extractor
+    .createJsParser([
+      JsExtractors.callExpression(['t', '[this].t', 'translate'], {
+        arguments: {
+          text: 0,
+          context: 1
+        }
+      })
+    ])
+    .parseFilesGlob('./src/**/!(*.test|*.spec).@(js|jsx)');
+
+  return extractor;
+};
 
 describe('I18n Component', () => {
+  const getText = textExtractor();
+
   it('should render a basic component', () => {
     const props = {
       locale: 'es'
@@ -35,5 +56,24 @@ describe('I18n Component', () => {
       localeKey,
       placeholder
     }).toMatchSnapshot('translate');
+  });
+
+  it('should generate a predictable pot output snapshot', () => {
+    expect(getText.getPotString()).toMatchSnapshot('pot output');
+  });
+
+  it('should have locale keys that exist in the default language JSON', () => {
+    const messages = (getText.getMessages && getText.getMessages()) || [];
+    const missingKeys = [];
+
+    messages.forEach(value => {
+      const keyCheck = (value && value.text) || '';
+
+      if (keyCheck && !_get(enLocales, keyCheck, null)) {
+        missingKeys.push(keyCheck);
+      }
+    });
+
+    expect(missingKeys).toMatchSnapshot('missing locale keys');
   });
 });
