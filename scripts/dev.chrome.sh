9937f12c3657451d72af3a617dd5dd123569a354
CD Cabrera <ccabrera@redhat.com>
2019-10-10 12:18:58 -0400

    fix(build): issues/105 local run chroming update
    * build, branch checkout to chrome repo clone, expand messaging
    * build, use NPM instead of yarn to build chrome to avoid errors
    * readme requirements updated to include NPM

--- scripts/dev.chrome.sh
+++ scripts/dev.chrome.sh
@@ -8,16 +8,33 @@ gitRepo()
   local GITREPO=$1
   local DIR=$2
   local DIR_REPO=$3
+  local GITBRANCH=$4
   local CURRENT_DATE=$(date "+%s")
   local EXPIRE=$(head -n 1 $DIR/expire.txt)
 
   if [ ! -d $DIR_REPO/build ] || [ "${EXPIRE:-0}" -lt "${CURRENT_DATE}" ]; then
     mkdir -p $DIR
     rm -rf $DIR/temp
-    (cd $DIR && git clone --depth=1 $GITREPO temp > /dev/null 2>&1)
+
+    printf "\n${YELLOW}Accessing ${GITREPO}${NOCOLOR}\n"
+
+    if [ -z "$GITBRANCH" ]; then
+      (cd $DIR && git clone --depth=1 $GITREPO temp > /dev/null 2>&1)
+    else
+      (cd $DIR && git clone $GITREPO temp > /dev/null 2>&1)
+
+      printf "${YELLOW}Attempting to access branch \"${BRANCH}\" ...${NOCOLOR}"
+
+      if [ ! -z "$(cd $DIR/temp && git branch -a | grep $GITBRANCH)" ]; then
+        (cd $DIR/temp && git checkout $GITBRANCH > /dev/null 2>&1)
+        printf "${GREEN}SUCCESS${NOCOLOR}\n"
+      else
+        printf "${YELLOW}non-existant ...IGNORE, use default${NOCOLOR}\n"
+      fi
+    fi
 
     if [ $? -eq 0 ]; then
-      printf "\n${GREEN}Checking ${GITREPO} ...${NOCOLOR}"
+      printf "${YELLOW}Confirm repository ...${NOCOLOR}"
 
       rm -rf $DIR_REPO
       cp -R  $DIR/temp $DIR_REPO
@@ -27,7 +44,7 @@ gitRepo()
 
       echo $(date -v +10d "+%s") > $DIR/expire.txt
 
-      printf "${GREEN}Clone SUCCESS${NOCOLOR}\n"
+      printf "${GREEN}SUCCESS${NOCOLOR}\n"
 
     elif [ -d $DIR_REPO ]; then
       printf "${YELLOW}Unable to connect, using cached ${GITREPO}...${NOCOLOR}\n"
@@ -52,35 +69,42 @@ cleanLocalDotEnv()
 #
 buildChrome()
 {
-  local DIR_REPO=$1
-  local DIR_PUBLIC=$2
-  local SNIPPET_HEAD=$3
-  local SNIPPET_BODY=$4
+  local GITREPO=$1
+  local DIR_REPO=$2
+  local DIR_PUBLIC=$3
+  local SNIPPET_HEAD=$4
+  local SNIPPET_BODY=$5
 
   if [ ! -d $DIR_REPO/build ]; then
     printf "\n${GREEN}Building Chrome...${NOCOLOR}"
 
-    if [ -z "$(node -v)" ] || [ -z "$(yarn -v)" ]; then
-      printf "${YELLOW}Node and Yarn need to be installed in order to local run.${NOCOLOR}\n"
+    if [ -z "$(node -v)" ] || [ -z "$(npm -v)" ]; then
+      printf "${YELLOW}Node and NPM need to be installed in order to local run.${NOCOLOR}\n"
       exit 0
     fi
 
-    (cd $DIR_REPO && yarn && yarn build > /dev/null 2>&1)
+    (cd $DIR_REPO && npm install && npm run build > /dev/null 2>&1)
     printf "\n${GREEN}Build SUCCESS${NOCOLOR}"
   fi
 
-  printf "\n${GREEN}Setting up local chrome... ${NOCOLOR}"
+  printf "\n${YELLOW}Setting up local chrome ...${NOCOLOR}"
 
   mkdir -p $DIR_PUBLIC
   cp -R $DIR_REPO/build/ $DIR_PUBLIC
 
-  printf "${GREEN}dotenv includes... ${NOCOLOR}"
+  printf "${YELLOW}dotenv includes ...${NOCOLOR}"
 
   HEADER_CONTENT=$(node -pe "require('fs').readFileSync('${SNIPPET_HEAD}').toString().replace(/\n/g, '').concat('<style>.pf-c-page__sidebar .ins-c-skeleton{display:none}</style>')")
   BODY_CONTENT=$(node -pe "require('fs').readFileSync('${SNIPPET_BODY}').toString().replace(/\n/g,'').replace(/Logging in\.\.\./i, 'Development')")
 
-  echo "\nREACT_APP_INCLUDE_CONTENT_HEADER=${HEADER_CONTENT}\nREACT_APP_INCLUDE_CONTENT_BODY=${BODY_CONTENT}\n" > ./.env.development.local
-  printf "${GREEN}SUCCESS${NOCOLOR}\n"
+  if [[ ! -z "$HEADER_CONTENT" ]] && [[ ! -z "$BODY_CONTENT" ]]; then
+    echo "\nREACT_APP_INCLUDE_CONTENT_HEADER=${HEADER_CONTENT}\nREACT_APP_INCLUDE_CONTENT_BODY=${BODY_CONTENT}\n" > ./.env.development.local
+    printf "${GREEN}SUCCESS${NOCOLOR}\n"
+  else
+    printf "\n${RED}ERROR, include content doesn't exist${NOCOLOR}\n"
+    printf "${RED}    This is most likely due to an alteration on the includes repository.${NOCOLOR}\n"
+    printf "${RED}    You'll need to confirm the repository can still be accessed\n    ${GITREPO}${NOCOLOR}\n\n"
+  fi
 }
 #
 #
@@ -96,11 +120,24 @@ buildChrome()
   REPO="https://github.com/RedHatInsights/insights-chrome.git"
   DATADIR=./.chrome
   DATADIR_REPO=./.chrome/insights-chrome
+  BRANCH="prod-stable"
   PUBLICDIR=./public/apps/chrome
   HEAD=./public/apps/chrome/snippets/head.html
   BODY=./public/apps/chrome/snippets/body.html
 
-  gitRepo $REPO $DATADIR $DATADIR_REPO
+  while getopts u option;
+    do
+      case $option in
+        u ) UPDATE=true;;
+      esac
+  done
+
+  if [ "$UPDATE" = true ]; then
+    printf "${YELLOW}Updating chrome...${NOCOLOR}\n"
+    rm -rf $DATADIR
+  fi
+
+  gitRepo $REPO $DATADIR $DATADIR_REPO $BRANCH
   cleanLocalDotEnv
-  buildChrome $DATADIR_REPO $PUBLICDIR $HEAD $BODY
+  buildChrome $REPO $DATADIR_REPO $PUBLICDIR $HEAD $BODY
 }
