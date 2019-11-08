f4490377093568f78f8c456a614eef5030639ede
CD Cabrera <ccabrera@redhat.com>
2019-11-08 14:43:11 -0500

    feat(rhelGraphCard): issues/79 activate hypervisor display
    * rhelGraphCard, activate hypervisor display
    * graphHelpers, remove in favor of product specific helpers
    * rhelGraphCardHelpers, expand for tooltips, remove previous label
    * i18n, make locale strings product specific

--- src/components/rhelGraphCard/rhelGraphCardHelpers.js
+++ src/components/rhelGraphCard/rhelGraphCardHelpers.js
@@ -24,37 +24,6 @@ const getChartXAxisLabelIncrement = granularity => {
 };
 
 /**
- * Return translated labels based on granularity.
- *
- * @param {string} granularity, see enum of RHSM_API_QUERY_GRANULARITY_TYPES
- * @param {string} tooltipLabel
- * @returns {Object}
- */
-const getGraphLabels = ({ granularity, tooltipLabel }) => {
-  const labels = {
-    label: tooltipLabel
-  };
-
-  switch (granularity) {
-    case GRANULARITY_TYPES.WEEKLY:
-      labels.previousLabel = translate('curiosity-graph.tooltipPreviousLabelWeekly');
-      break;
-    case GRANULARITY_TYPES.MONTHLY:
-      labels.previousLabel = translate('curiosity-graph.tooltipPreviousLabelMonthly');
-      break;
-    case GRANULARITY_TYPES.QUARTERLY:
-      labels.previousLabel = translate('curiosity-graph.tooltipPreviousLabelQuarterly');
-      break;
-    case GRANULARITY_TYPES.DAILY:
-    default:
-      labels.previousLabel = translate('curiosity-graph.tooltipPreviousLabelDaily');
-      break;
-  }
-
-  return labels;
-};
-
-/**
  * Return a formatted date string.
  *
  * @param {Date} date
@@ -67,10 +36,10 @@ const getTooltipDate = ({ date, granularity }) => {
 
   switch (granularity) {
     case GRANULARITY_TYPES.QUARTERLY:
-      formattedDateTooltip = dateHelpers.timestampQuarterFormats.long;
+      formattedDateTooltip = dateHelpers.timestampQuarterFormats.yearLong;
       break;
     case GRANULARITY_TYPES.MONTHLY:
-      formattedDateTooltip = dateHelpers.timestampMonthFormats.long;
+      formattedDateTooltip = dateHelpers.timestampMonthFormats.yearLong;
       break;
     default:
       formattedDateTooltip = dateHelpers.timestampDayFormats.long;
@@ -78,6 +47,40 @@ const getTooltipDate = ({ date, granularity }) => {
   }
 
   return momentDate.format(formattedDateTooltip);
+};
+
+/**
+ * Get tooltips for x axis while displaying y axis values.
+ *
+ * @param {Object} itemsByKey
+ * @param {string} granularity, see enum of RHSM_API_QUERY_GRANULARITY_TYPES
+ * @returns {string | *}
+ */
+/**
+ * ToDo: we have access to the datasets and index which gives us access to the previous date.
+ * Consider adding back in the year on tooltip cross year displays
+ */
+const getTooltips = ({ itemsByKey, granularity }) => {
+  let hypervisor = itemsByKey.hypervisor && itemsByKey.hypervisor.y;
+  let sockets = itemsByKey.sockets && itemsByKey.sockets.y;
+  let threshold = itemsByKey.threshold && itemsByKey.threshold.y;
+
+  hypervisor = (hypervisor && `${translate('curiosity-graph.rhelTooltipHypervisor')}: ${hypervisor}`) || '';
+  sockets = (sockets && `${translate('curiosity-graph.rhelTooltipSockets')}: ${sockets}`) || '';
+  threshold = (threshold && `${translate('curiosity-graph.rhelTooltipThreshold')}: ${threshold}`) || '';
+
+  const date =
+    ((hypervisor || sockets || threshold) &&
+      `${translate('curiosity-graph.rhelTooltipDate')}: ${getTooltipDate({
+        date: itemsByKey.sockets.date,
+        granularity
+      })}`) ||
+    '';
+
+  return (
+    `${threshold}\n${sockets}${(sockets && '\n') || ''}${hypervisor}${(hypervisor && '\n') || ''}${date}`.trim() ||
+    translate('curiosity-graph.tooltipNoData')
+  );
 };
 
 /**
@@ -117,6 +120,8 @@ const xAxisTickFormat = ({ date, granularity, tick, previousDate }) => {
       formattedDate = isNewYear
         ? momentDate.format(dateHelpers.timestampDayFormats.yearShort)
         : momentDate.format(dateHelpers.timestampDayFormats.short);
+
+      formattedDate = formattedDate.replace(/\s(\d{4})$/, '\n$1');
       break;
   }
 
@@ -129,12 +134,12 @@ const xAxisTickFormat = ({ date, granularity, tick, previousDate }) => {
  * @param {number|string} tick
  * @returns {string}
  */
-const yAxisTickFormat = tick => numbro(tick).format({ average: true, mantissa: 1, optionalMantissa: true });
+const yAxisTickFormat = ({ tick }) => numbro(tick).format({ average: true, mantissa: 1, optionalMantissa: true });
 
 const rhelGraphCardHelpers = {
   getChartXAxisLabelIncrement,
-  getGraphLabels,
   getTooltipDate,
+  getTooltips,
   xAxisTickFormat,
   yAxisTickFormat
 };
@@ -143,8 +148,8 @@ export {
   rhelGraphCardHelpers as default,
   rhelGraphCardHelpers,
   getChartXAxisLabelIncrement,
-  getGraphLabels,
   getTooltipDate,
+  getTooltips,
   xAxisTickFormat,
   yAxisTickFormat
 };
