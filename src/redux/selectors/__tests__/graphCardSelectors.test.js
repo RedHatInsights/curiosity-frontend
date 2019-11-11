1d4149c45fdff58adc7eb8feb11c8aec0ef5ebf9
CD Cabrera <ccabrera@redhat.com>
2019-11-11 17:04:52 -0500

    fix(graphCardSelectors): confirm granularity loaded correctly (#127)
    * additional check to avoid loading prior cache
    * tests, expand and update selector

--- src/redux/selectors/__tests__/graphCardSelectors.test.js
+++ src/redux/selectors/__tests__/graphCardSelectors.test.js
@@ -1,14 +1,21 @@
 import graphCardSelectors from '../graphCardSelectors';
 import { rhelApiTypes } from '../../../types/rhelApiTypes';
+import { dateHelpers } from '../../../common/dateHelpers';
 
 describe('GraphCardSelectors', () => {
   it('should return specific selectors', () => {
     expect(graphCardSelectors).toMatchSnapshot('selectors');
   });
 
-  it('Should error on a RHEL product ID without granularity provided', () => {
+  it('should error on missing a reducer response', () => {
+    const state = {};
+    expect(graphCardSelectors.rhelGraphCard(state)).toMatchSnapshot('rhelGraphCard: missing reducer error');
+  });
+
+  it('should error on a RHEL product ID without granularity provided', () => {
     const state = {
       rhelGraph: {
+        component: {},
         capacity: {
           fulfilled: true,
           metaQuery: {},
@@ -25,9 +32,10 @@ describe('GraphCardSelectors', () => {
     expect(graphCardSelectors.rhelGraphCard(state)).toMatchSnapshot('rhelGraphCard: no granularity error');
   });
 
-  it('Should handle pending state on a RHEL product ID', () => {
+  it('should handle pending state on a RHEL product ID', () => {
     const state = {
       rhelGraph: {
+        component: {},
         capacity: {
           fulfilled: true,
           metaQuery: {
@@ -48,13 +56,75 @@ describe('GraphCardSelectors', () => {
     expect(graphCardSelectors.rhelGraphCard(state)).toMatchSnapshot('rhelGraphCard: pending');
   });
 
-  it('Should pass data through on a RHEL product ID when granularity provided mismatches between aggregated responses', () => {
+  it('should pass data through on a RHEL product ID when granularity provided mismatches between aggregated responses', () => {
     const state = {
       rhelGraph: {
+        component: {},
         capacity: {
           fulfilled: true,
           metaQuery: {
             [rhelApiTypes.RHSM_API_QUERY_GRANULARITY]: rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.MONTHLY
+          },
+          data: {
+            [rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA]: [
+              {
+                [rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA_TYPES.DATE]: '2019-09-04T00:00:00.000Z',
+                [rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA_TYPES.SOCKETS]: 100,
+                [rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA_TYPES.HYPERVISOR_SOCKETS]: 50,
+                [rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA_TYPES.PHYSICAL_SOCKETS]: 50
+              }
+            ]
+          }
+        },
+        report: {
+          fulfilled: true,
+          metaQuery: {
+            [rhelApiTypes.RHSM_API_QUERY_GRANULARITY]: rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY
+          },
+          data: {
+            [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA]: [
+              {
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.DATE]: '2019-09-04T00:00:00.000Z',
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.SOCKETS]: 2,
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.HYPERVISOR_SOCKETS]: 1,
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.PHYSICAL_SOCKETS]: 1
+              }
+            ]
+          }
+        }
+      }
+    };
+
+    expect(graphCardSelectors.rhelGraphCard(state)).toMatchSnapshot('rhelGraphCard: granularity mismatch fulfilled');
+
+    expect(
+      graphCardSelectors.rhelGraphCard({
+        rhelGraph: {
+          capacity: {
+            ...state.rhelGraph.capacity,
+            metaQuery: {
+              [rhelApiTypes.RHSM_API_QUERY_GRANULARITY]: rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY
+            }
+          },
+          report: {
+            ...state.rhelGraph.report
+          }
+        }
+      })
+    ).toMatchSnapshot('rhelGraphCard: granularity mismatch on component');
+  });
+
+  it('should populate data on a RHEL product ID when api response provided mismatches index or date', () => {
+    const state = {
+      rhelGraph: {
+        component: {
+          graphGranularity: rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY,
+          ...dateHelpers.getRangedDateTime(rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY)
+        },
+        capacity: {
+          fulfilled: true,
+          metaQuery: {
+            [rhelApiTypes.RHSM_API_QUERY_GRANULARITY]: rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY
           },
           data: { [rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA]: [] }
         },
@@ -63,17 +133,32 @@ describe('GraphCardSelectors', () => {
           metaQuery: {
             [rhelApiTypes.RHSM_API_QUERY_GRANULARITY]: rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY
           },
-          data: { [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA]: [] }
+          data: {
+            [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA]: [
+              {
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.DATE]: '2019-09-04T00:00:00.000Z',
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.SOCKETS]: 2,
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.HYPERVISOR_SOCKETS]: 1,
+                [rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.PHYSICAL_SOCKETS]: 1
+              }
+            ]
+          }
         }
       }
     };
 
-    expect(graphCardSelectors.rhelGraphCard(state)).toMatchSnapshot('rhelGraphCard: granularity mismatch fulfilled');
+    expect(graphCardSelectors.rhelGraphCard(state)).toMatchSnapshot(
+      'rhelGraphCard: data populated on mismatch fulfilled'
+    );
   });
 
-  it('Should map a fulfilled RHEL product ID response to an aggregated output', () => {
+  it('should map a fulfilled RHEL product ID response to an aggregated output', () => {
     const state = {
       rhelGraph: {
+        component: {
+          graphGranularity: rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY,
+          ...dateHelpers.getRangedDateTime(rhelApiTypes.RHSM_API_QUERY_GRANULARITY_TYPES.DAILY)
+        },
         capacity: {
           fulfilled: true,
           metaQuery: {
