320e5716f098ef881192f07c65c736eda2ce4d14
CD Cabrera <ccabrera@redhat.com>
2019-11-15 02:04:55 -0500

    feat(rhelView,authentication): issues/77 pass path parameter
    * rhelView, path parameter as a product id
    * authentication, expand routing, update for build navigation

--- src/components/rhelView/__tests__/rhelView.test.js
+++ src/components/rhelView/__tests__/rhelView.test.js
@@ -4,10 +4,13 @@ import { RhelView } from '../rhelView';
 
 describe('RhelView Component', () => {
   it('should render a non-connected component', () => {
-    const props = {};
+    const props = {
+      routeDetail: {
+        pathParameter: 'lorem ipsum'
+      }
+    };
 
     const component = shallow(<RhelView {...props} />);
-
     expect(component).toMatchSnapshot('non-connected');
   });
 });
