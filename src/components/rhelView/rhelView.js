fad128bfb24e47d16f145b5b29b383081f086688
CD Cabrera <ccabrera@redhat.com>
2019-11-15 02:04:55 -0500

    feat(rhelView,authentication): issues/77 pass path parameter
    * rhelView, path parameter as a product id
    * authentication, expand routing, update for build navigation

--- src/components/rhelView/rhelView.js
+++ src/components/rhelView/rhelView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
 import { PageSection } from '@patternfly/react-core';
 import { connectRouter } from '../../redux';
@@ -8,20 +9,26 @@ class RhelView extends React.Component {
   componentDidMount() {}
 
   render() {
+    const { routeDetail } = this.props;
+
     return (
       <React.Fragment>
         <PageHeader>
           <PageHeaderTitle title="Red Hat Enterprise Linux" />
         </PageHeader>
         <PageSection>
-          <RhelGraphCard />
+          <RhelGraphCard productId={routeDetail.pathParameter} />
         </PageSection>
       </React.Fragment>
     );
   }
 }
 
-RhelView.propTypes = {};
+RhelView.propTypes = {
+  routeDetail: PropTypes.shape({
+    pathParameter: PropTypes.string.isRequired
+  }).isRequired
+};
 
 RhelView.defaultProps = {};
 
@@ -29,6 +36,7 @@ const mapStateToProps = () => ({});
 
 const mapDispatchToProps = () => ({});
 
+// Todo: clean up "connected" component appears unnecessary, look at converting from class
 const ConnectedRhelView = connectRouter(mapStateToProps, mapDispatchToProps)(RhelView);
 
 export { ConnectedRhelView as default, ConnectedRhelView, RhelView };
