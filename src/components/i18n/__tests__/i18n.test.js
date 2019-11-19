c7f92d28ed1623cd89474c26c3b2d6d2bee4c894
CD Cabrera <ccabrera@redhat.com>
2019-11-19 03:00:12 -0500

    fix(i18n): translateComponent wrapper, alias

--- src/components/i18n/__tests__/i18n.test.js
+++ src/components/i18n/__tests__/i18n.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { mount, shallow } from 'enzyme';
 import _get from 'lodash/get';
 import { GettextExtractor, JsExtractors } from 'gettext-extractor';
-import { I18n, translate } from '../i18n';
+import { I18n, translate, translateComponent } from '../i18n';
+import { helpers } from '../../../common';
 import enLocales from '../../../../public/locales/en-US';
 
 const textExtractor = () => {
@@ -46,6 +48,23 @@ describe('I18n Component', () => {
     );
 
     expect(component.html()).toMatchSnapshot('children');
+  });
+
+  it('should attempt to perform a component translate', () => {
+    const ExampleComponent = ({ t }) => <div>{t('lorem.ipsum', 'hello world')}</div>;
+
+    ExampleComponent.propTypes = {
+      t: PropTypes.func
+    };
+
+    ExampleComponent.defaultProps = {
+      t: helpers.noopTranslate
+    };
+
+    const TranslatedComponent = translateComponent(ExampleComponent);
+    const component = shallow(<TranslatedComponent />);
+
+    expect(component.html()).toMatchSnapshot('translated component');
   });
 
   it('should attempt to perform a string replace', () => {
