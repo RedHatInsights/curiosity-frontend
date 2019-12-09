1f2899ded15af398a4d23e68c446671e24172334
CD Cabrera <ccabrera@redhat.com>
2019-12-09 14:18:11 -0500

    chore(build): https for local proxy dotenv

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -3,19 +3,19 @@ const localhost = (process.env.PLATFORM === 'linux' && 'localhost') || 'host.doc
 module.exports = {
   routes: {
     '/locales': {
-      host: `http://${localhost}:5001`
+      host: `https://${localhost}:5001`
     },
     '/static': {
-      host: `http://${localhost}:5001`
+      host: `https://${localhost}:5001`
     },
     '/beta/apps/subscriptions': {
-      host: `http://${localhost}:5001`
+      host: `https://${localhost}:5001`
     },
     '/beta/staging/subscriptions': {
-      host: `http://${localhost}:5001`
+      host: `https://${localhost}:5001`
     },
     '/beta/subscriptions': {
-      host: `http://${localhost}:5001`
+      host: `https://${localhost}:5001`
     },
     '/api/rhsm-subscriptions': {
       host: 'https://ci.cloud.redhat.com'
