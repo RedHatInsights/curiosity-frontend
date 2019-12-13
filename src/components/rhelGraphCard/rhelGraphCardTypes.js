cced23187b8043daf2ddb0b18d8ee747b69ca8ab
CD Cabrera <ccabrera@redhat.com>
2019-12-13 14:20:05 -0500

    fix(locale): clean up default locale strings
    * locale, remove string overlap
    * helpers, adjust noopTranslate
    * rhelGraphCard, rhelGraphCardHelpers, string labels updated
    * rhelGraphCardTypes, clean old defaults, granularity labels

--- src/components/rhelGraphCard/rhelGraphCardTypes.js
+++ src/components/rhelGraphCard/rhelGraphCardTypes.js
@@ -2,10 +2,10 @@ import { translate } from '../i18n/i18n';
 import { RHSM_API_QUERY_GRANULARITY_TYPES as GRANULARITY_TYPES } from '../../types/rhsmApiTypes';
 
 const getDateMenuOptionsType = () => [
-  { title: translate('curiosity-graph.dropdownDaily', 'Daily'), value: GRANULARITY_TYPES.DAILY },
-  { title: translate('curiosity-graph.dropdownWeekly', 'Weekly'), value: GRANULARITY_TYPES.WEEKLY },
-  { title: translate('curiosity-graph.dropdownMonthly', 'Monthly'), value: GRANULARITY_TYPES.MONTHLY },
-  { title: translate('curiosity-graph.dropdownQuarterly', 'Quarterly'), value: GRANULARITY_TYPES.QUARTERLY }
+  { title: translate('curiosity-graph.dropdownDaily'), value: GRANULARITY_TYPES.DAILY },
+  { title: translate('curiosity-graph.dropdownWeekly'), value: GRANULARITY_TYPES.WEEKLY },
+  { title: translate('curiosity-graph.dropdownMonthly'), value: GRANULARITY_TYPES.MONTHLY },
+  { title: translate('curiosity-graph.dropdownQuarterly'), value: GRANULARITY_TYPES.QUARTERLY }
 ];
 
 const rhelGraphCardTypes = {
