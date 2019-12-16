a2ceeef54ea11048f933956c8251cfb2e85c75b0
CD Cabrera <ccabrera@redhat.com>
2019-12-15 22:20:48 -0500

    feat(openShiftView,rhelView): issues/157 openshift route (#164)
    * routerTypes, apply openshift view, update route detail filter
    * router, expand passed values to getRouteDetail
    * rhelView, apply translation for titles
    * openshiftView, translation, mirror rhelView, filter core values
    * graphCard, prop for cardTitle
    * i18n, locale, heading, title strings

---
+++ src/components/openshiftView/__tests__/opensiftView.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { OpenshiftView } from '../openshiftView';
+
+describe('OpenshiftView Component', () => {
+  it('should render a non-connected component', () => {
+    const props = {
+      routeDetail: {
+        pathParameter: 'lorem ipsum',
+        routeItem: {
+          title: 'Dolor sit'
+        }
+      }
+    };
+
+    const component = shallow(<OpenshiftView {...props} />);
+    expect(component).toMatchSnapshot('non-connected');
+  });
+
+  it('should have a fallback title', () => {
+    const props = {
+      routeDetail: {
+        pathParameter: 'lorem ipsum'
+      }
+    };
+
+    const component = shallow(<OpenshiftView {...props} />);
+    expect(component).toMatchSnapshot('title');
+  });
+});
