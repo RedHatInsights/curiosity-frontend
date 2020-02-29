a298a57cbd02a7559ca214214262853244bd3aa0
CD Cabrera <ccabrera@redhat.com>
2020-02-29 03:52:46 -0500

    fix(graphReducer,rhsmActions): clean up unused actions
    * graphReducer, remove unused action types
    * rhsmActions, remove unused actions

--- src/redux/actions/rhsmActions.js
+++ src/redux/actions/rhsmActions.js
@@ -12,28 +12,6 @@ const getGraphReportsCapacity = (id = null, query = {}) => dispatch =>
     }
   });
 
-const getGraphReports = (id = null, query = {}) => dispatch =>
-  dispatch({
-    type: rhsmTypes.GET_GRAPH_REPORT_RHSM,
-    payload: rhsmServices.getGraphReports(id, query),
-    meta: {
-      id,
-      query,
-      notifications: {}
-    }
-  });
+const rhsmActions = { getGraphReportsCapacity };
 
-const getGraphCapacity = (id = null, query = {}) => dispatch =>
-  dispatch({
-    type: rhsmTypes.GET_GRAPH_CAPACITY_RHSM,
-    payload: rhsmServices.getGraphCapacity(id, query),
-    meta: {
-      id,
-      query,
-      notifications: {}
-    }
-  });
-
-const rhsmActions = { getGraphReportsCapacity, getGraphCapacity, getGraphReports };
-
-export { rhsmActions as default, rhsmActions, getGraphReportsCapacity, getGraphCapacity, getGraphReports };
+export { rhsmActions as default, rhsmActions, getGraphReportsCapacity };
