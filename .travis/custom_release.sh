c9f76bea9718f0fabb6c0ab99509aee8f9917311
CD Cabrera <ccabrera@redhat.com>
2020-03-12 12:05:20 -0400

    chore(build): issues/218 apply stable deploy paths (#222)
    * pre.sh, minor local variable rename to avoid confusion
    * proxy run, stable path additions for spandx config, local run
    * contributing.md, branch and pr workflow updated
    * custom release, beta and stable branches for ci, qa, prod

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 #
 #
-# CI/Dev beta release for "ci-beta" and "qa-beta" branches, based on deploy stage name
+# Release a branch
+release()
+{
+  local DEPLOY_BRANCH=$1
+
+  printf "${YELLOW}PUSHING ${DEPLOY_BRANCH}${NOCOLOR}\n"
+  rm -rf ./build/.git
+  .travis/release.sh "${DEPLOY_BRANCH}"
+  printf "${GREEN}COMPLETED ${DEPLOY_BRANCH}${NOCOLOR}\n"
+}
+#
+#
+# CI/Dev beta release for "ci" and "qa" "beta" and "stable" branches, based on deploy stage name
 #
 releaseDev()
 {
-  if [[ "${TRAVIS_BRANCH}" = "ci" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
-    printf "${YELLOW}PUSHING ci-beta${NOCOLOR}\n"
-    rm -rf ./build/.git
-    .travis/release.sh "ci-beta"
-    printf "${GREEN}COMPLETED ci-beta${NOCOLOR}\n"
+  if [[ "${TRAVIS_BRANCH}" = "ci-beta" || "${TRAVIS_BRANCH}" = "dev" || "${TRAVIS_BRANCH}" = "ci" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
+    release "ci-beta"
+    release "qa-beta"
+  fi
 
-    printf "${YELLOW}PUSHING qa-beta${NOCOLOR}\n"
-    rm -rf ./build/.git
-    .travis/release.sh "qa-beta"
-    printf "${GREEN}COMPLETED qa-beta${NOCOLOR}\n"
+  if [[ "${TRAVIS_BRANCH}" = "ci-stable" || "${TRAVIS_BRANCH}" = "test" || "${TRAVIS_BRANCH}" = "qa" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]];  then
+    release "ci-stable"
+    release "qa-stable"
   fi
 }
 #
 #
-# Prod release for "master" branch based on deploy stage name.
+# Prod release for "master" and "stage" branches based on deploy stage name.
 #
 releaseProd()
 {
-  if [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
-    printf "${YELLOW}PUSHING prod-beta${NOCOLOR}\n"
-    rm -rf ./build/.git
-    .travis/release.sh "prod-beta"
-    printf "${GREEN}COMPLETED prod-beta${NOCOLOR}\n"
+  if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "stage" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
+    release "prod-beta"
   fi
 
-  if [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]];  then
-    printf "${YELLOW}PUSHING prod-stable${NOCOLOR}\n"
-    rm -rf ./build/.git
-    .travis/release.sh "prod-stable"
-    printf "${GREEN}COMPLETED prod-stable${NOCOLOR}\n"
+  if [[ "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]];  then
+    release "prod-stable"
   fi
 }
 #
