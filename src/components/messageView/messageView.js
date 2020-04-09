27b183877648981d59e51be87d43b10e80006647
CD Cabrera <ccabrera@redhat.com>
2020-04-09 17:24:48 -0400

    fix(authentication,messageView): auth empty state
    * authentication, update auth empty state
    * messageView, add pageTitle prop

--- src/components/messageView/messageView.js
+++ src/components/messageView/messageView.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { EmptyState as PfEmptyState, EmptyStateBody, EmptyStateIcon, EmptyStateVariant } from '@patternfly/react-core';
+import {
+  EmptyState as PfEmptyState,
+  EmptyStateBody,
+  EmptyStateIcon,
+  EmptyStateVariant,
+  Title
+} from '@patternfly/react-core';
 import { PageLayout, PageHeader } from '../pageLayout/pageLayout';
 import { helpers } from '../../common';
 
 /**
- * FixMe: Patternfly EmptyStateIcon PropType is not intuitive
- * Requires the use of a function proptype?!?
+ * FixMe: Patternfly EmptyStateIcon PropType registers as function?
  */
 /**
  * Render a message view.
  *
  * @returns {Node}
  */
-const MessageView = ({ icon, message, title }) => (
+const MessageView = ({ icon, message, pageTitle, title }) => (
   <PageLayout>
-    <PageHeader>{title || helpers.UI_DISPLAY_CONFIG_NAME}</PageHeader>
+    <PageHeader>{pageTitle || helpers.UI_DISPLAY_NAME}</PageHeader>
     <PfEmptyState variant={EmptyStateVariant.full} className="fadein">
+      {title && (
+        <Title headingLevel="h2" size="lg">
+          {title}
+        </Title>
+      )}
       {icon && <EmptyStateIcon icon={icon} />}
       {message && <EmptyStateBody>{message}</EmptyStateBody>}
     </PfEmptyState>
@@ -31,6 +41,7 @@ const MessageView = ({ icon, message, title }) => (
 MessageView.propTypes = {
   icon: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
   message: PropTypes.string,
+  pageTitle: PropTypes.string,
   title: PropTypes.string
 };
 
@@ -42,6 +53,7 @@ MessageView.propTypes = {
 MessageView.defaultProps = {
   icon: null,
   message: null,
+  pageTitle: null,
   title: null
 };
 
