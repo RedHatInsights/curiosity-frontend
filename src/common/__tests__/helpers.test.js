00c33115c946d433feaacefc2f99d6bc8d7a3c4e
CD Cabrera <ccabrera@redhat.com>
2020-04-17 16:48:00 -0400

    fix(helpers): access to an app specific window object
    * browserExpose, GUI functionality, local run browser console

--- src/common/__tests__/helpers.test.js
+++ src/common/__tests__/helpers.test.js
@@ -14,4 +14,12 @@ describe('Helpers', () => {
     expect(helpers.isPromise(Promise.resolve())).toBe(true);
     expect(helpers.isPromise(() => 'lorem')).toBe(false);
   });
+
+  it('should expose a window object', () => {
+    helpers.browserExpose({ lorem: 'ipsum' });
+    expect(window[helpers.UI_WINDOW_ID]).toMatchSnapshot('window object');
+
+    helpers.browserExpose({ dolor: 'sit' }, { limit: true });
+    expect(window[helpers.UI_WINDOW_ID]).toMatchSnapshot('limited window object');
+  });
 });
