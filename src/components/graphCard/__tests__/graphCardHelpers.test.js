4a0fd417027b5c7d2fa52d31a36e40b16d159349
CD Cabrera <ccabrera@redhat.com>
2020-05-29 13:17:39 -0400

    feat(chartArea): issues/283 victory html tooltips (#295)
    * chartArea, expose foreignObject for html tooltips
    * graphCard, apply graphCardChartTooltip
    * graphCardHelpers, move tooltip to graphCardChartTooltip
    * graphCardChartTooltip, initial component
    * i18n, locale strings for tooltips
    * styling, tooltip styling

--- src/components/graphCard/__tests__/graphCardHelpers.test.js
+++ src/components/graphCard/__tests__/graphCardHelpers.test.js
@@ -3,7 +3,6 @@ import {
   graphCardHelpers,
   getChartXAxisLabelIncrement,
   getTooltipDate,
-  getTooltips,
   xAxisTickFormat,
   yAxisTickFormat
 } from '../graphCardHelpers';
@@ -31,117 +30,6 @@ describe('GraphCardHelpers', () => {
     const quarterly = getTooltipDate({ granularity: GRANULARITY_TYPES.QUARTERLY, date: '2019-06-01T00:00:00Z' });
 
     expect({ daily, weekly, monthly, quarterly }).toMatchSnapshot('granularity based date');
-  });
-
-  it('getTooltips should return a formatted tooltip based on data and granularity', () => {
-    const itemsByKey = {};
-
-    const daily = () => getTooltips({ itemsByKey, granularity: GRANULARITY_TYPES.DAILY });
-    const weekly = () => getTooltips({ itemsByKey, granularity: GRANULARITY_TYPES.WEEKLY });
-    const monthly = () => getTooltips({ itemsByKey, granularity: GRANULARITY_TYPES.MONTHLY });
-    const quarterly = () => getTooltips({ itemsByKey, granularity: GRANULARITY_TYPES.QUARTERLY });
-
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'no data granularity data display'
-    );
-
-    // report value > 0
-    itemsByKey.hypervisorSockets = { x: 0, y: 50, date: '2019-06-01T00:00:00Z', hasData: undefined };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display > 0 and UNDEFINED hasData on report'
-    );
-
-    itemsByKey.hypervisorSockets = { x: 0, y: 50, date: '2019-06-01T00:00:00Z', hasData: false };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display > 0 and NOT hasData on report'
-    );
-
-    itemsByKey.hypervisorSockets = { x: 0, y: 50, date: '2019-06-01T00:00:00Z', hasData: true };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display > 0 and hasData on report'
-    );
-
-    // report value = 0
-    itemsByKey.hypervisorSockets = { x: 0, y: 0, date: '2019-06-01T00:00:00Z', hasData: undefined };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display for zero and UNDEFINED hasData on report'
-    );
-
-    itemsByKey.hypervisorSockets = { x: 0, y: 0, date: '2019-06-01T00:00:00Z', hasData: false };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display for zero and NOT hasData on report'
-    );
-
-    itemsByKey.hypervisorSockets = { x: 0, y: 0, date: '2019-06-01T00:00:00Z', hasData: true };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display for zero and hasData on report'
-    );
-
-    // report value = null
-    itemsByKey.hypervisorSockets = { x: 0, y: null, date: '2019-06-01T00:00:00Z', hasData: undefined };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display for null and UNDEFINED hasData on report'
-    );
-
-    itemsByKey.hypervisorSockets = { x: 0, y: null, date: '2019-06-01T00:00:00Z', hasData: false };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display for null and NOT hasData on report'
-    );
-
-    itemsByKey.hypervisorSockets = { x: 0, y: null, date: '2019-06-01T00:00:00Z', hasData: true };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'data display for null and hasData on report'
-    );
-
-    delete itemsByKey.hypervisorSockets;
-
-    // threshold value > 0
-    itemsByKey.threshold = { x: 0, y: 100, date: '2019-06-01T00:00:00Z', hasInfinite: undefined };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for > 0 and UNDEFINED hasInfinite on threshold'
-    );
-
-    itemsByKey.threshold = { x: 0, y: 100, date: '2019-06-01T00:00:00Z', hasInfinite: false };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for > 0 and NOT hasInfinite on threshold'
-    );
-
-    itemsByKey.threshold = { x: 0, y: 100, date: '2019-06-01T00:00:00Z', hasInfinite: true };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for > 0 and hasInfinite on threshold'
-    );
-
-    // threshold value = 0
-    itemsByKey.threshold = { x: 0, y: 0, date: '2019-06-01T00:00:00Z', hasInfinite: undefined };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for zero and UNDEFINED hasInfinite on threshold'
-    );
-
-    itemsByKey.threshold = { x: 0, y: 0, date: '2019-06-01T00:00:00Z', hasInfinite: false };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for zero and NOT hasInfinite on threshold'
-    );
-
-    itemsByKey.threshold = { x: 0, y: 0, date: '2019-06-01T00:00:00Z', hasInfinite: true };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for zero and hasInfinite on threshold'
-    );
-
-    // threshold value = null
-    itemsByKey.threshold = { x: 0, y: null, date: '2019-06-01T00:00:00Z', hasInfinite: undefined };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for null and UNDEFINED hasInfinite on threshold'
-    );
-
-    itemsByKey.threshold = { x: 0, y: null, date: '2019-06-01T00:00:00Z', hasInfinite: false };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for null and NOT hasInfinite on threshold'
-    );
-
-    itemsByKey.threshold = { x: 0, y: null, date: '2019-06-01T00:00:00Z', hasInfinite: true };
-    expect({ daily: daily(), weekly: weekly(), monthly: monthly(), quarterly: quarterly() }).toMatchSnapshot(
-      'threshold display for null and hasInfinite on threshold'
-    );
   });
 
   /**
