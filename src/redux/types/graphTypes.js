d5f73657e65788b5e0baaaf973bb4310f733546f
CD Cabrera <ccabrera@redhat.com>
2020-06-03 12:02:02 -0400

    feat(chartArea,graphCard): issues/283 interactive legend (#299)
    * chartArea, expose chart methods for interactivity, redraw
    * graphCard, pass chart methods to legend
    * graphCardChartLegend, apply redux state, add button click
    * graphReducer, apply legend toggle state
    * reduxTypes, graphTypes, set legend type
    * styling, allow button color

---
+++ src/redux/types/graphTypes.js
@@ -0,0 +1,12 @@
+const SET_GRAPH_LEGEND = 'SET_GRAPH_LEGEND';
+
+/**
+ * Graph action, reducer types.
+ *
+ * @type {{SET_GRAPH_LEGEND: string}}
+ */
+const graphTypes = {
+  SET_GRAPH_LEGEND
+};
+
+export { graphTypes as default, graphTypes, SET_GRAPH_LEGEND };
