c08e9b95e1718c7f06c6c0acb5faff31b79ce9fa
CD Cabrera <ccabrera@redhat.com>
2020-06-29 11:28:14 -0400

    fix(rhsmServices,redux): issues/10 inventory guests, state (#330)
    * build, dotenv host inventory guest API endpoint added
    * redux actions, reducers, types for inventory guests
    * rhsmServices, add base host inventory guests API
    * rhsmApiTypes, common API response types, inventory guests

--- src/redux/actions/rhsmActions.js
+++ src/redux/actions/rhsmActions.js
@@ -19,6 +19,13 @@ const getGraphReportsCapacity = (id = null, query = {}) => dispatch =>
     }
   });
 
+/**
+ * Get a hosts response listing from RHSM subscriptions.
+ *
+ * @param {string} id
+ * @param {object} query
+ * @returns {Function}
+ */
 const getHostsInventory = (id = null, query = {}) => dispatch =>
   dispatch({
     type: rhsmTypes.GET_HOSTS_INVENTORY_RHSM,
@@ -30,6 +37,24 @@ const getHostsInventory = (id = null, query = {}) => dispatch =>
     }
   });
 
-const rhsmActions = { getGraphReportsCapacity, getHostsInventory };
+/**
+ * Get a host's guest response listing from RHSM subscriptions.
+ *
+ * @param {string} id
+ * @param {object} query
+ * @returns {Function}
+ */
+const getHostsInventoryGuests = (id = null, query = {}) => dispatch =>
+  dispatch({
+    type: rhsmTypes.GET_HOSTS_INVENTORY_GUESTS_RHSM,
+    payload: rhsmServices.getHostsInventoryGuests(id, query),
+    meta: {
+      id,
+      query,
+      notifications: {}
+    }
+  });
 
-export { rhsmActions as default, rhsmActions, getGraphReportsCapacity, getHostsInventory };
+const rhsmActions = { getGraphReportsCapacity, getHostsInventory, getHostsInventoryGuests };
+
+export { rhsmActions as default, rhsmActions, getGraphReportsCapacity, getHostsInventory, getHostsInventoryGuests };
