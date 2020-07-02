afc62b24df4657d876e21eb2865ca50db6002f54
CD Cabrera <ccabrera@redhat.com>
2020-07-02 13:11:04 -0400

    fix(routerTypes): issues/328 lazy load routing (#337)
    * build, frontend and notifications packages patched
    * build, integration dist/file output test
    * router, apply react suspense loading
    * routerTypes, react lazy app routes

--- src/components/router/__tests__/routerTypes.test.js
+++ src/components/router/__tests__/routerTypes.test.js
@@ -8,4 +8,20 @@ describe('RouterTypes', () => {
     expect(platformModalRedirect).toMatchSnapshot('platformModalRedirect');
     expect(routes).toMatchSnapshot('routes');
   });
+
+  it('should return a lazy loaded view for every route', () => {
+    const lazyLoadComponents = [];
+
+    routes.forEach(({ component, to }) => {
+      const routeComponent = Object.getOwnPropertyNames(component)
+        .map(prop => (component[prop] || '').toString())
+        .find(val => /react/i.test(val));
+
+      if (/lazy/.test(routeComponent)) {
+        lazyLoadComponents.push({ routeComponentType: routeComponent, route: to });
+      }
+    });
+
+    expect(lazyLoadComponents.length).toBe(4);
+  });
 });
