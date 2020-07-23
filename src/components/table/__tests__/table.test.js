9869c86fd8066e712bbe5569127f09d5be6ee62a
CD Cabrera <ccabrera@redhat.com>
2020-07-23 15:18:52 -0400

    feat(table,empty,skeleton): issues/10 add table component (#346)
    * table, convenience wrapper for pf tables
    * tableEmpty, convenience wrapper for platform comp, empty state
    * tableSkeleton, wrapper for skeleton table, allow variants
    * locales, i18n, table strings
    * styles, correct pf table behavior on small screens, no border fix

---
+++ src/components/table/__tests__/table.test.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TableVariant } from '@patternfly/react-table';
+import { Table } from '../table';
+
+describe('Table Component', () => {
+  it('should render a non-connected component', () => {
+    const props = {
+      columnHeaders: ['lorem', 'ipsum', 'dolor', 'sit']
+    };
+
+    const component = shallow(<Table {...props} />);
+    expect(component).toMatchSnapshot('non-connected');
+  });
+
+  it('should allow variations in table layout', () => {
+    const props = {
+      columnHeaders: ['lorem ipsum'],
+      rows: [{ cells: ['dolor'] }, { cells: ['sit'] }]
+    };
+
+    const component = shallow(<Table {...props} />);
+    expect(component).toMatchSnapshot('generated rows');
+
+    component.setProps({
+      borders: false,
+      isHeader: false
+    });
+    expect(component).toMatchSnapshot('borders and table header removed');
+
+    component.setProps({
+      ariaLabel: 'lorem ipsum aria-label',
+      summary: 'lorem ipsum summary'
+    });
+    expect(component).toMatchSnapshot('ariaLabel and summary');
+
+    component.setProps({
+      className: 'lorem-ipsum-class',
+      variant: TableVariant.compact
+    });
+    expect(component).toMatchSnapshot('className and variant');
+  });
+
+  it('should allow expandable content', () => {
+    const props = {
+      columnHeaders: ['lorem ipsum'],
+      rows: [{ cells: ['dolor'], expandedContent: 'dolor sit expandable content' }, { cells: ['sit'] }]
+    };
+
+    const component = shallow(<Table {...props} />);
+    expect(component).toMatchSnapshot('expandable content');
+
+    const componentInstance = component.instance();
+    componentInstance.onCollapse({ index: 0, isOpen: true });
+    expect(component).toMatchSnapshot('expanded row');
+  });
+
+  it('should pass child components, nodes when there are no rows', () => {
+    const props = {
+      columnHeaders: ['lorem ipsum'],
+      rows: []
+    };
+
+    const component = shallow(<Table {...props}>Loading...</Table>);
+    expect(component).toMatchSnapshot('children');
+  });
+});
