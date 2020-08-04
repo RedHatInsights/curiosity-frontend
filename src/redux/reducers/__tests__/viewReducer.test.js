916312c34be50e8bcbf67fb522456ccf134b9c06
CD Cabrera <ccabrera@redhat.com>
2020-08-04 11:49:27 -0400

    fix(queryTypes): issues/10 rhsm to query type (#361)
    * graphCard, c3graphCard, filter to query name
    * queryTypes, rhsmTypes, filter to query, align naming
    * toolbar, apply query SLA type
    * viewReducer, apply query types

--- src/redux/reducers/__tests__/viewReducer.test.js
+++ src/redux/reducers/__tests__/viewReducer.test.js
@@ -1,5 +1,12 @@
 import viewReducer from '../viewReducer';
-import { rhsmTypes as types } from '../../types';
+import { queryTypes as types } from '../../types';
+import {
+  RHSM_API_QUERY_GRANULARITY,
+  RHSM_API_QUERY_LIMIT,
+  RHSM_API_QUERY_OFFSET,
+  RHSM_API_QUERY_SLA,
+  RHSM_API_QUERY_USAGE
+} from '../../../types/rhsmApiTypes';
 
 describe('ViewReducer', () => {
   it('should return the initial state', () => {
@@ -8,19 +15,22 @@ describe('ViewReducer', () => {
 
   it('should handle specific defined types', () => {
     const specificTypes = [
-      types.SET_FILTER_GRANULARITY_RHSM,
-      types.SET_FILTER_SLA_RHSM,
-      types.SET_FILTER_USAGE_RHSM,
-      types.SET_CLEAR_FILTERS,
-      types.SET_PAGE_LIMIT_RHSM,
-      types.SET_PAGE_OFFSET_RHSM
+      types.SET_QUERY_GRANULARITY_RHSM,
+      types.SET_QUERY_SLA_RHSM,
+      types.SET_QUERY_USAGE_RHSM,
+      types.SET_QUERY_CLEAR,
+      types.SET_QUERY_PAGE_LIMIT_RHSM,
+      types.SET_QUERY_PAGE_OFFSET_RHSM
     ];
 
     specificTypes.forEach(value => {
       const dispatched = {
         type: value,
-        granularity: 'lorem granularity',
-        sla: 'lorem sla',
+        [RHSM_API_QUERY_GRANULARITY]: 'lorem granularity',
+        [RHSM_API_QUERY_SLA]: 'lorem sla',
+        [RHSM_API_QUERY_USAGE]: 'ipsum usage',
+        [RHSM_API_QUERY_LIMIT]: 10,
+        [RHSM_API_QUERY_OFFSET]: 10,
         viewId: 'test_id'
       };
 
