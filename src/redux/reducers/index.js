41377305da56c327b75c130bbed1d9f85e27ed0b
CD Cabrera <ccabrera@redhat.com>
2020-08-04 12:36:44 -0400

    feat(toolbar): issues/10 add usage filter (#361)
    * i18n, locale strings for usage
    * toolbar, toolbarTypes, add usage filter
    * redux, middleware, multiActionMiddleware, add array of actions
    * redux, reducers, types, toolbar addtions
    * styles, add toolbar styles

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,17 +2,27 @@ import { combineReducers } from 'redux';
 import { notifications } from '@redhat-cloud-services/frontend-components-notifications/cjs';
 import graphReducer from './graphReducer';
 import inventoryReducer from './inventoryReducer';
-import viewReducer from './viewReducer';
+import toolbarReducer from './toolbarReducer';
 import userReducer from './userReducer';
+import viewReducer from './viewReducer';
 
 const reducers = {
   notifications,
   graph: graphReducer,
   inventory: inventoryReducer,
+  toolbar: toolbarReducer,
   user: userReducer,
   view: viewReducer
 };
 
 const reduxReducers = combineReducers(reducers);
 
-export { reduxReducers as default, reduxReducers, graphReducer, inventoryReducer, userReducer, viewReducer };
+export {
+  reduxReducers as default,
+  reduxReducers,
+  graphReducer,
+  inventoryReducer,
+  toolbarReducer,
+  userReducer,
+  viewReducer
+};
---
+++ src/redux/types/toolbarTypes.js
@@ -0,0 +1,14 @@
+const SET_ACTIVE_FILTERS = 'SET_ACTIVE_FILTERS';
+const SET_FILTER_TYPE = 'SET_FILTER_TYPE';
+
+/**
+ * Filter, toolbar reducer types.
+ *
+ * @type {{SET_FILTER_TYPE: string, SET_ACTIVE_FILTERS: string}}
+ */
+const toolbarTypes = {
+  SET_ACTIVE_FILTERS,
+  SET_FILTER_TYPE
+};
+
+export { toolbarTypes as default, toolbarTypes, SET_ACTIVE_FILTERS, SET_FILTER_TYPE };
