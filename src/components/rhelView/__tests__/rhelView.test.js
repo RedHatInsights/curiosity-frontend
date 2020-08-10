66f5721cd21f16b2aefbd2f2f8a97f2b6d2a1344
CD Cabrera <ccabrera@redhat.com>
2020-08-09 22:29:31 -0400

    feat(inventoryList,views): issues/10 display inventory list (#372)
    * styles, min-heights graphCard, inventoryList
    * inventoryList, initial inventory list display
    * inventoryListHelpers, parse table cells
    * inventoryList perPageDefault for skeleton loader
    * pagination, initial pagination
    * rhelView, openshiftView add inventoryList
    * locale, i18n strings

--- src/components/rhelView/__tests__/rhelView.test.js
+++ src/components/rhelView/__tests__/rhelView.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { RhelView } from '../rhelView';
+import { parseRowCellsListData } from '../../inventoryList/inventoryListHelpers';
 
 describe('RhelView Component', () => {
   it('should render a non-connected component', () => {
@@ -45,5 +46,25 @@ describe('RhelView Component', () => {
 
     const component = shallow(<RhelView {...props} />);
     expect(component).toMatchSnapshot('alternate graph');
+  });
+
+  it('should have default props that set product configuration', () => {
+    const { initialGraphFilters, initialInventoryFilters, query } = RhelView.defaultProps;
+    expect({ initialGraphFilters, initialInventoryFilters, query }).toMatchSnapshot('initial configuration');
+
+    const filteredInventoryData = parseRowCellsListData({
+      filters: initialInventoryFilters,
+      cellData: {
+        displayName: 'lorem',
+        inventoryId: 'lorem inventory id',
+        hardwareType: 'ipsum',
+        numberOfGuests: 3,
+        sockets: 10,
+        cores: 12,
+        lastSeen: 'lorem month ago'
+      }
+    });
+
+    expect(filteredInventoryData).toMatchSnapshot('filter results');
   });
 });
