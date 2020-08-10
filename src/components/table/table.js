7322940592ab7b45c5471269a28612b87f6e9a27
CD Cabrera <ccabrera@redhat.com>
2020-08-09 22:42:08 -0400

    fix(table,tableSkeleton): issues/10 align content prop to pf (#372)
    * table, pf fixme annotations, align to pf title/content prop
    * tableSkeleton, switch to generic node, related to pf title align

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -9,6 +9,37 @@ import { helpers } from '../../common';
 import { translate } from '../i18n/i18n';
 
 /**
+ * FixMe: PF tables uses sequentially ordered lists/arrays to denote expandable sections
+ * Ideal solution is to nest expandable sections within their respective parents. There
+ * are scenarios where manipulating a sequentially ordered list/array can, and will,
+ * lead to unintended results. And forces an equal, if not greater, amount of effort on
+ * state tracking in the consuming GUI/app. Compounding the issue, the index is used/returned
+ * during events for a particular row being "expanded". This ends up making after-the-fact
+ * application of the expandable list/array items problematic since said index is
+ * constantly updating/shifting as rows are dynamically added. Add in a "not every row
+ * needs to be expandable" scenario, you can start to see the issues. All of that
+ * effort means that in some cases it may actually be quicker to simply use PF styles and
+ * a HTML table directly, bypassing PF-React entirely.
+ *
+ * Applying a React node as content in the sequentially ordered list/array appears to
+ * automatically "mount" said node. How this handles in child components where subsequent
+ * AJAX/XHR calls are called means you are potentially hitting an API with dozens of calls,
+ * row dependent, even when a user may not expand the nested row/child. This could easily
+ * be resolved by nesting and applying the expandable contents after-the-fact.
+ */
+/**
+ * FixMe: PF tables misalignment of the column headers list/array and row cells breaks the component
+ * The related PF error for this is cryptic. An immediate solution would be to simply fix the error
+ * messaging, or do a length check before consuming the lists/arrays. This happens when a consumer
+ * places more or less column header cells than row cells.
+ */
+/**
+ * FixMe: PF tables break when an empty list/array is used for the column headers
+ * This appears related to the mismatched column header and row cells. The solution is to simply not
+ * render the header row instead of throwing an error. This is similar to how the empty rows
+ * list/array is handled.
+ */
+/**
  * A table.
  *
  * @augments React.Component
@@ -16,6 +47,7 @@ import { translate } from '../i18n/i18n';
  */
 class Table extends React.Component {
   state = {
+    isCollapsible: false,
     updatedColumnHeaders: null,
     updatedRows: null
   };
@@ -67,9 +99,15 @@ class Table extends React.Component {
     const { columnHeaders, rows } = this.props;
     const updatedColumnHeaders = [];
     const updatedRows = [];
+    let isCollapsible = false;
 
     columnHeaders.forEach(columnHeader => {
-      updatedColumnHeaders.push(columnHeader);
+      if (columnHeader?.title) {
+        const { title, ...settings } = columnHeader;
+        updatedColumnHeaders.push({ title, ...settings });
+      } else {
+        updatedColumnHeaders.push({ title: columnHeader });
+      }
     });
 
     rows.forEach(({ expandedContent, cells, isExpanded }) => {
@@ -79,6 +117,7 @@ class Table extends React.Component {
       updatedRows.push(rowObj);
 
       if (expandedContent) {
+        isCollapsible = true;
         rowObj.isOpen = isExpanded || false;
 
         updatedRows.push({
@@ -90,9 +129,9 @@ class Table extends React.Component {
       }
 
       cells.forEach(cell => {
-        if (cell?.cell) {
-          const { cell: contentCell, ...settings } = cell;
-          rowObj.cells.push({ title: contentCell, ...settings });
+        if (cell?.title) {
+          const { title, ...settings } = cell;
+          rowObj.cells.push({ title, ...settings });
         } else {
           rowObj.cells.push({ title: cell });
         }
@@ -100,19 +139,27 @@ class Table extends React.Component {
     });
 
     this.setState({
+      isCollapsible,
       updatedColumnHeaders,
       updatedRows
     });
   }
 
   /**
+   * FixMe: PF Tables automatically applies an expandable className when using the "onCollapse" prop?
+   * Not every row may need to be expandable. In certain scenarios a table may have no expandable
+   * sections, however it appears the "onCollapse" styling, and additionally added "table cells" are still
+   * applied.
+   */
+  /**
    * Apply props to table.
    *
    * @returns {Node}
    */
   renderTable() {
-    const { updatedColumnHeaders, updatedRows } = this.state;
+    const { isCollapsible, updatedColumnHeaders, updatedRows } = this.state;
     const { ariaLabel, borders, children, className, isHeader, summary, t, variant } = this.props;
+    const pfTableProps = {};
     let emptyTable = null;
 
     if (!updatedRows?.length) {
@@ -125,6 +172,10 @@ class Table extends React.Component {
       );
     }
 
+    if (isCollapsible) {
+      pfTableProps.onCollapse = (event, index, isOpen, data) => this.onCollapse({ event, index, isOpen, data });
+    }
+
     return (
       <React.Fragment>
         <PfTable
@@ -133,9 +184,9 @@ class Table extends React.Component {
           aria-label={ariaLabel || t('curiosity-inventory.tableAriaLabel', { appName: helpers.UI_DISPLAY_NAME })}
           summary={summary || t('curiosity-inventory.tableSummary')}
           variant={variant}
-          onCollapse={(event, index, isOpen, data) => this.onCollapse({ event, index, isOpen, data })}
           rows={(updatedRows?.length && updatedRows) || []}
           cells={updatedColumnHeaders || []}
+          {...pfTableProps}
         >
           {isHeader && <TableHeader />}
           <TableBody />
@@ -186,7 +237,7 @@ Table.propTypes = {
         PropTypes.oneOfType([
           PropTypes.node,
           PropTypes.shape({
-            cell: PropTypes.node
+            title: PropTypes.node
           })
         ])
       ),
