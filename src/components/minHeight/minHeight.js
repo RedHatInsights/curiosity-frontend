57472b42dbea28e0297e695b2c3ddf36dae26e4c
CD Cabrera <ccabrera@redhat.com>
2020-10-07 16:36:03 -0400

    fix(minHeight): issues/403 resize events reset height (#447)

--- src/components/minHeight/minHeight.js
+++ src/components/minHeight/minHeight.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { helpers } from '../../common';
 
 /**
  * Set a min-height to prevent page jump component.
  *
  * @augments React.Component
+ * @fires onResizeContainer
  */
 class MinHeight extends React.Component {
   containerRef = React.createRef();
 
   updatedMinHeight = 0;
 
+  updatedContainerWidth = 0;
+
+  resizeObserver = helpers.noop;
+
   componentDidMount() {
     this.setMinHeight();
+    this.setResizeObserver();
   }
 
   componentDidUpdate() {
@@ -23,14 +30,40 @@ class MinHeight extends React.Component {
     }
   }
 
+  componentWillUnmount() {
+    this.resizeObserver();
+  }
+
+  /**
+   * On resize adjust graph display.
+   *
+   * @event onResizeContainer
+   */
+  onResizeContainer = () => {
+    const { updatedContainerWidth } = this;
+    const clientWidth = this.containerRef?.current?.clientWidth || 0;
+
+    if (clientWidth !== updatedContainerWidth) {
+      this.updatedContainerWidth = clientWidth;
+      this.setMinHeight(true);
+    }
+  };
+
   /**
    * Set minHeight on mount or update.
+   *
+   * @param {boolean} resetMinHeight
    */
-  setMinHeight() {
+  setMinHeight(resetMinHeight) {
     const { updatedMinHeight } = this;
     const { minHeight: overrideMinHeight } = this.props;
-    // const { clientHeight = 0 } = this.containerRef.current || {};
-    const clientHeight = this.containerRef?.current?.clientHeight || 0;
+    const { current: domElement = {} } = this.containerRef;
+
+    if (resetMinHeight && domElement.style) {
+      domElement.style.minHeight = 0;
+    }
+
+    const clientHeight = domElement?.clientHeight || 0;
 
     if (clientHeight !== updatedMinHeight) {
       this.updatedMinHeight = clientHeight;
@@ -38,6 +71,23 @@ class MinHeight extends React.Component {
 
     if (overrideMinHeight > this.updatedMinHeight) {
       this.updatedMinHeight = overrideMinHeight;
+    }
+  }
+
+  /**
+   * Set ResizeObserver for scenarios when min-height needs to be updated.
+   */
+  setResizeObserver() {
+    const containerElement = this.containerRef.current;
+    const { ResizeObserver } = window;
+
+    if (containerElement && ResizeObserver) {
+      const resizeObserver = new ResizeObserver(this.onResizeContainer);
+      resizeObserver.observe(containerElement);
+      this.resizeObserver = () => resizeObserver.unobserve(containerElement);
+    } else {
+      window.addEventListener('resize', this.onResizeContainer);
+      this.resizeObserver = () => window.removeEventListener('resize', this.onResizeContainer);
     }
   }
 
