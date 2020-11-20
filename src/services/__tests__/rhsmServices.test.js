92372ed6ab3fb21da36f9f1c82e00cd78ae1bad9
CD Cabrera <ccabrera@redhat.com>
2020-11-20 09:56:00 -0500

    feat(rhsmServices,redux): issues/491 initial api, state setup (#517)
    * build, dotenv, subscriptions inventory endpoint, platform test
    * inventoryListSel, minor test update
    * redux, rhsmActions,reducer,type, subscriptions inventory state
    * rhsmApiTypes, expand for subscriptions, sort types
    * rhsmServices, getSubscriptionsInventory api method
    * subscriptionsListSel, base selector from inventoryListSel

--- src/services/__tests__/rhsmServices.test.js
+++ src/services/__tests__/rhsmServices.test.js
@@ -5,7 +5,7 @@ describe('RhsmServices', () => {
   beforeEach(() => {
     moxios.install();
 
-    moxios.stubRequest(/\/(tally|capacity|hosts|version).*?/, {
+    moxios.stubRequest(/\/(tally|capacity|hosts|subscriptions|version).*?/, {
       status: 200,
       responseText: 'success',
       timeout: 1
@@ -17,7 +17,7 @@ describe('RhsmServices', () => {
   });
 
   it('should export a specific number of methods and classes', () => {
-    expect(Object.keys(rhsmServices)).toHaveLength(5);
+    expect(Object.keys(rhsmServices)).toHaveLength(6);
   });
 
   it('should have specific methods', () => {
@@ -26,6 +26,7 @@ describe('RhsmServices', () => {
     expect(rhsmServices.getGraphReports).toBeDefined();
     expect(rhsmServices.getHostsInventory).toBeDefined();
     expect(rhsmServices.getHostsInventoryGuests).toBeDefined();
+    expect(rhsmServices.getSubscriptionsInventory).toBeDefined();
   });
 
   /**
