266d3e038f6a476ec3b8ddd69c613728ecd541cb
CD Cabrera <ccabrera@redhat.com>
2020-12-14 20:12:08 -0500

    feat(productView): issues/524 display satellite (#538)
    * locale, satellite strings
    * productView, basic product display
    * productViewSatellite, apply satellite to basic display
    * routerTypes, satellite routing
    * rhsmApiTypes, satellite products

---
+++ src/components/productView/__tests__/productViewSatellite.test.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ProductViewSatellite } from '../productViewSatellite';
+import { parseRowCellsListData } from '../../inventoryList/inventoryListHelpers';
+
+describe('ProductViewSatellite Component', () => {
+  it('should render a non-connected component', () => {
+    const props = {
+      routeDetail: {
+        pathParameter: 'lorem ipsum',
+        productParameter: 'dolor sit'
+      }
+    };
+
+    const component = shallow(<ProductViewSatellite {...props} />);
+    expect(component).toMatchSnapshot('non-connected');
+  });
+
+  it('should set product configuration', () => {
+    const {
+      initialGraphFilters,
+      initialGuestsFilters,
+      initialInventoryFilters,
+      query
+    } = ProductViewSatellite.defaultProps.productConfig;
+    expect({ initialGraphFilters, initialGuestsFilters, initialInventoryFilters, query }).toMatchSnapshot(
+      'initial configuration'
+    );
+
+    const inventoryData = {
+      displayName: 'lorem',
+      inventoryId: 'lorem inventory id',
+      hardwareType: 'ipsum',
+      measurementType: null,
+      numberOfGuests: 3,
+      sockets: 10,
+      cores: 12,
+      lastSeen: 'lorem date obj',
+      loremIpsum: 'hello world'
+    };
+
+    const filteredInventoryData = parseRowCellsListData({
+      filters: initialInventoryFilters,
+      cellData: inventoryData
+    });
+
+    expect(filteredInventoryData).toMatchSnapshot('filteredInventoryData results');
+
+    const filteredInventoryDataAuthorized = parseRowCellsListData({
+      filters: initialInventoryFilters,
+      cellData: inventoryData,
+      session: { authorized: { inventory: true } }
+    });
+
+    expect(filteredInventoryDataAuthorized).toMatchSnapshot('filteredInventoryData results, authorized');
+
+    const guestsData = {
+      displayName: 'lorem',
+      inventoryId: 'lorem inventory id',
+      subscriptionManagerId: 'lorem subscription id',
+      lastSeen: 'lorem date obj',
+      loremIpsum: 'hello world'
+    };
+
+    const filteredGuestsData = parseRowCellsListData({
+      filters: initialGuestsFilters,
+      cellData: guestsData
+    });
+
+    expect(filteredGuestsData).toMatchSnapshot('filteredGuestsData results');
+
+    const filteredGuestsDataAuthorized = parseRowCellsListData({
+      filters: initialGuestsFilters,
+      cellData: guestsData,
+      session: { authorized: { inventory: true } }
+    });
+
+    expect(filteredGuestsDataAuthorized).toMatchSnapshot('filteredGuestsData results, authorized');
+  });
+});
