6e8b8214bf079c67fe7eaebb6fbdd435ba495f0e
CD Cabrera <ccabrera@redhat.com>
2021-01-08 13:09:00 -0500

    fix(router,routerTypes): issues/476 remove competing routes (#545)
    * router, restructure redirect, remove unused param
    * routerHelpers, minor optional chaining
    * routerTypes, remove competing routes, avoid page refresh

--- src/components/router/routerHelpers.js
+++ src/components/router/routerHelpers.js
@@ -88,7 +88,7 @@ const getRouteDetail = ({ id = null, pathname = null }) => {
 
   if (!routeItem && pathname) {
     routeItem = routes.find(value => pathname === value.to);
-    routeItem = routeItem || routes.find(item => pathname.includes(item.to.split(':')[0]));
+    routeItem = routeItem || routes.find(item => pathname.includes(item?.to?.split(':')[0]));
   }
 
   return { ...(routeItem || {}) };
