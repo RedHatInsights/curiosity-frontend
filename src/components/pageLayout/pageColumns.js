738ce30c51148d5241866b98f050175963b180ef
CD Cabrera <ccabrera@redhat.com>
2021-02-10 14:24:09 -0500

    feat(pageColumns): ent-3504 allow page columns

---
+++ src/components/pageLayout/pageColumns.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Flex, FlexItem } from '@patternfly/react-core';
+import { helpers } from '../../common';
+
+/**
+ * Render page columns based on children.
+ *
+ * @param {object} props
+ * @param {Node} props.children
+ * @param {string} props.className
+ * @returns {Node}
+ */
+const PageColumns = ({ children, className }) => (
+  <Flex spaceItems={{ sm: 'spaceItemsNone' }} className={`curiosity-page-columns ${className}`}>
+    {React.Children.toArray(children)
+      .filter(child => React.isValidElement(child))
+      .map(child => (
+        <FlexItem
+          key={helpers.generateId()}
+          style={{ flexBasis: 0 }}
+          grow={{ sm: 'grow' }}
+          className="curiosity-page-columns-column"
+        >
+          {child}
+        </FlexItem>
+      ))}
+  </Flex>
+);
+
+/**
+ * Prop types.
+ *
+ * @type {{children: Node, className: string}}
+ */
+PageColumns.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string
+};
+
+/**
+ * Default props.
+ *
+ * @type {{className: string}}
+ */
+PageColumns.defaultProps = {
+  className: ''
+};
+
+export { PageColumns as default, PageColumns };
