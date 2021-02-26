5c26abdc784648d8deff83a587118ee82a7fc29b
CD Cabrera <ccabrera@redhat.com>
2021-02-25 21:03:29 -0500

    feat(productViewOpenShiftContainer): ent-3324 two column view (#591)
    * i18n, locale strings
    * openshiftView, converted to productViewOpenShiftContainer
    * productView(s), remove unused viewSelector, redux refs
    * productViewOpenShiftContainer, useSelector for uom
    * routerConfig, point to new component
    * viewSelectors, removed, unnecessary

---
+++ src/components/productView/__tests__/productViewOpenShiftContainer.test.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ProductViewOpenShiftContainer } from '../productViewOpenShiftContainer';
+import { parseRowCellsListData } from '../../inventoryList/inventoryListHelpers';
+
+describe('ProductViewOpenShiftContainer Component', () => {
+  it('should render a non-connected component', () => {
+    const props = {
+      routeDetail: {
+        pathParameter: 'lorem ipsum',
+        productParameter: 'dolor sit'
+      }
+    };
+
+    const component = shallow(<ProductViewOpenShiftContainer {...props} />);
+    expect(component).toMatchSnapshot('non-connected');
+  });
+
+  it('should set multiple product configurations', () => {
+    const [productOne, productTwo] = ProductViewOpenShiftContainer.defaultProps.productConfig;
+    expect([
+      {
+        initialGraphFilters: productOne.initialGraphFilters,
+        initialInventoryFilters: productOne.initialInventoryFilters,
+        query: productOne.query
+      },
+      {
+        initialGraphFilters: productTwo.initialGraphFilters,
+        initialInventoryFilters: productTwo.initialInventoryFilters,
+        query: productTwo.query
+      }
+    ]).toMatchSnapshot('initial configuration');
+
+    const inventoryData = {
+      displayName: 'lorem',
+      inventoryId: 'lorem inventory id',
+      coreHours: 12.53,
+      lastSeen: 'lorem date obj',
+      loremIpsum: 'hello world'
+    };
+
+    const filteredInventoryDataProductOne = parseRowCellsListData({
+      filters: productOne.initialInventoryFilters,
+      cellData: inventoryData
+    });
+
+    expect(filteredInventoryDataProductOne).toMatchSnapshot('filteredInventoryData results, Product One');
+
+    const filteredInventoryDataProductTwo = parseRowCellsListData({
+      filters: productTwo.initialInventoryFilters,
+      cellData: inventoryData
+    });
+
+    expect(filteredInventoryDataProductTwo).toMatchSnapshot('filteredInventoryData results, Product Two');
+
+    const fallbackInventoryData = {
+      ...inventoryData,
+      coreHours: null,
+      inventoryId: null,
+      lastSeen: null
+    };
+
+    const fallbackFilteredInventoryDataProductOne = parseRowCellsListData({
+      filters: productOne.initialInventoryFilters,
+      cellData: fallbackInventoryData
+    });
+
+    expect(fallbackFilteredInventoryDataProductOne).toMatchSnapshot(
+      'filteredInventoryData results, fallback display, Product One'
+    );
+
+    const fallbackFilteredInventoryDataProductTwo = parseRowCellsListData({
+      filters: productTwo.initialInventoryFilters,
+      cellData: fallbackInventoryData
+    });
+
+    expect(fallbackFilteredInventoryDataProductTwo).toMatchSnapshot(
+      'filteredInventoryData results, fallback display, Product Two'
+    );
+  });
+});
