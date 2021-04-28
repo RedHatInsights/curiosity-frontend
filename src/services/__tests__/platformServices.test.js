a0d3c1cc4c8a62488ebcfd1cb0599b5ffd272522
CD Cabrera <ccabrera@redhat.com>
2021-04-28 10:24:59 -0400

    fix(build): npm updates (#648)

--- src/services/__tests__/platformServices.test.js
+++ src/services/__tests__/platformServices.test.js
@@ -1,6 +1,19 @@
 import platformServices from '../platformServices';
 
 describe('PlatformServices', () => {
+  // Return a promise, or promise like, response for errors
+  const returnPromiseAsync = async promiseAsyncCall => {
+    let response;
+
+    try {
+      response = await promiseAsyncCall();
+    } catch (e) {
+      response = e;
+    }
+
+    return response;
+  };
+
   it('should export a specific number of methods and classes', () => {
     expect(Object.keys(platformServices)).toHaveLength(7);
   });
@@ -19,38 +32,31 @@ describe('PlatformServices', () => {
    *  timeout errors associated with this test sometimes stem from endpoint
    *  settings or missing globals, see "before" above, or the "setupTests" config
    */
-  it('should return async for most methods and resolve successfully', done => {
+  it('should return async for most methods and resolve successfully', async () => {
     const promises = Object.keys(platformServices).map(value => platformServices[value]());
+    const response = await Promise.all(promises);
 
-    Promise.all(promises).then(success => {
-      expect(success.length).toEqual(Object.keys(platformServices).length);
-      done();
-    });
+    expect(response.length).toEqual(Object.keys(platformServices).length);
   });
 
-  it('should return a successful getUser', done => {
-    platformServices.getUser().then(value => {
-      expect(value).toMatchSnapshot('success authorized user');
-      done();
-    });
+  it('should return a successful getUser', async () => {
+    const response = await platformServices.getUser();
+
+    expect(response).toMatchSnapshot('success authorized user');
   });
 
-  it('should return a successful getUser with a specific response', done => {
+  it('should return a successful getUser with a specific response', async () => {
     window.insights.chrome.auth.getUser = jest.fn().mockImplementation(() => Promise.resolve('lorem ipsum'));
+    const response = await platformServices.getUser();
 
-    platformServices.getUser().then(value => {
-      expect(value).toMatchSnapshot('specific success for authorized user');
-      done();
-    });
+    expect(response).toMatchSnapshot('specific success for authorized user');
   });
 
-  it('should return a failed getUser', done => {
+  it('should return a failed getUser', async () => {
     window.insights.chrome.auth.getUser = undefined;
+    const response = await returnPromiseAsync(platformServices.getUser);
 
-    platformServices.getUser().catch(error => {
-      expect(error).toMatchSnapshot('failed authorized user');
-      done();
-    });
+    expect(response).toMatchSnapshot('failed authorized user');
   });
 
   it('should return a failed getUserPermissions', () => {
@@ -61,22 +67,18 @@ describe('PlatformServices', () => {
     );
   });
 
-  it('should return a failed hideGlobalFilter', done => {
+  it('should return a failed hideGlobalFilter', async () => {
     window.insights.chrome.hideGlobalFilter = undefined;
+    const response = await returnPromiseAsync(platformServices.hideGlobalFilter);
 
-    platformServices.hideGlobalFilter().catch(error => {
-      expect(error).toMatchSnapshot('failed hideGlobalFilter');
-      done();
-    });
+    expect(response).toMatchSnapshot('failed hideGlobalFilter');
   });
 
-  it('should return a failed initializeChrome', done => {
+  it('should return a failed initializeChrome', async () => {
     window.insights.chrome.init = undefined;
+    const response = await returnPromiseAsync(platformServices.initializeChrome);
 
-    platformServices.initializeChrome().catch(error => {
-      expect(error).toMatchSnapshot('failed initializeChrome');
-      done();
-    });
+    expect(response).toMatchSnapshot('failed initializeChrome');
   });
 
   it('should return a failed onNavigation', () => {
@@ -86,13 +88,11 @@ describe('PlatformServices', () => {
     );
   });
 
-  it('should return a failed setAppName', done => {
+  it('should return a failed setAppName', async () => {
     window.insights.chrome.identifyApp = undefined;
+    const response = await returnPromiseAsync(platformServices.setAppName);
 
-    platformServices.setAppName().catch(error => {
-      expect(error).toMatchSnapshot('failed setAppName');
-      done();
-    });
+    expect(response).toMatchSnapshot('failed setAppName');
   });
 
   it('should return a failed setNavigation', () => {
