475eed1b86c0102592620618e139274ee2f2bcb0
CD Cabrera <ccabrera@redhat.com>
2021-05-05 22:26:46 -0400

    refactor(routerHelpers): ent-2745 consolidate nav, route lookup (#656)
    * productViewMissing, switch to route config, allow alias matching
    * router, switch to combined nav route detail lookup
    * routerHelpers, consolidate nav, route lookup, clean up

--- src/components/router/__tests__/routerHelpers.test.js
+++ src/components/router/__tests__/routerHelpers.test.js
@@ -1,16 +1,10 @@
 import {
-  appName,
-  baseName,
-  basePath,
+  routerHelpers,
   dynamicBaseName,
+  dynamicBasePath,
   getErrorRoute,
-  getNavigationDetail,
-  getRouteDetail,
-  getNavRouteDetail,
-  navigation,
-  platformLandingRedirect,
-  platformModalRedirect,
-  productGroups,
+  getRouteConfig,
+  getRouteConfigByPath,
   routes
 } from '../routerHelpers';
 
@@ -19,25 +13,20 @@ describe('RouterHelpers', () => {
     const updatedUrl = new URL(url);
     const { location } = window;
     delete window.location;
-    window.location = { href: updatedUrl.href, search: updatedUrl.search, hash: updatedUrl.hash };
+    // mock
+    window.location = {
+      href: updatedUrl.href,
+      search: updatedUrl.search,
+      hash: updatedUrl.hash,
+      pathname: updatedUrl.pathname
+    };
     await callback();
+    // restore
     window.location = location;
   };
 
   it('should return specific properties', () => {
-    expect(appName).toMatchSnapshot('appName');
-    expect(baseName).toBeDefined();
-    expect(basePath).toBeDefined();
-    expect(dynamicBaseName).toBeDefined();
-    expect(getErrorRoute).toBeDefined();
-    expect(getNavigationDetail).toBeDefined();
-    expect(getRouteDetail).toBeDefined();
-    expect(getNavRouteDetail).toBeDefined();
-    expect(navigation).toMatchSnapshot('navigation');
-    expect(platformLandingRedirect).toMatchSnapshot('platformLandingRedirect');
-    expect(platformModalRedirect).toMatchSnapshot('platformModalRedirect');
-    expect(productGroups).toMatchSnapshot('productGroups');
-    expect(routes).toMatchSnapshot('routes');
+    expect(routerHelpers).toMatchSnapshot('routerHelpers');
   });
 
   it('should return a generated baseName using NO path prefix', () => {
@@ -102,52 +91,103 @@ describe('RouterHelpers', () => {
     ).toMatchSnapshot('insights, beta app lorem route name');
   });
 
+  it('should return a generated basePath', () => {
+    expect(
+      dynamicBasePath({
+        pathName: '/insights/appName',
+        appName: 'appName'
+      })
+    ).toMatchSnapshot('insights, app base path');
+
+    expect(
+      dynamicBasePath({
+        pathName: '/beta/insights/appName',
+        appName: 'appName'
+      })
+    ).toMatchSnapshot('insights, beta app base path');
+
+    expect(
+      dynamicBasePath({
+        pathName: '/insights/appName/loremRoute',
+        appName: 'appName'
+      })
+    ).toMatchSnapshot('insights, app lorem route name base path');
+
+    expect(
+      dynamicBasePath({
+        pathName: '/beta/insights/appName/loremRoute',
+        appName: 'appName'
+      })
+    ).toMatchSnapshot('insights, beta app lorem route name base path');
+  });
+
   it('should return an error route', () => {
     expect(getErrorRoute).toMatchSnapshot('error route');
   });
 
   it('should return navigation and route details that align to location', () => {
     expect({
-      nav: getNavigationDetail({ id: 'optin' }),
-      route: getRouteDetail({ id: 'optin' }),
-      navRoute: getNavRouteDetail({ id: 'optin' })
+      navRoute: getRouteConfig({ id: 'optin' })
     }).toMatchSnapshot('detail: specific route ID');
 
     expect({
-      nav: getNavigationDetail({ id: 'rhel-arm' }),
-      route: getRouteDetail({ id: 'rhel-arm' }),
-      navRoute: getNavRouteDetail({ id: 'rhel-arm' })
+      navRoute: getRouteConfig({ id: 'rhel-arm' })
     }).toMatchSnapshot('detail: specific navigation ID');
 
     expect({
-      nav: getNavigationDetail({ pathname: '/rhel' }),
-      route: getRouteDetail({ pathname: '/rhel' }),
-      navRoute: getNavRouteDetail({ pathname: '/rhel' })
+      navRoute: getRouteConfig({ pathName: '/rhel' })
     }).toMatchSnapshot('detail: match specific path navigation');
 
     expect({
-      nav: getNavigationDetail({ id: 'lorem-missing', pathname: '/rhel' }),
-      route: getRouteDetail({ id: 'lorem-missing', pathname: '/rhel' }),
-      navRoute: getNavRouteDetail({ id: 'lorem-missing', pathname: '/rhel' })
+      navRoute: getRouteConfig({ id: 'lorem-missing', pathName: '/rhel' })
     }).toMatchSnapshot('detail: missing ID, specific path');
 
     expect({
-      nav: getNavigationDetail({ id: 'lorem', pathname: '/lorem-ipsum-missing', returnDefault: true }),
-      route: getRouteDetail({ id: 'lorem', pathname: '/lorem-ipsum-missing', returnDefault: true }),
-      navRoute: getNavRouteDetail({ id: 'lorem', pathname: '/lorem-ipsum-missing', returnDefault: true })
-    }).toMatchSnapshot('detail: missing id and pathname, default');
+      navRoute: getRouteConfig({ id: 'lorem', pathName: '/lorem-ipsum-missing', returnDefault: true })
+    }).toMatchSnapshot('detail: missing id and pathName, default');
 
     expect({
-      nav: getNavigationDetail({ id: 'lorem', pathname: '/lorem-ipsum-missing', returnDefault: false }),
-      route: getRouteDetail({ id: 'lorem', pathname: '/lorem-ipsum-missing', returnDefault: false }),
-      navRoute: getNavRouteDetail({ id: 'lorem', pathname: '/lorem-ipsum-missing', returnDefault: false })
-    }).toMatchSnapshot('detail: missing id and pathname and default');
+      navRoute: getRouteConfig({ id: 'lorem', pathName: '/lorem-ipsum-missing', returnDefault: false })
+    }).toMatchSnapshot('detail: missing id and pathName and default');
 
     expect({
-      nav: getNavigationDetail({}),
-      route: getRouteDetail({}),
-      navRoute: getNavRouteDetail({})
+      navRoute: getRouteConfig({})
     }).toMatchSnapshot('detail: missing parameters');
+  });
+
+  it('should return default navigation and route details', () => {
+    mockWindowLocation({
+      url: 'https://ci.foo.redhat.com/loremIpsum/dolorSit/',
+      callback: () => {
+        expect({
+          navRoute: getRouteConfigByPath()
+        }).toMatchSnapshot('detail: defaults');
+      }
+    });
+  });
+
+  it('should return navigation and route details from a path', () => {
+    expect({
+      navRoute: getRouteConfigByPath({ pathName: '/rhel' }).firstMatch
+    }).toMatchSnapshot('detail: match specific path');
+
+    expect({
+      navRoute: getRouteConfigByPath({ pathName: '/rhel-arm' }).firstMatch
+    }).toMatchSnapshot('detail: specific product path');
+
+    expect({
+      navRoute: getRouteConfigByPath({ pathName: '/lorem-ipsum-missing' }).firstMatch
+    }).toMatchSnapshot('detail: missing pathName');
+
+    expect({
+      navRoute: getRouteConfigByPath({}).firstMatch
+    }).toMatchSnapshot('detail: missing parameters');
+  });
+
+  it('should return navigation and route details from a related name', () => {
+    expect({
+      navRoute: getRouteConfigByPath({ pathName: '/lorem-ipsum/RHEL%20for%20ARM' }).firstMatch
+    }).toMatchSnapshot('detail: match related name');
   });
 
   it('should handle location search and hash passthrough values', () => {
@@ -155,7 +195,7 @@ describe('RouterHelpers', () => {
       url: 'https://ci.foo.redhat.com/subscriptions/rhel',
       callback: () => {
         expect({
-          routeHref: getNavigationDetail({ pathname: '/rhel' }).routeHref
+          routeHref: getRouteConfig({ pathName: '/rhel' }).routeHref
         }).toMatchSnapshot('NO search and hash');
       }
     });
@@ -164,7 +204,7 @@ describe('RouterHelpers', () => {
       url: 'https://ci.foo.redhat.com/subscriptions/rhel?dolor=sit',
       callback: () => {
         expect({
-          routeHref: getNavigationDetail({ pathname: '/rhel' }).routeHref
+          routeHref: getRouteConfig({ pathName: '/rhel' }).routeHref
         }).toMatchSnapshot('search');
       }
     });
@@ -173,7 +213,7 @@ describe('RouterHelpers', () => {
       url: 'https://ci.foo.redhat.com/subscriptions/rhel#lorem',
       callback: () => {
         expect({
-          routeHref: getNavigationDetail({ pathname: '/rhel' }).routeHref
+          routeHref: getRouteConfig({ pathName: '/rhel' }).routeHref
         }).toMatchSnapshot('hash');
       }
     });
@@ -182,7 +222,7 @@ describe('RouterHelpers', () => {
       url: 'https://ci.foo.redhat.com/subscriptions/rhel?dolor=sit#lorem',
       callback: () => {
         expect({
-          routeHref: getNavigationDetail({ pathname: '/rhel' }).routeHref
+          routeHref: getRouteConfig({ pathName: '/rhel' }).routeHref
         }).toMatchSnapshot('search and hash');
       }
     });
@@ -201,6 +241,6 @@ describe('RouterHelpers', () => {
       }
     });
 
-    expect(lazyLoadComponents.length).toBe(12);
+    expect(lazyLoadComponents.length === routes.length).toBe(true);
   });
 });
