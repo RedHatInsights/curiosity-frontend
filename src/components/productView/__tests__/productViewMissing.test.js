c0927de9274df23bb69bc2caee06ad7dee05ed06
CD Cabrera <ccabrera@redhat.com>
2021-06-01 11:49:53 -0400

    fix(redirect): ent-3833 restructure, force a path (#695)
    * testing, window.replace noop
    * authentication, remove isRedirect
    * productViewMissing, on available product cards redirect
    * redirect, restructure, force a path, pass search, hash
    * router, var rename, centralize exports, index.js
    * routerHelpers, optional chain

--- src/components/productView/__tests__/productViewMissing.test.js
+++ src/components/productView/__tests__/productViewMissing.test.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ProductViewMissing } from '../productViewMissing';
+import { Redirect } from '../../router';
 
 describe('ProductViewMissing Component', () => {
   it('should render a non-connected component', () => {
-    const props = {};
+    const props = {
+      availableProductsRedirect: 1
+    };
     const component = shallow(<ProductViewMissing {...props} />);
     expect(component).toMatchSnapshot('non-connected');
   });
 
   it('should render a predictable set of product cards', () => {
-    const props = {};
+    const props = {
+      availableProductsRedirect: 1
+    };
 
     mockWindowLocation(
       () => {
@@ -18,8 +23,17 @@ describe('ProductViewMissing Component', () => {
         expect(component).toMatchSnapshot('non-connected');
       },
       {
-        url: 'https://ci.foo.redhat.com/loremIpsum/dolorSit/'
+        url: 'https://ci.foo.redhat.com/openshift/subscriptions/'
       }
     );
+  });
+
+  it('should redirect when there are limited product cards', () => {
+    const props = {};
+
+    mockWindowLocation(() => {
+      const component = shallow(<ProductViewMissing {...props} />);
+      expect(component.find(Redirect).html()).toMatchSnapshot('redirect');
+    });
   });
 });
