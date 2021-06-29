da0e8cab679a1d1e589538bdcbbf22109d9dd400
CD Cabrera <ccabrera@redhat.com>
2021-06-29 17:09:59 -0400

    refactor(chart): ent-3843 context, hooks for chart components (#718)
    * build, use updated Victory components, spelling dictionary
    * chart, chartArea rename/remove, refactor to use context, hooks
    * chartContext, context for related components, toggle handler
    * chartElements, bundle base Victory line, areaChart with props
    * chartHelpers, generate element props
    * chartIcon, return a pf icon, or custom svg
    * chartLegend, container component, provide chart toggle interaction
    * chartTooltip, function, return Victory compatible component, styling
    * graphCard, ChartLegend, Tooltips, update towards new chart components
    * productViews, update towards new chart components

---
+++ src/components/chart/chartElements.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { VictoryStack as ChartStack, VictoryTooltip as ChartCursorTooltip } from 'victory';
+import { createContainer } from 'victory-create-container';
+import { Chart, ChartArea, ChartAxis, ChartContainer, ChartLine, ChartThreshold } from '@patternfly/react-charts';
+import { useChartContext } from './chartContext';
+import { chartTooltip } from './chartTooltip';
+
+/**
+ * Generate a compatible Victory chart element/facet component.
+ *
+ * @param {object} props
+ * @param {object} props.chartTypeDefaults
+ * @returns {Node}
+ */
+const ChartElements = ({ chartTypeDefaults }) => {
+  const { chartSettings = {}, chartContainerRef, chartTooltipRef } = useChartContext();
+  const {
+    chartDomain,
+    chartElementsProps,
+    chartWidth,
+    hasData,
+    padding,
+    themeColor,
+    xAxisProps,
+    yAxisProps
+  } = chartSettings;
+
+  let containerComponent = <ChartContainer />;
+  let yAxis = null;
+  let chartElements = [];
+  let stackedChartElements = [];
+
+  if (hasData) {
+    /**
+     * Note: both cursor and voronoiDimension attrs required if the need is to have...
+     * the tooltip populate consistently without being "near" a chart element y axis point
+     */
+    const VictoryVoronoiCursorContainer = createContainer('voronoi', 'cursor');
+    const TooltipLabelComponent = chartTooltip({ chartSettings, chartContainerRef, chartTooltipRef });
+
+    containerComponent = (
+      <VictoryVoronoiCursorContainer
+        cursorDimension="x"
+        voronoiDimension="x"
+        labels={obj => obj}
+        labelComponent={
+          <ChartCursorTooltip
+            dx={0}
+            dy={0}
+            centerOffset={{ x: 0, y: 0 }}
+            flyoutStyle={{ fill: 'transparent', stroke: 'transparent' }}
+            labelComponent={<TooltipLabelComponent />}
+          />
+        }
+        voronoiPadding={50}
+        mouseFollowTooltips
+      />
+    );
+  }
+
+  if (Array.isArray(yAxisProps)) {
+    yAxis = yAxisProps.map(axisProps => (
+      <ChartAxis key={`yaxis-${axisProps.orientation}`} {...axisProps} animate={false} />
+    ));
+  }
+
+  const setChartElement = ({ chartType, props }) => {
+    const { component: Component, ...defaultProps } = chartTypeDefaults[chartType] || chartTypeDefaults.area;
+    return <Component {...{ ...defaultProps, ...props }} />;
+  };
+
+  chartElements = chartElementsProps?.elements.map(setChartElement);
+  stackedChartElements = chartElementsProps?.stackedElements.map(setChartElement);
+
+  return (
+    <Chart
+      animate={{ duration: 0 }}
+      width={chartWidth}
+      themeColor={themeColor}
+      {...{ padding, containerComponent, ...chartDomain }}
+    >
+      <ChartAxis {...xAxisProps} animate={false} />
+      {yAxis}
+      {chartElements}
+      <ChartStack>{stackedChartElements}</ChartStack>
+    </Chart>
+  );
+};
+
+ChartElements.propTypes = {
+  chartTypeDefaults: PropTypes.objectOf(
+    PropTypes.shape({
+      component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
+      animate: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]),
+      interpolation: PropTypes.oneOf(['monotoneX', 'step'])
+    })
+  )
+};
+
+ChartElements.defaultProps = {
+  chartTypeDefaults: {
+    area: {
+      component: ChartArea,
+      animate: {
+        duration: 250,
+        onLoad: { duration: 250 }
+      },
+      interpolation: 'monotoneX'
+    },
+    line: {
+      component: ChartLine,
+      animate: {
+        duration: 250,
+        onLoad: { duration: 250 }
+      },
+      interpolation: 'monotoneX'
+    },
+    threshold: {
+      component: ChartThreshold,
+      animate: {
+        duration: 100,
+        onLoad: { duration: 100 }
+      },
+      interpolation: 'step'
+    }
+  }
+};
+
+export { ChartElements as default, ChartElements };
