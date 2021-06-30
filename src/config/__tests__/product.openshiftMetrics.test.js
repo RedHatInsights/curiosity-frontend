e16b7a4d0d025b003ffb0d3332a81ddfce37cc50
CD Cabrera <ccabrera@redhat.com>
2021-06-30 16:19:02 -0400

    refactor(routes,productViews): ent-2745 base product configs. (#689)
    * config, add products, update routing with products
    * productView, adjust for using array of product configs
    * productViewDefault, new default product component
    * productViewRhel, OpenShift, Satellite, default props to configs

---
+++ src/config/__tests__/product.openshiftMetrics.test.js
@@ -0,0 +1,131 @@
+import { config } from '../product.openshiftMetrics';
+import { parseRowCellsListData } from '../../components/inventoryList/inventoryListHelpers';
+import {
+  RHSM_API_QUERY_SORT_DIRECTION_TYPES as SORT_DIRECTION_TYPES,
+  RHSM_API_QUERY_TYPES
+} from '../../types/rhsmApiTypes';
+
+describe('Product OpenShift Metrics config', () => {
+  it('should apply graph configuration', () => {
+    const { initialGraphSettings } = config;
+
+    expect({
+      productActionDisplay: initialGraphSettings.actionDisplay({
+        data: {
+          coreHours: [
+            {
+              y: 0
+            },
+            {
+              y: 400
+            },
+            {
+              y: 100
+            }
+          ]
+        },
+        meta: {
+          totalCoreHours: 500
+        }
+      })
+    }).toMatchSnapshot('product action display should display a total value below 1000');
+
+    expect({
+      productActionDisplay: initialGraphSettings.actionDisplay({
+        data: {
+          coreHours: [
+            {
+              y: 0
+            },
+            {
+              y: 800000
+            },
+            {
+              y: 100000
+            }
+          ]
+        },
+        meta: {
+          totalCoreHours: 900000
+        }
+      })
+    }).toMatchSnapshot('product action display should display a total value below 1000000');
+
+    expect({
+      productActionDisplay: initialGraphSettings.actionDisplay({
+        data: {
+          coreHours: [
+            {
+              y: 0
+            },
+            {
+              y: 1000
+            },
+            {
+              y: 100
+            }
+          ]
+        },
+        meta: {
+          totalCoreHours: 1100
+        }
+      })
+    }).toMatchSnapshot('product action display should display a total value');
+
+    expect({
+      productActionDisplay: initialGraphSettings.actionDisplay({
+        data: {
+          loremIpsum: [
+            {
+              y: 0
+            },
+            {
+              y: 1000
+            },
+            {
+              y: 100
+            }
+          ]
+        },
+        meta: {
+          totalCoreHours: undefined
+        }
+      })
+    }).toMatchSnapshot('product action display should NOT display a total value');
+  });
+
+  it('should apply hosts inventory configuration', () => {
+    const { initialInventoryFilters: initialFilters, inventoryHostsQuery: inventoryQuery } = config;
+
+    const inventoryData = {
+      displayName: 'lorem',
+      inventoryId: 'lorem inventory id',
+      coreHours: 12.53,
+      lastSeen: 'lorem date obj',
+      loremIpsum: 'hello world'
+    };
+
+    const filteredInventoryData = parseRowCellsListData({
+      filters: initialFilters,
+      cellData: inventoryData
+    });
+
+    expect(filteredInventoryData).toMatchSnapshot('filtered');
+
+    const fallbackInventoryData = {
+      ...inventoryData,
+      coreHours: null,
+      inventoryId: null,
+      lastSeen: null
+    };
+
+    const fallbackFilteredInventoryData = parseRowCellsListData({
+      filters: initialFilters,
+      cellData: fallbackInventoryData
+    });
+
+    expect(fallbackFilteredInventoryData).toMatchSnapshot('filtered, fallback display');
+
+    expect(inventoryQuery[RHSM_API_QUERY_TYPES.DIRECTION] === SORT_DIRECTION_TYPES.DESCENDING).toBe(true);
+  });
+});
