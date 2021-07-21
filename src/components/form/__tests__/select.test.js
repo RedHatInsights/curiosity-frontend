82d7acaf3a1c3b12de79d44f9bb7a71fdbc1739d
CD Cabrera <ccabrera@redhat.com>
2021-07-21 17:23:10 -0400

    fix(select): focused unit testing (#740)

--- src/components/form/__tests__/select.test.js
+++ src/components/form/__tests__/select.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
 import { SelectVariant } from '@patternfly/react-core';
 import { FilterIcon } from '@patternfly/react-icons';
 import { Select, SelectDirection, SelectPosition } from '../select';
 
 describe('Select Component', () => {
-  it('should render a basic component', () => {
+  it('should render a basic component', async () => {
     const props = {
       id: 'test',
       options: [
@@ -14,11 +13,11 @@ describe('Select Component', () => {
       ]
     };
 
-    const component = mount(<Select {...props} />);
+    const component = await mountHookWrapper(<Select {...props} />);
     expect(component.render()).toMatchSnapshot('basic component');
   });
 
-  it('should render a checkbox select', () => {
+  it('should render a checkbox select', async () => {
     const props = {
       id: 'test',
       options: [
@@ -30,30 +29,30 @@ describe('Select Component', () => {
       placeholder: 'multiselect test'
     };
 
-    const component = mount(<Select {...props} />);
+    const component = await mountHookWrapper(<Select {...props} />);
     expect(component.render()).toMatchSnapshot('checkbox select');
   });
 
-  it('should allow alternate array and object options', () => {
+  it('should allow alternate array and object options', async () => {
     const props = {
       id: 'test',
       options: ['lorem', 'ipsum', 'hello', 'world'],
       selectedOptions: ['ipsum']
     };
 
-    const component = mount(<Select {...props} />);
+    const component = await mountHookWrapper(<Select {...props} />);
 
-    expect(component.render()).toMatchSnapshot('string array');
+    expect(component.state('options')).toMatchSnapshot('string array');
 
     component.setProps({
       options: { lorem: 'ipsum', hello: 'world' },
       selectedOptions: ['world', 'ipsum']
     });
 
-    expect(component.render()).toMatchSnapshot('key value object');
+    expect(component.state('options')).toMatchSnapshot('key value object');
   });
 
-  it('should allow plain objects as values, and be able to select options based on values within the object', () => {
+  it('should allow plain objects as values, and be able to select options based on values within the object', async () => {
     const props = {
       id: 'test',
       options: [
@@ -64,11 +63,11 @@ describe('Select Component', () => {
       selectedOptions: ['world']
     };
 
-    const component = mount(<Select {...props} />);
-    expect(component.render()).toMatchSnapshot('select when option values are objects');
+    const component = await mountHookWrapper(<Select {...props} />);
+    expect(component.state('options')).toMatchSnapshot('select when option values are objects');
   });
 
-  it('should allow selected options to match value or title', () => {
+  it('should allow selected options to match value or title', async () => {
     const props = {
       id: 'test',
       options: { lorem: 'ipsum', hello: 'world', dolor: 'set' },
@@ -76,45 +75,61 @@ describe('Select Component', () => {
       variant: SelectVariant.checkbox
     };
 
-    const component = mount(<Select {...props} />);
-    expect(component.render()).toMatchSnapshot('value or title match');
+    const component = await mountHookWrapper(<Select {...props} />);
+    expect(component.state('options')).toMatchSnapshot('value or title match');
   });
 
-  it('should return an emulated onchange event', done => {
+  it('should return an emulated onchange event', async () => {
+    const mockOnSelect = jest.fn();
     const props = {
       id: 'test',
       options: ['lorem', 'ipsum', 'hello', 'world'],
-      selectedOptions: ['ipsum']
+      selectedOptions: ['ipsum'],
+      onSelect: mockOnSelect
     };
 
-    props.onSelect = event => {
-      expect(event).toMatchSnapshot('emulated event');
-      done();
-    };
-
-    const component = mount(<Select {...props} />);
+    const component = await mountHookWrapper(<Select {...props} />);
     component.instance().onSelect({}, 'hello');
+    const {
+      currentTarget: helloCurrentTarget,
+      options: helloOptions,
+      target: helloTarget,
+      ...helloRest
+    } = mockOnSelect.mock.calls[0][0];
+    expect(mockOnSelect).toHaveBeenCalledTimes(1);
+    expect(helloRest).toMatchSnapshot('default emulated event');
 
     component.setProps({
       variant: SelectVariant.checkbox
     });
 
     component.instance().onSelect({}, 'world');
+    const {
+      currentTarget: worldCurrentTarget,
+      options: worldOptions,
+      target: worldTarget,
+      ...worldRest
+    } = mockOnSelect.mock.calls[1][0];
+    expect(mockOnSelect).toHaveBeenCalledTimes(2);
+    expect(worldRest).toMatchSnapshot('checkbox emulated event');
   });
 
-  it('should render an expanded select', () => {
+  it('should render an expanded select', async () => {
     const props = {
       id: 'test',
       options: ['lorem', 'ipsum', 'hello', 'world']
     };
 
-    const component = mount(<Select {...props} />);
-    component.find('button').simulate('click');
+    const component = await mountHookWrapper(<Select {...props} />, {
+      callback: ({ component: comp }) => {
+        comp.find('button').simulate('click');
+      }
+    });
 
-    expect(component).toMatchSnapshot('expanded');
+    expect(component.find('ul.pf-c-select__menu').find('button')).toMatchSnapshot('expanded');
   });
 
-  it('should disable toggle text', () => {
+  it('should disable toggle text', async () => {
     const props = {
       id: 'test',
       options: ['lorem', 'ipsum'],
@@ -122,31 +137,39 @@ describe('Select Component', () => {
       isToggleText: false
     };
 
-    const component = shallow(<Select {...props} />);
-    expect(component).toMatchSnapshot('disabled text');
+    const component = await shallowHookWrapper(<Select {...props} />);
+    expect(component.find('.curiosity-select-pf__no-toggle-text').props().className).toMatchSnapshot('disabled text');
   });
 
-  it('should allow alternate direction and position options', () => {
+  it('should allow alternate direction and position options', async () => {
     const props = {
       id: 'test',
       options: ['lorem', 'ipsum'],
       direction: SelectDirection.up
     };
 
-    const component = shallow(<Select {...props} />);
-    expect(component).toMatchSnapshot('direction up');
+    const component = await shallowHookWrapper(<Select {...props} />);
+    const upLeftProps = component.find('.curiosity-select-pf').props();
+    expect({
+      direction: upLeftProps.direction,
+      className: upLeftProps.className
+    }).toMatchSnapshot('direction up');
 
     component.setProps({ direction: SelectDirection.down, position: SelectPosition.right });
-    expect(component).toMatchSnapshot('position right');
+    const downRightProps = component.find('.curiosity-select-pf').props();
+    expect({
+      direction: downRightProps.direction,
+      className: downRightProps.className
+    }).toMatchSnapshot('position right');
   });
 
-  it('should allow being disabled with missing options', () => {
+  it('should allow being disabled with missing options', async () => {
     const props = {
       id: 'test',
       options: undefined
     };
 
-    const component = shallow(<Select {...props} />);
+    const component = await shallowHookWrapper(<Select {...props} />);
     expect(component).toMatchSnapshot('no options');
 
     component.setProps({
@@ -164,13 +187,13 @@ describe('Select Component', () => {
     expect(component).toMatchSnapshot('options, but disabled');
   });
 
-  it('should allow data- props', () => {
+  it('should allow data- props', async () => {
     const props = {
       'data-lorem': 'ipsum',
       'data-dolor-sit': 'dolor sit'
     };
 
-    const component = mount(<Select {...props} />);
+    const component = await mountHookWrapper(<Select {...props} />);
     expect(component.props()).toMatchSnapshot('data- attributes');
   });
 });
