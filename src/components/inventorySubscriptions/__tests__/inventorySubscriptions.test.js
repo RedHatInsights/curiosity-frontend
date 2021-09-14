cdcabrera/subscriptions-insights-frontend

fix(rhsmServices): ent-3352 api response props, mock and tests
* inventorySubscriptions, test updates
* productViewRHEL, OpenShiftContainer, Satellite, initial query, test
* rhsmServices, types, redux, response constants update

src/components/inventorySubscriptions/__tests__/inventorySubscriptions.test.js
@@ -89,10 +89,10 @@ describe('InventorySubscriptions Component', () => {
     const component = shallow(<InventorySubscriptions {...props} />);
     const componentInstance = component.instance();
 
-    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'productName' });
-    componentInstance.onColumnSort({}, { direction: SortByDirection.desc, id: 'productName' });
+    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'nextEvent' });
+    componentInstance.onColumnSort({}, { direction: SortByDirection.desc, id: 'nextEvent' });
     componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'loremIpsumBrokenOnPurpose' });
-    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'productName' });
+    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'nextEvent' });
 
     expect(mockDispatch.mock.calls).toMatchSnapshot('dispatch filter');
   });

src/config/product.rhel.js
@@ -48,7 +48,7 @@ const config = {
     [RHSM_API_QUERY_TYPES.OFFSET]: 0
   },
   inventorySubscriptionsQuery: {
-    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.UPCOMING_EVENT_DATE,
+    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.NEXT_EVENT,
     [RHSM_API_QUERY_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
     [RHSM_API_QUERY_TYPES.LIMIT]: 100,
     [RHSM_API_QUERY_TYPES.OFFSET]: 0
@@ -197,11 +197,11 @@ const config = {
       cellWidth: 15
     },
     {
-      id: 'upcomingEventDate',
+      id: 'nextEvent',
       cell: data =>
-        (data?.upcomingEventDate?.value &&
-          helpers.isDate(data?.upcomingEventDate?.value) &&
-          moment.utc(data?.upcomingEventDate?.value).format('YYYY-DD-MM')) ||
+        (data?.nextEvent?.value &&
+          helpers.isDate(data?.nextEvent?.value) &&
+          moment.utc(data?.nextEvent?.value).format('YYYY-DD-MM')) ||
         '',
       isSortable: true,
       isWrappable: true,

src/config/product.satellite.js
@@ -47,7 +47,7 @@ const config = {
     [RHSM_API_QUERY_TYPES.OFFSET]: 0
   },
   inventorySubscriptionsQuery: {
-    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.UPCOMING_EVENT_DATE,
+    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.NEXT_EVENT,
     [RHSM_API_QUERY_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
     [RHSM_API_QUERY_TYPES.LIMIT]: 100,
     [RHSM_API_QUERY_TYPES.OFFSET]: 0

src/redux/selectors/subscriptionsListSelectors.js
@@ -108,7 +108,7 @@ const selector = createDeepEqualSelector([statePropsFilter, queryFilter], (respo
     // Apply "display logic" then return a custom value for entries
     const customInventoryValue = ({ key, value }) => {
       switch (key) {
-        case rhsmApiTypes.RHSM_API_RESPONSE_INVENTORY_SUBSCRIPTIONS_DATA_TYPES.UPCOMING_EVENT_DATE:
+        case rhsmApiTypes.RHSM_API_RESPONSE_INVENTORY_SUBSCRIPTIONS_DATA_TYPES.NEXT_EVENT:
           return (value && new Date(value)) || null;
         default:
           return value ?? null;

RedHatInsights/curiosity-frontend

src/components/inventorySubscriptions/__tests__/inventorySubscriptions.test.js
@@ -89,10 +89,10 @@ describe('InventorySubscriptions Component', () => {
     const component = shallow(<InventorySubscriptions {...props} />);
     const componentInstance = component.instance();
 
-    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'productName' });
-    componentInstance.onColumnSort({}, { direction: SortByDirection.desc, id: 'productName' });
+    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'nextEventDate' });
+    componentInstance.onColumnSort({}, { direction: SortByDirection.desc, id: 'nextEventDate' });
     componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'loremIpsumBrokenOnPurpose' });
-    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'productName' });
+    componentInstance.onColumnSort({}, { direction: SortByDirection.asc, id: 'nextEventDate' });
 
     expect(mockDispatch.mock.calls).toMatchSnapshot('dispatch filter');
   });

src/config/product.rhel.js
@@ -48,7 +48,7 @@ const config = {
     [RHSM_API_QUERY_TYPES.OFFSET]: 0
   },
   inventorySubscriptionsQuery: {
-    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.UPCOMING_EVENT_DATE,
+    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.NEXT_EVENT_DATE,
     [RHSM_API_QUERY_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
     [RHSM_API_QUERY_TYPES.LIMIT]: 100,
     [RHSM_API_QUERY_TYPES.OFFSET]: 0
@@ -197,11 +197,11 @@ const config = {
       cellWidth: 15
     },
     {
-      id: 'upcomingEventDate',
+      id: 'nextEventDate',
       cell: data =>
-        (data?.upcomingEventDate?.value &&
-          helpers.isDate(data?.upcomingEventDate?.value) &&
-          moment.utc(data?.upcomingEventDate?.value).format('YYYY-DD-MM')) ||
+        (data?.nextEventDate?.value &&
+          helpers.isDate(data?.nextEventDate?.value) &&
+          moment.utc(data?.nextEventDate?.value).format('YYYY-DD-MM')) ||
         '',
       isSortable: true,
       isWrappable: true,

src/config/product.satellite.js
@@ -47,7 +47,7 @@ const config = {
     [RHSM_API_QUERY_TYPES.OFFSET]: 0
   },
   inventorySubscriptionsQuery: {
-    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.UPCOMING_EVENT_DATE,
+    [RHSM_API_QUERY_TYPES.SORT]: RHSM_API_QUERY_SUBSCRIPTIONS_SORT_TYPES.NEXT_EVENT_DATE,
     [RHSM_API_QUERY_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
     [RHSM_API_QUERY_TYPES.LIMIT]: 100,
     [RHSM_API_QUERY_TYPES.OFFSET]: 0

src/redux/selectors/subscriptionsListSelectors.js
@@ -108,7 +108,7 @@ const selector = createDeepEqualSelector([statePropsFilter, queryFilter], (respo
     // Apply "display logic" then return a custom value for entries
     const customInventoryValue = ({ key, value }) => {
       switch (key) {
-        case rhsmApiTypes.RHSM_API_RESPONSE_INVENTORY_SUBSCRIPTIONS_DATA_TYPES.UPCOMING_EVENT_DATE:
+        case rhsmApiTypes.RHSM_API_RESPONSE_INVENTORY_SUBSCRIPTIONS_DATA_TYPES.NEXT_EVENT_DATE:
           return (value && new Date(value)) || null;
         default:
           return value ?? null;