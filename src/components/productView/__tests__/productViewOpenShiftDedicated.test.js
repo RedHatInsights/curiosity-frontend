84a3000399f2662732d767fca8d340ec37e724bd
CD Cabrera <ccabrera@redhat.com>
2021-10-22 02:54:13 -0400

    refactor(toolbar): ent-4208 activate product hooks (#820)
    * locale, key consistency
    * productViews, toolbar props, related tests
    * toolbar, refactor, move functionality into fields
    * toolbarFieldDisplayName, hooks, strings
    * toolbarFieldGranularity, hooks, strings
    * toolbarFieldRangedMonthly, hooks, strings
    * toolbarFieldUom, hooks, strings
    * toolbarFieldUsage, hooks, strings
    * toolbarHelpers, removed

--- src/components/productView/__tests__/productViewOpenShiftDedicated.test.js
+++ src/components/productView/__tests__/productViewOpenShiftDedicated.test.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import { ProductViewOpenShiftDedicated } from '../productViewOpenShiftDedicated';
 import { ToolbarFieldRangedMonthly } from '../../toolbar/toolbarFieldRangedMonthly';
-import { config } from '../../../config/product.openshiftDedicated';
 
 describe('ProductViewOpenShiftDedicated Component', () => {
   it('should render a basic component', async () => {
-    const props = {
-      useRouteDetail: () => ({
-        pathParameter: 'lorem ipsum',
-        productConfig: [config],
-        productParameter: 'lorem ipsum product label',
-        viewParameter: 'dolor sit'
-      })
-    };
+    const props = {};
     const component = await shallowHookComponent(<ProductViewOpenShiftDedicated {...props} />);
     expect(component).toMatchSnapshot('basic');
   });
 
   it('should be a custom product view', async () => {
-    const props = {
-      useRouteDetail: () => ({
-        pathParameter: 'lorem ipsum',
-        productConfig: [config],
-        productParameter: 'lorem ipsum product label',
-        viewParameter: 'dolor sit'
-      })
-    };
+    const props = {};
     const component = await shallowHookComponent(<ProductViewOpenShiftDedicated {...props} />);
-    const { toolbarGraph, toolbarGraphDescription, toolbarProduct } = component.props();
+    const { toolbarGraph, toolbarGraphDescription } = component.props();
 
     expect(toolbarGraph.type === ToolbarFieldRangedMonthly).toBe(true);
     expect(toolbarGraphDescription).toBe(true);
-    expect(toolbarProduct).toBe(false);
   });
 });
--- src/components/toolbar/__tests__/toolbarFieldDisplayName.test.js
+++ src/components/toolbar/__tests__/toolbarFieldDisplayName.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import { ToolbarFieldDisplayName } from '../toolbarFieldDisplayName';
 import { store } from '../../../redux/store';
 import { TextInput } from '../../form/textInput';
 import { helpers } from '../../../common/helpers';
+import { RHSM_API_QUERY_TYPES } from '../../../types/rhsmApiTypes';
 
 describe('ToolbarFieldDisplayName Component', () => {
   let mockDispatch;
@@ -16,16 +17,20 @@ describe('ToolbarFieldDisplayName Component', () => {
     jest.clearAllMocks();
   });
 
-  it('should render a non-connected component', () => {
-    const props = {};
+  it('should render a basic component', () => {
+    const props = {
+      useProduct: () => ({ viewId: 'loremIpsum' }),
+      useProductInventoryHostsQuery: () => ({ [RHSM_API_QUERY_TYPES.DISPLAY_NAME]: 'lorem ipsum' })
+    };
     const component = shallow(<ToolbarFieldDisplayName {...props} />);
 
-    expect(component).toMatchSnapshot('non-connected');
+    expect(component).toMatchSnapshot('basic');
   });
 
   it('should handle updating display name through redux state', () => {
     const props = {
-      value: 'lorem ipsum'
+      useProduct: () => ({ viewId: 'loremIpsum' }),
+      useProductInventoryHostsQuery: () => ({ [RHSM_API_QUERY_TYPES.DISPLAY_NAME]: 'lorem ipsum' })
     };
 
     const component = shallow(<ToolbarFieldDisplayName {...props} />);
@@ -35,24 +40,26 @@ describe('ToolbarFieldDisplayName Component', () => {
     expect(mockDispatch.mock.calls).toMatchSnapshot('dispatch display name');
   });
 
-  it('should handle updating display name using the enter key', () => {
+  it('should handle updating display name using the enter key', async () => {
     const props = {
-      value: 'lorem ipsum'
+      useProduct: () => ({ viewId: 'loremIpsum' }),
+      useProductInventoryHostsQuery: () => ({ [RHSM_API_QUERY_TYPES.DISPLAY_NAME]: 'lorem ipsum' })
     };
 
-    const component = mount(<ToolbarFieldDisplayName {...props} />);
+    const component = await mountHookComponent(<ToolbarFieldDisplayName {...props} />);
     const mockEvent = { keyCode: 13, currentTarget: { value: 'dolor sit' }, persist: helpers.noop };
     component.find(TextInput).instance().onKeyUp(mockEvent);
 
     expect(mockDispatch.mock.calls).toMatchSnapshot('enter display name');
   });
 
-  it('should handle clearing display name through redux state', () => {
+  it('should handle clearing display name through redux state', async () => {
     const props = {
-      value: 'lorem ipsum'
+      useProduct: () => ({ viewId: 'loremIpsum' }),
+      useProductInventoryHostsQuery: () => ({ [RHSM_API_QUERY_TYPES.DISPLAY_NAME]: 'lorem ipsum' })
     };
 
-    const component = mount(<ToolbarFieldDisplayName {...props} />);
+    const component = await mountHookComponent(<ToolbarFieldDisplayName {...props} />);
     const mockEvent = { keyCode: 27, currentTarget: { value: '' }, persist: helpers.noop };
     component.find(TextInput).instance().onKeyUp(mockEvent);
 
