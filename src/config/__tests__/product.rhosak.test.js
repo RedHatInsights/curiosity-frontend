a8f31d37a0d7caa6445c4645a639cf8052227a76
CD Cabrera <ccabrera@redhat.com>
2021-11-10 17:37:12 -0500

    fix(graphCardHelpers): ent-4510 allow product custom axes (#829)

--- src/config/__tests__/product.rhosak.test.js
+++ src/config/__tests__/product.rhosak.test.js
@@ -8,4 +8,20 @@ describe('Product RHOSAK config', () => {
     expect(generateChartSettings(initialGraphFilters)).toMatchSnapshot('filters');
     expect(initialGraphSettings).toMatchSnapshot('settings');
   });
+
+  it('should handle a custom yAxisTickFormat for floating points', () => {
+    const generateTicks = (method = config.initialGraphSettings.yAxisTickFormat) => {
+      const ticks = {};
+      for (let i = 0.00001345; i < 13; i++) {
+        const multiplier = i < 1 ? i : Math.pow(10, i);
+        for (let k = 1; k < 16; k++) {
+          const incrementMultiplier = k * multiplier;
+          ticks[incrementMultiplier] = method({ tick: incrementMultiplier });
+        }
+      }
+      return ticks;
+    };
+
+    expect(generateTicks()).toMatchSnapshot('yAxisTickFormat');
+  });
 });
