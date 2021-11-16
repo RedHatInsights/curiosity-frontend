ede82fda9bb0ae8514422bb6e047bae777856ee9
CD Cabrera <ccabrera@redhat.com>
2021-11-16 10:09:24 -0500

    DEV expand unit test for transform

--- src/services/rhsm/__tests__/rhsmTransformers.test.js
+++ src/services/rhsm/__tests__/rhsmTransformers.test.js
@@ -1,4 +1,5 @@
 import { rhsmTranformers } from '../rhsmTranformers';
+import { rhsmConstants } from '../rhsmConstants';
 
 describe('RHSM Transformers', () => {
   it('should have specific RHSM response transformers', () => {
@@ -6,7 +7,18 @@ describe('RHSM Transformers', () => {
   });
 
   it('should attempt to parse an instances response', () => {
-    expect(rhsmTranformers.instances()).toMatchSnapshot('instances');
+    expect(
+      rhsmTranformers.instances({
+        [rhsmConstants.RHSM_API_RESPONSE_DATA]: [
+          {
+            [rhsmConstants.RHSM_API_RESPONSE_INSTANCES_DATA_TYPES.MEASUREMENTS]: [1000, 0.0003456, 2]
+          }
+        ],
+        [rhsmConstants.RHSM_API_RESPONSE_META]: {
+          [rhsmConstants.RHSM_API_RESPONSE_INSTANCES_META_TYPES.MEASUREMENTS]: ['c', 'a', 'b']
+        }
+      })
+    ).toMatchSnapshot('instances');
   });
 
   it('should attempt to parse a tally response', () => {
