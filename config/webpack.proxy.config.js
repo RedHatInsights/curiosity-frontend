fb484c9897fa2f85eba0ec881bb41718267b5b73
CD Cabrera <ccabrera@redhat.com>
2022-02-03 09:53:33 -0500

    build(dev): local run, content security policy update (#880)

--- config/webpack.proxy.config.js
+++ config/webpack.proxy.config.js
@@ -1,7 +1,7 @@
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { setHtmlPlugin, setReplacePlugin, setCommonPlugins } = require('./build.plugins');
 const { setupDotenvFilesForEnv } = require('./build.dotenv');
-const setProxyRoutes = require('./spandx.config');
+const { setProxyRoutes } = require('./spandx.config');
 
 const { _BUILD_RELATIVE_DIRNAME, DEV_BRANCH, DEV_PORT } = setupDotenvFilesForEnv({
   env: 'proxy'
