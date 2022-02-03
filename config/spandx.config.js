27a233e8c2c31774060f2350ee0fe016a8ad3748
CD Cabrera <ccabrera@redhat.com>
2022-02-03 09:53:33 -0500

    build(dev): local run, content security policy update (#880)

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -1,12 +1,25 @@
 /**
- * Set proxy routes.
+ * Set dev run routes.
+ *
+ * @returns {object}
+ */
+const setDevRoutes = () => ({
+  routes: {
+    '/api/rhsm-subscriptions': {
+      host: `http://localhost:5000`
+    }
+  }
+});
+
+/**
+ * Set proxy run routes.
  *
  * @param {object} params
  * @param {string} params.DEV_PORT
  * @param {string} params.BETA_PREFIX
  * @returns {object}
  */
-const setProxyRoutes = ({ DEV_PORT, BETA_PREFIX = '' }) => ({
+const setProxyRoutes = ({ DEV_PORT, BETA_PREFIX = '' } = {}) => ({
   routes: {
     '/locales': {
       host: `https://localhost:${DEV_PORT}${BETA_PREFIX}/apps/subscriptions`
@@ -14,4 +27,4 @@ const setProxyRoutes = ({ DEV_PORT, BETA_PREFIX = '' }) => ({
   }
 });
 
-module.exports = setProxyRoutes;
+module.exports = { setDevRoutes, setProxyRoutes };
--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,6 +1,7 @@
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { setHtmlPlugin, setReplacePlugin, setCommonPlugins } = require('./build.plugins');
 const { setupDotenvFilesForEnv } = require('./build.dotenv');
+const { setDevRoutes } = require('./spandx.config');
 
 const { _BUILD_RELATIVE_DIRNAME, DEV_BRANCH, DEV_PORT } = setupDotenvFilesForEnv({ env: process.env.NODE_ENV });
 
@@ -12,6 +13,7 @@ const { config: webpackConfig, plugins } = config({
   env: (/(prod|stage|qa|ci)(-stable|-beta)$/.test(DEV_BRANCH) && DEV_BRANCH) || 'prod-stable',
   port: Number.parseInt(DEV_PORT, 10),
   rootFolder: _BUILD_RELATIVE_DIRNAME,
+  routes: setDevRoutes(),
   standalone: true,
   useProxy: false,
   htmlPlugin: setHtmlPlugin(),
