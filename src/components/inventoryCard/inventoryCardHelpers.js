a3a242ed49eea52fbf91ce7546b105ea4003a703
CD Cabrera <ccabrera@redhat.com>
2022-02-17 09:57:31 -0500

    feat(inventoryCardHelpers): ent-4689 custom tooltip config (#884)
    * inventoryCardHelpers, restructure, allow tooltips
    * tooltip, pf tooltip wrapper, styling

--- src/components/inventoryCard/inventoryCardHelpers.js
+++ src/components/inventoryCard/inventoryCardHelpers.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { cellWidth as PfCellWidth, SortByDirection, wrappable } from '@patternfly/react-table';
 import _camelCase from 'lodash/camelCase';
+import _isPlainObject from 'lodash/isPlainObject';
+import { Tooltip } from '../tooltip/tooltip';
 import { translate } from '../i18n/i18n';
 import {
   RHSM_API_QUERY_SORT_DIRECTION_TYPES as SORT_DIRECTION_TYPES,
@@ -9,16 +11,150 @@ import {
 import { helpers } from '../../common';
 
 /**
- * Apply sort filter to filters.
+ * ToDo: review setting up a transformed cell cache for already transformed cells.
+ * - review using a simple state and key memoized component
+ * - review using lru cache in a inventoryCardContext custom hook
+ */
+
+/**
+ * Apply product inventory config properties consistently.
+ *
+ * @param {Function|string|number} prop
+ * @param {object} options
+ * @param {*[]|*} options.params
+ * @returns {React.ReactNode}
+ */
+const applyConfigProperty = (prop, { params = [] } = {}) => {
+  let updatedProp = prop;
+
+  if (typeof prop === 'function') {
+    updatedProp = prop(...((Array.isArray(params) && params) || [params]));
+  }
+
+  if (typeof updatedProp === 'string' || typeof updatedProp === 'number' || React.isValidElement(updatedProp)) {
+    return updatedProp;
+  }
+
+  return undefined;
+};
+
+/**
+ * Generate header and row cell configuration from filters.
+ *
+ * @param {Array<{id: string, cell:React.ReactNode|{ title: string }, cellWidth: number,
+ *     header:React.ReactNode|{ title: string }, onSort: Function, showEmptyCell: boolean,
+ *     sortId: string, sortActive: boolean, sortDirection: string,
+ *     transforms: Array}>} filters
+ * @param {object} cellData
+ * @param {object} session
+ * @returns {{bodyCells: { title: React.ReactNode }[], headerCells: { title: React.ReactNode }[]}}
+ */
+const applyHeaderRowCellFilters = (filters = [], cellData = {}, session = {}) => {
+  const headerCells = [];
+  const bodyCells = [];
+
+  filters.forEach(
+    ({ id, cell, cellWidth, header, onSort, showEmptyCell = true, sortId, sortActive, sortDirection, transforms }) => {
+      const headerCellUpdated = { title: translate('curiosity-inventory.header', { context: id }), transforms: [] };
+      const bodyCellUpdated = { title: '' };
+
+      // set filtered base header and body cells, or if filter doesn't exist skip
+      if (cellData[id]) {
+        headerCellUpdated.title = cellData[id]?.title ?? id;
+        bodyCellUpdated.title = cellData[id]?.value ?? '';
+      } else {
+        if (helpers.DEV_MODE || helpers.REVIEW_MODE) {
+          console.warn(`Warning: Filter "${id}" not found in "table row" response data.`, cellData);
+        }
+        if (showEmptyCell === false) {
+          return;
+        }
+      }
+
+      // set header cell title
+      if (header) {
+        const updatedHeaderCellTitle = applyConfigProperty(header, { params: [{ ...cellData }, { ...session }] });
+        if (updatedHeaderCellTitle) {
+          headerCellUpdated.title = updatedHeaderCellTitle;
+        } else if (_isPlainObject(header)) {
+          Object.assign(headerCellUpdated, { ...header });
+        }
+
+        // set header cell tooltip
+        if (header.tooltip && headerCellUpdated.title) {
+          const updatedHeaderCellTooltip = applyConfigProperty(header.tooltip, {
+            params: [{ ...cellData }, { ...session }]
+          });
+          if (updatedHeaderCellTooltip) {
+            headerCellUpdated.title = <Tooltip content={updatedHeaderCellTooltip}>{headerCellUpdated.title}</Tooltip>;
+          }
+
+          delete headerCellUpdated.tooltip;
+        }
+      }
+
+      // set header cell transforms
+      if (Array.isArray(headerCellUpdated.transforms)) {
+        if (Array.isArray(transforms)) {
+          headerCellUpdated.transforms = headerCellUpdated.transforms.concat([...transforms]);
+        }
+
+        if (typeof cellWidth === 'number') {
+          headerCellUpdated.transforms.push(PfCellWidth(cellWidth));
+        }
+      }
+
+      // set header cell onSort
+      if (typeof onSort === 'function') {
+        headerCellUpdated.onSort = obj => onSort({ ...cellData }, { ...obj, id: sortId || id });
+        headerCellUpdated.sortActive = sortActive;
+        headerCellUpdated.sortDirection = sortDirection;
+      }
+
+      // set body cell title
+      if (cell) {
+        const updatedBodyCellTitle = applyConfigProperty(cell, { params: [{ ...cellData }, { ...session }] });
+        if (updatedBodyCellTitle) {
+          bodyCellUpdated.title = updatedBodyCellTitle;
+        } else if (_isPlainObject(cell)) {
+          Object.assign(bodyCellUpdated, { ...cell });
+        }
+
+        // set body cell tooltip
+        if (cell.tooltip && bodyCellUpdated.title) {
+          const updatedBodyCellTooltip = applyConfigProperty(cell.tooltip, {
+            params: [{ ...cellData }, { ...session }]
+          });
+          if (updatedBodyCellTooltip) {
+            bodyCellUpdated.title = <Tooltip content={updatedBodyCellTooltip}>{bodyCellUpdated.title}</Tooltip>;
+          }
+
+          delete bodyCellUpdated.tooltip;
+        }
+      }
+
+      headerCells.push(headerCellUpdated);
+      bodyCells.push(bodyCellUpdated);
+    }
+  );
+
+  return {
+    headerCells,
+    bodyCells
+  };
+};
+
+/**
+ * Shallow clone filter, and apply a column sort filter.
  *
  * @param {object} params
- * @param {{ onSort: Function, sortActive: boolean, sortDirection: string, isSortDefault: boolean,
- *     sortDefaultInitialDirection: string }} params.filter
+ * @param {{onSort: Function, sortActive: boolean, sortDirection: string, isSortDefault: boolean,
+ *     sortDefaultInitialDirection: string}} params.filter
  * @param {Function} params.onSort
  * @param {object} params.query
- * @returns {object}
+ * @returns {{}}
  */
-const applySortFilters = ({ filter = {}, onSort, query = {} }) => {
+const applySortFilters = ({ filter = {}, onSort, query = {} } = {}) => {
   const { id, sortId } = filter;
   const updatedId = sortId || id;
   const updatedFilter = { ...filter };
@@ -66,7 +202,14 @@ const applySortFilters = ({ filter = {}, onSort, query = {} }) => {
   return updatedFilter;
 };
 
-const applyWrappableFilters = ({ filter = {} }) => {
+/**
+ * Shallow clone and apply a consistent PF "wrappable" transformation config allowing column content to wrap.
+ *
+ * @param {object} params
+ * @param {object} params.filter
+ * @returns {{}}
+ */
+const applyWrappableFilters = ({ filter = {} } = {}) => {
   const updatedFilter = { ...filter };
 
   if (Array.isArray(updatedFilter.transforms)) {
@@ -79,15 +222,18 @@ const applyWrappableFilters = ({ filter = {} }) => {
 };
 
 /**
- * Apply additional properties to filters.
+ * Shallow clone and apply, sequence specific, additional properties to filters.
  *
  * @param {object} params
- * @param {Array} params.filters
+ * @param {Array<{id: string, cell:*, cellWidth: number, header:*, onSort: Function,
+ *     showEmptyCell: boolean, sortId: string, sortActive: boolean,
+ *     sortDirection: string, transforms: Array, isSortDefault: boolean,
+ *     sortDefaultInitialDirection: string}>} params.filters
  * @param {Function} params.onSort
  * @param {object} params.query
- * @returns {Array}
+ * @returns {*[]}
  */
-const parseInventoryFilters = ({ filters = [], onSort, query = {} }) =>
+const parseInventoryFilters = ({ filters = [], onSort, query = {} } = {}) =>
   [...filters].map(filter => {
     const updatedFilter = { ...filter };
 
@@ -106,105 +252,39 @@ const parseInventoryFilters = ({ filters = [], onSort, query = {} }) =>
  * Parse and return formatted/filtered table cells, and apply table filters.
  *
  * @param {object} params
- * @param {Array} params.filters
+ * @param {Array<{id: string, cell:React.ReactNode|{ title: string }, cellWidth: number,
+ *     header:React.ReactNode|{ title: string }, onSort: Function, showEmptyCell: boolean,
+ *     sortId: string, sortActive: boolean, sortDirection: string,
+ *     transforms: Array}>} params.filters
  * @param {object} params.cellData
  * @param {object} params.session
- * @returns {{columnHeaders: Array, cells: Array, data: object}}
+ * @returns {{columnHeaders: { title: React.ReactNode }[], cells: { title: React.ReactNode }[], data: {}}}
  */
-const parseRowCellsListData = ({ filters = [], cellData = {}, session = {} }) => {
+const parseRowCellsListData = ({ filters = [], cellData = {}, session = {} } = {}) => {
   const updatedColumnHeaders = [];
   const updatedCells = [];
   const allCells = {};
 
   // Apply basic translation and value
-  Object.entries(cellData).forEach(([key, value]) => {
+  Object.entries(cellData).forEach(([key, value = '']) => {
     allCells[key] = {
       title: translate('curiosity-inventory.header', { context: key }),
       value
     };
 
     updatedColumnHeaders.push(allCells[key].title);
-    updatedCells.push(value);
+    updatedCells.push(value || '...');
   });
 
   // Apply filters to header and cell values
-  if (filters?.length) {
+  if (filters?.length && Object.keys(allCells).length) {
     updatedColumnHeaders.length = 0;
     updatedCells.length = 0;
 
-    filters.forEach(({ id, cell, cellWidth, header, onSort, sortId, sortActive, sortDirection, transforms }) => {
-      let headerUpdated;
-      let cellUpdated;
+    const { headerCells = [], bodyCells = [] } = applyHeaderRowCellFilters(filters, allCells, session);
 
-      if (allCells[id]) {
-        headerUpdated = allCells[id]?.title ?? id;
-        cellUpdated = allCells[id]?.value ?? '';
-      } else if (id) {
-        headerUpdated = translate('curiosity-inventory.header', { context: id });
-        cellUpdated = '';
-      }
-
-      // set table header cell filter params
-      if (header) {
-        headerUpdated = (typeof header === 'function' && header({ ...allCells })) || header;
-      }
-
-      if (
-        typeof headerUpdated === 'string' ||
-        typeof headerUpdated === 'number' ||
-        React.isValidElement(headerUpdated)
-      ) {
-        headerUpdated = {
-          title: headerUpdated
-        };
-      }
-
-      if (headerUpdated) {
-        headerUpdated.transforms = [];
-
-        if (Array.isArray(transforms)) {
-          headerUpdated.transforms = headerUpdated.transforms.concat([...transforms]);
-        }
-
-        if (typeof cellWidth === 'number') {
-          headerUpdated.transforms.push(PfCellWidth(cellWidth));
-        }
-      }
-
-      if (typeof onSort === 'function') {
-        headerUpdated = {
-          ...headerUpdated,
-          onSort: obj => onSort({ ...allCells }, { ...obj, id: sortId || id }),
-          sortActive,
-          sortDirection
-        };
-      }
-
-      // set table row cell filter params
-      if (cell) {
-        cellUpdated = typeof cell === 'function' ? cell({ ...allCells }, { ...session }) : cell;
-      }
-
-      if (typeof cellUpdated === 'string' || typeof cellUpdated === 'number' || React.isValidElement(cellUpdated)) {
-        cellUpdated = {
-          title: cellUpdated
-        };
-      } else if (!cellUpdated?.title) {
-        if (helpers.DEV_MODE || helpers.REVIEW_MODE) {
-          console.error(
-            `PF table throws an error when cell values don't conform to what it is expecting, or align exactly to column headers.
-            \n\nSee cell ID=${id} with VALUE=${cellUpdated}`
-          );
-        }
-
-        cellUpdated = {
-          title: ''
-        };
-      }
-
-      updatedColumnHeaders.push(headerUpdated);
-      updatedCells.push(cellUpdated);
-    });
+    updatedColumnHeaders.push(...headerCells);
+    updatedCells.push(...bodyCells);
   }
 
   return {
@@ -215,6 +295,8 @@ const parseRowCellsListData = ({ filters = [], cellData = {}, session = {} }) =>
 };
 
 const inventoryCardHelpers = {
+  applyConfigProperty,
+  applyHeaderRowCellFilters,
   applySortFilters,
   applyWrappableFilters,
   parseInventoryFilters,
@@ -224,6 +306,8 @@ const inventoryCardHelpers = {
 export {
   inventoryCardHelpers as default,
   inventoryCardHelpers,
+  applyConfigProperty,
+  applyHeaderRowCellFilters,
   applySortFilters,
   applyWrappableFilters,
   parseInventoryFilters,
