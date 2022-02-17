4ea4ac351aa3f8a19cf5d8fbd74dc881b601c56c
CD Cabrera <ccabrera@redhat.com>
2022-02-17 16:17:12 -0500

    feat(toolbarFieldBillingProvider): ent-4689 select, services (#893)
    * locale, placeholder, category strings
    * productViewContext, add secondaryFilters for toolbar config
    * toolbar, secondaryFields, mod for consuming option component
    * toolbarContext, hook for applying secondary fields
    * toolbarFieldBillingProvider, select component
    * toolbarFieldSelectCategory, toolbarFieldOptions add provider
    * redux, viewReducer, queryTypes,  add billing provider query
    * services, rhsmConstants, billing provider adds

--- src/components/toolbar/__tests__/toolbarContext.test.js
+++ src/components/toolbar/__tests__/toolbarContext.test.js
@@ -1,6 +1,12 @@
-import { context, useToolbarFieldClear, useToolbarFieldClearAll, useToolbarFieldQueries } from '../toolbarContext';
+import {
+  context,
+  useToolbarFieldClear,
+  useToolbarFieldClearAll,
+  useToolbarFieldQueries,
+  useToolbarSecondaryFields
+} from '../toolbarContext';
 import { store } from '../../../redux/store';
-import { RHSM_API_QUERY_TYPES } from '../../../types/rhsmApiTypes';
+import { RHSM_API_QUERY_SET_TYPES as RHSM_API_QUERY_TYPES } from '../../../services/rhsm/rhsmConstants';
 
 describe('ToolbarContext', () => {
   let mockDispatch;
@@ -25,6 +31,8 @@ describe('ToolbarContext', () => {
 
     onClearField(RHSM_API_QUERY_TYPES.USAGE);
 
+    onClearField(RHSM_API_QUERY_TYPES.BILLING_PROVIDER);
+
     expect(mockDispatch.mock.calls).toMatchSnapshot('clear single field');
   });
 
@@ -33,6 +41,7 @@ describe('ToolbarContext', () => {
       useToolbarFieldClearAll({
         useProductQuery: () => ({
           lorem: 'ipsum',
+          [RHSM_API_QUERY_TYPES.BILLING_PROVIDER]: 'testBillingProvider',
           [RHSM_API_QUERY_TYPES.SLA]: 'testSla',
           [RHSM_API_QUERY_TYPES.USAGE]: 'testUsage',
           [RHSM_API_QUERY_TYPES.UOM]: 'testUom'
@@ -50,10 +59,30 @@ describe('ToolbarContext', () => {
   it('should apply a hook for retrieving api queries specific to toolbar', () => {
     const { result: productQuery } = shallowHook(() =>
       useToolbarFieldQueries({
-        useProductQuery: () => ({ lorem: 'ipsum', [RHSM_API_QUERY_TYPES.SLA]: 'testSla' }),
+        useProductQuery: () => ({
+          lorem: 'ipsum',
+          [RHSM_API_QUERY_TYPES.SLA]: 'testSla',
+          [RHSM_API_QUERY_TYPES.USAGE]: 'testUsage',
+          [RHSM_API_QUERY_TYPES.BILLING_PROVIDER]: 'testProvider'
+        }),
         useProductGraphTallyQuery: () => ({ dolor: 'sit', [RHSM_API_QUERY_TYPES.GRANULARITY]: 'testGranularity' })
       })
     );
     expect(productQuery).toMatchSnapshot('query');
+  });
+
+  it('should apply a hook for retrieving secondary toolbar field components', () => {
+    const { result: secondaryFields } = shallowHook(() =>
+      useToolbarSecondaryFields({
+        useProductToolbarConfig: () => ({
+          secondaryFilters: [
+            {
+              id: 'rangedMonthly'
+            }
+          ]
+        })
+      })
+    );
+    expect(secondaryFields).toMatchSnapshot('secondary field, rangedMonthly');
   });
 });
