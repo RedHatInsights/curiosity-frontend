fef62d1402b554e84191154c9e78ef5f3b7300c0
CD Cabrera <ccabrera@redhat.com>
2022-03-25 15:47:00 -0400

    fix(build): npm updates (#912)
    * build, move npmCheckUpdates to sh
    * docs, schedule for deps updates

---
+++ scripts/dependencies.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+#
+# main()
+#
+{
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  NOCOLOR="\e[39m"
+
+  echo "Confirm and update build dependencies..."
+
+  DEPS_UPDATE=$(ncu "$@");
+  echo "$DEPS_UPDATE"
+
+  DEPS_LOG=$(ncu);
+  echo $DEPS_LOG
+
+  printf "${YELLOW}Generate dependency log...${NOCOLOR}"
+  echo "Curiosity dependency update log" > ./dependency-update-log.txt
+  echo "$DEPS_UPDATE" >> ./dependency-update-log.txt
+  echo "$DEPS_LOG" >> ./dependency-update-log.txt
+  printf "${GREEN}COMPLETED${NOCOLOR}\n"
+  printf "${YELLOW}Log generated at ./dependency-update-log.txt${NOCOLOR}\n"
+}
