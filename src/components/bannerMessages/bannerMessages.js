144b743fef3adce5d6b54e19b32ba8b0e977ff5c
CD Cabrera <ccabrera@redhat.com>
2022-04-07 23:06:03 -0400

    refactor(bannerMessages): ent-4899 align hook components

--- src/components/bannerMessages/bannerMessages.js
+++ src/components/bannerMessages/bannerMessages.js
@@ -3,42 +3,22 @@ import PropTypes from 'prop-types';
 import { Alert, AlertActionCloseButton, AlertVariant, Button } from '@patternfly/react-core';
 import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 import { useShallowCompareEffect } from 'react-use';
-import { apiQueries, storeHooks } from '../../redux';
+import { useGetAppMessages } from './bannerMessagesContext';
+import { helpers } from '../../common';
 import { translate } from '../i18n/i18n';
-import { dateHelpers, helpers } from '../../common';
-import { RHSM_API_QUERY_GRANULARITY_TYPES as GRANULARITY_TYPES, RHSM_API_QUERY_TYPES } from '../../types/rhsmApiTypes';
-import { useRouteDetail } from '../../hooks/useRouter';
 
 /**
  * Render banner messages.
  *
  * @param {object} props
  * @param {Array} props.messages
- * @param {Function} props.useRouteDetail
- * @param {Function} props.useAppMessages
+ * @param {Function} props.useGetAppMessages
  * @returns {Node}
  */
-const BannerMessages = ({ messages, useRouteDetail: useAliasRouteDetail, useAppMessages: useAliasAppMessages }) => {
+const BannerMessages = ({ messages, useGetAppMessages: useAliasGetAppMessages }) => {
   const [hideAlerts, setHideAlerts] = useState({});
   const [alerts, setAlerts] = useState([]);
-  const { pathParameter: productId, productConfig } = useAliasRouteDetail() || {};
-  const isProductConfig = productConfig?.length === 1 && productConfig?.[0];
-  const { query } = apiQueries.parseRhsmQuery(productConfig?.[0]?.query || {});
-  const { appMessages } = useAliasAppMessages();
-
-  useShallowCompareEffect(() => {
-    if (productId && isProductConfig) {
-      const { startDate, endDate } = dateHelpers.getRangedDateTime('CURRENT');
-      const updatedGraphQuery = {
-        ...query,
-        [RHSM_API_QUERY_TYPES.GRANULARITY]: GRANULARITY_TYPES.DAILY,
-        [RHSM_API_QUERY_TYPES.START_DATE]: startDate.toISOString(),
-        [RHSM_API_QUERY_TYPES.END_DATE]: endDate.toISOString()
-      };
-
-      storeHooks.rhsmActions.useGetMessageReports(productId, updatedGraphQuery);
-    }
-  }, [productId, isProductConfig, query]);
+  const { data: appMessages } = useAliasGetAppMessages();
 
   useShallowCompareEffect(() => {
     const updatedMessages = [];
@@ -81,7 +61,7 @@ const BannerMessages = ({ messages, useRouteDetail: useAliasRouteDetail, useAppM
 /**
  * Prop types.
  *
- * @type {{useAppMessages: Function, messages: Array, useRouteDetail: Function}}
+ * @type {{useGetAppMessages: Function, messages: Array}}
  */
 BannerMessages.propTypes = {
   messages: PropTypes.arrayOf(
@@ -92,14 +72,13 @@ BannerMessages.propTypes = {
       variant: PropTypes.oneOf([...Object.values(AlertVariant)])
     })
   ),
-  useAppMessages: PropTypes.func,
-  useRouteDetail: PropTypes.func
+  useGetAppMessages: PropTypes.func
 };
 
 /**
  * Default props.
  *
- * @type {{useAppMessages: Function, messages: Array, useRouteDetail: Function}}
+ * @type {{useGetAppMessages: Function, messages: Array}}
  */
 BannerMessages.defaultProps = {
   messages: [
@@ -126,8 +105,7 @@ BannerMessages.defaultProps = {
       )
     }
   ],
-  useAppMessages: storeHooks.rhsmSelectors.useAppMessages,
-  useRouteDetail
+  useGetAppMessages
 };
 
 export { BannerMessages as default, BannerMessages };
