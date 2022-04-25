94a12d52ee623bf2b8d20e4a9c36da93d5327419
CD Cabrera <ccabrera@redhat.com>
2022-04-25 13:54:54 -0400

    refactor(authentication): ent-4899 align hook components (#921)
    * authContext, bundle api calls, logic into single hook

--- src/components/authentication/authentication.js
+++ src/components/authentication/authentication.js
@@ -1,70 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { BinocularsIcon } from '@patternfly/react-icons';
 import { Maintenance } from '@redhat-cloud-services/frontend-components/Maintenance';
 import { NotAuthorized } from '@redhat-cloud-services/frontend-components/NotAuthorized';
-import { useMount, useUnmount } from 'react-use';
-import { reduxActions, storeHooks } from '../../redux';
-import { routerHooks } from '../../hooks/useRouter';
 import { routerHelpers, Redirect } from '../router';
 import { rhsmConstants } from '../../services/rhsm/rhsmConstants';
 import { helpers } from '../../common';
 import MessageView from '../messageView/messageView';
 import { translate } from '../i18n/i18n';
-import { AuthenticationContext, useAuth } from './authenticationContext';
+import { AuthenticationContext, useGetAuthorization } from './authenticationContext';
 
 /**
  * An authentication pass-through component.
  *
  * @param {object} props
  * @param {string} props.appName
- * @param {Function} props.authorizeUser
  * @param {React.ReactNode} props.children
- * @param {Function} props.hideGlobalFilter
- * @param {Function} props.initializeChrome
  * @param {boolean} props.isDisabled
- * @param {Function} props.onNavigation
- * @param {Function} props.setAppName
  * @param {Function} props.t
- * @param {Function} props.useAuth
- * @param {Function} props.useDispatch
- * @param {Function} props.useHistory
+ * @param {Function} props.useGetAuthorization
  * @returns {React.ReactNode}
  */
-const Authentication = ({
-  appName,
-  authorizeUser,
-  children,
-  hideGlobalFilter,
-  initializeChrome,
-  isDisabled,
-  onNavigation,
-  setAppName,
-  t,
-  useAuth: useAliasAuth,
-  useDispatch: useAliasDispatch,
-  useHistory: useAliasHistory
-}) => {
-  const [unregister, setUnregister] = useState(() => helpers.noop);
-  const dispatch = useAliasDispatch();
-  const history = useAliasHistory();
-
-  const { pending, data: authData = {} } = useAliasAuth();
-  const { authorized = {}, errorCodes, errorStatus } = authData;
+const Authentication = ({ appName, children, isDisabled, t, useGetAuthorization: useAliasGetAuthorization }) => {
+  const { pending, data = {} } = useAliasGetAuthorization();
+  const { authorized = {}, errorCodes, errorStatus } = data;
   const { [appName]: isAuthorized } = authorized;
-
-  useMount(async () => {
-    if (!isAuthorized) {
-      await dispatch(authorizeUser());
-    }
-
-    dispatch([initializeChrome(), setAppName(appName), hideGlobalFilter()]);
-    setUnregister(() => dispatch(onNavigation(event => history.push(event.navId))));
-  });
-
-  useUnmount(() => {
-    unregister();
-  });
 
   const renderContent = () => {
     if (isDisabled) {
@@ -97,50 +57,32 @@ const Authentication = ({
     );
   };
 
-  return <AuthenticationContext.Provider value={authData}>{renderContent()}</AuthenticationContext.Provider>;
+  return <AuthenticationContext.Provider value={data}>{renderContent()}</AuthenticationContext.Provider>;
 };
 
 /**
  * Prop types.
  *
- * @type {{authorizeUser: Function, onNavigation: Function, useHistory: Function, setAppName: Function, t: Function,
- *     children: React.ReactNode, appName: string, initializeChrome: Function, useDispatch: Function,
- *     isDisabled: boolean, useAuth: Function,hideGlobalFilter: Function}}
+ * @type {{useGetAuthorization: Function, children: React.ReactNode, appName: string, isDisabled: boolean}}
  */
 Authentication.propTypes = {
   appName: PropTypes.string,
-  authorizeUser: PropTypes.func,
   children: PropTypes.node.isRequired,
-  hideGlobalFilter: PropTypes.func,
-  initializeChrome: PropTypes.func,
   isDisabled: PropTypes.bool,
-  onNavigation: PropTypes.func,
-  setAppName: PropTypes.func,
   t: PropTypes.func,
-  useDispatch: PropTypes.func,
-  useHistory: PropTypes.func,
-  useAuth: PropTypes.func
+  useGetAuthorization: PropTypes.func
 };
 
 /**
  * Default props.
  *
- * @type {{authorizeUser: Function, onNavigation: Function, useHistory: Function, setAppName: Function, t: Function,
- *     appName: string, initializeChrome: Function, useDispatch: Function, isDisabled: boolean, useAuth: Function,
- *     hideGlobalFilter: Function}}
+ * @type {{useGetAuthorization: Function, t: Function, appName: string, isDisabled: boolean}}
  */
 Authentication.defaultProps = {
   appName: routerHelpers.appName,
-  authorizeUser: reduxActions.platform.authorizeUser,
-  hideGlobalFilter: reduxActions.platform.hideGlobalFilter,
-  initializeChrome: reduxActions.platform.initializeChrome,
   isDisabled: helpers.UI_DISABLED,
-  onNavigation: reduxActions.platform.onNavigation,
-  setAppName: reduxActions.platform.setAppName,
   t: translate,
-  useDispatch: storeHooks.reactRedux.useDispatch,
-  useHistory: routerHooks.useHistory,
-  useAuth
+  useGetAuthorization
 };
 
 export { Authentication as default, Authentication };
