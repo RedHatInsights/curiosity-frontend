dfb3645a98b95e7093d5e4cc3997034509601c70
CD Cabrera <ccabrera@redhat.com>
2022-04-28 11:40:07 -0400

    refactor(bannerMessages): ent-4899 align hook components (#926)
    * bannerMessages, context, bundle api call, logic into hook
    * productView, context, move banner under product columns
    * redux, clean up unused hooks for actions, remove selectors
    * rhsmTransformers, cloudigradeHasMismatch

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -1,11 +1,9 @@
-import appMessagesSelectors from './appMessagesSelectors';
 import guestsListSelectors from './guestsListSelectors';
 import graphCardSelectors from './graphCardSelectors';
 import inventoryListSelectors from './inventoryListSelectors';
 import userSelectors from './userSelectors';
 
 const reduxSelectors = {
-  appMessages: appMessagesSelectors,
   guestsList: guestsListSelectors,
   graphCard: graphCardSelectors,
   inventoryList: inventoryListSelectors,
