f80f31414962fbe90ce048f78a690ffe479aab63
CD Cabrera <ccabrera@redhat.com>
2022-04-28 11:40:07 -0400

    refactor(bannerMessages): ent-4899 align hook components (#926)
    * bannerMessages, context, bundle api call, logic into hook
    * productView, context, move banner under product columns
    * redux, clean up unused hooks for actions, remove selectors
    * rhsmTransformers, cloudigradeHasMismatch

---
+++ src/components/bannerMessages/bannerMessagesContext.js
@@ -0,0 +1,86 @@
+import { useShallowCompareEffect } from 'react-use';
+import { reduxActions, storeHooks } from '../../redux';
+import { useProduct, useProductQuery } from '../productView/productViewContext';
+import { dateHelpers } from '../../common';
+import {
+  rhsmConstants,
+  RHSM_API_QUERY_GRANULARITY_TYPES as GRANULARITY_TYPES,
+  RHSM_API_QUERY_SET_TYPES
+} from '../../services/rhsm/rhsmConstants';
+
+/**
+ * ToDo: useGetAppMessages is setup to work with existing Tally response, pre-metrics
+ * Banner messages scans the returned Tally listing for the HAS_CLOUDIGRADE_MISMATCH. In the future
+ * this may need to be updated to pull from the "meta" object part of the Tally response.
+ */
+/**
+ * Get app messages.
+ *
+ * @param {object} options
+ * @param {Function} options.getMessageReports
+ * @param {Function} options.useDispatch
+ * @param {Function} options.useProduct
+ * @param {Function} options.useProductQuery
+ * @param {Function} options.useSelectorsResponse
+ * @returns {object}
+ */
+const useGetAppMessages = ({
+  getMessageReports = reduxActions.rhsm.getMessageReports,
+  useDispatch: useAliasDispatch = storeHooks.reactRedux.useDispatch,
+  useProduct: useAliasProduct = useProduct,
+  useProductQuery: useAliasProductQuery = useProductQuery,
+  useSelectorsResponse: useAliasSelectorsResponse = storeHooks.reactRedux.useSelectorsResponse
+} = {}) => {
+  const { productId } = useAliasProduct();
+  const query = useAliasProductQuery();
+  const dispatch = useAliasDispatch();
+  const { error, fulfilled, pending, data } = useAliasSelectorsResponse({
+    id: 'messages',
+    selector: ({ messages }) => messages?.report?.[productId]
+  });
+
+  useShallowCompareEffect(() => {
+    if (productId) {
+      const { startDate, endDate } = dateHelpers.getRangedDateTime('CURRENT');
+      const updatedQuery = {
+        ...query,
+        [RHSM_API_QUERY_SET_TYPES.GRANULARITY]: GRANULARITY_TYPES.DAILY,
+        [RHSM_API_QUERY_SET_TYPES.START_DATE]: startDate.toISOString(),
+        [RHSM_API_QUERY_SET_TYPES.END_DATE]: endDate.toISOString()
+      };
+
+      getMessageReports(productId, updatedQuery)(dispatch);
+    }
+  }, [productId, query]);
+
+  const updatedData = {
+    cloudigradeMismatch: false
+  };
+
+  if (fulfilled) {
+    const { messages = {} } = data || {};
+
+    updatedData.cloudigradeMismatch =
+      messages?.data
+        ?.reverse()
+        ?.find(
+          ({ [rhsmConstants.RHSM_API_RESPONSE_TALLY_META_TYPES.HAS_CLOUDIGRADE_MISMATCH]: mismatch }) =>
+            mismatch === true
+        ) !== undefined;
+  }
+
+  return {
+    error,
+    fulfilled,
+    pending,
+    data: {
+      ...updatedData
+    }
+  };
+};
+
+const context = {
+  useGetAppMessages
+};
+
+export { context as default, context, useGetAppMessages };
