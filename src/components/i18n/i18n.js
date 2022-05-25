2ab8365afe6a2e3dcaadd6b72a370a633330b489
CD Cabrera <ccabrera@redhat.com>
2022-05-25 18:12:06 -0400

    fix(i18n): ent-5022 allow empty string context value (#939)

--- src/components/i18n/i18n.js
+++ src/components/i18n/i18n.js
@@ -7,15 +7,24 @@ import { useMount } from 'react-use';
 import { helpers } from '../../common/helpers';
 
 /**
+ * Check to help provide an empty context.
+ *
+ * @type {string}
+ */
+const EMPTY_CONTEXT = 'LOCALE_EMPTY_CONTEXT';
+
+/**
  * Apply a string towards a key. Optional replacement values and component/nodes.
  * See, https://react.i18next.com/
  *
  * @param {string|Array} translateKey A key reference, or an array of a primary key with fallback keys.
  * @param {string|object|Array} values A default string if the key can't be found. An object with i18next settings. Or an array of objects (key/value) pairs used to replace string tokes. i.e. "[{ hello: 'world' }]"
  * @param {Array} components An array of HTML/React nodes used to replace string tokens. i.e. "[<span />, <React.Fragment />]"
+ * @param {object} options
+ * @param {string} options.emptyContextValue Check to allow an empty context value.
  * @returns {string|React.ReactNode}
  */
-const translate = (translateKey, values = null, components) => {
+const translate = (translateKey, values = null, components, { emptyContextValue = EMPTY_CONTEXT } = {}) => {
   const updatedValues = values;
   let updatedTranslateKey = translateKey;
 
@@ -25,8 +34,11 @@ const translate = (translateKey, values = null, components) => {
 
   if (Array.isArray(updatedValues?.context)) {
     updatedValues.context = updatedValues.context
+      .map(value => (value === emptyContextValue && ' ') || value)
       .filter(value => typeof value === 'string' && value.length > 0)
       .join('_');
+  } else if (updatedValues?.context === emptyContextValue) {
+    updatedValues.context = ' ';
   }
 
   if (helpers.TEST_MODE) {
@@ -143,4 +155,4 @@ I18n.defaultProps = {
   locale: null
 };
 
-export { I18n as default, I18n, i18next, translate, translateComponent };
+export { I18n as default, I18n, i18next, translate, translateComponent, EMPTY_CONTEXT };
