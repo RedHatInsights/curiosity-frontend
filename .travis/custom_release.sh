4a107f4a7076c20b8bfc2a3097975a57d0c43454
CD Cabrera <ccabrera@redhat.com>
2022-07-20 20:50:13 -0400

    build(branching): sw-450 restructure, rename (#956)
    * build, rc npm script
    * CONTRIBUTING, document branching, tags, release
    * custom_release, travis, align release branching
    * dependabot, align dev branch

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -14,35 +14,39 @@ release()
 }
 #
 #
-# CI/Dev beta release for "ci" and "qa" "beta" and "stable" branches, based on deploy stage name
+# Dev release when pushing into "dev" and "main"/"master" branches. Beta/stable condition for Travis build stage name.
 #
 releaseDev()
 {
-  if [[ "${TRAVIS_BRANCH}" = "ci-beta" || "${TRAVIS_BRANCH}" = "dev" || "${TRAVIS_BRANCH}" = "ci" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
+  if [[ "${TRAVIS_BRANCH}" = "dev" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
     release "ci-beta"
     release "qa-beta"
   fi
 
-  if [[ "${TRAVIS_BRANCH}" = "ci-stable" || "${TRAVIS_BRANCH}" = "test" || "${TRAVIS_BRANCH}" = "qa" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]];  then
+  if [[ "${TRAVIS_BRANCH}" = "main" || "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]];  then
     release "ci-stable"
     release "qa-stable"
   fi
 }
 #
 #
-# Prod release for "main" and "stage" branches based on deploy stage name.
+# Prod release for "main" based on commit message, and tags. Sync related lower environments.
 #
 releaseProd()
 {
-  if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "stage" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
+  local UPDATED_UI_VERSION="$(node -p 'require(`./package.json`).version')";
+
+  if [[ "${TRAVIS_COMMIT_MESSAGE}" = *"chore(release):"* ]]; then
+    if [[ "${TRAVIS_BRANCH}" = "v${UPDATED_UI_VERSION}" || "${TRAVIS_BRANCH}" = "prod" || "${TRAVIS_BRANCH}" = "prod-stable" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]]; then
+      release "ci-stable"
+      release "qa-stable"
+      release "prod-beta"
+      release "prod-stable"
+    fi
+  elif [[ "${TRAVIS_BRANCH}" = *"-alpha"* || "${TRAVIS_BRANCH}" = *"-beta"* || "${TRAVIS_BRANCH}" = *"-rc."* ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Beta"* ]]; then
     release "prod-beta"
   fi
 
-  if [[ "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "prod" || "${TRAVIS_BRANCH}" = "main" || "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME != *"Beta"* ]];  then
-    if [[ "${TRAVIS_COMMIT_MESSAGE}" = *"chore(release):"* ]]; then
-      release "prod-stable"
-    fi
-  fi
 }
 #
 #
