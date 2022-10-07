7d5dbf7868e1e7d5bb51b12759636b11d5c43eb4
CD Cabrera <ccabrera@redhat.com>
2022-10-07 11:58:24 -0400

    feat(rhods): sw-270 activate product display (#980)
    * locale, rhacs strings
    * config, rhacs config, routing
    * rhsmConstants, product id

---
+++ src/config/__tests__/product.rhods.test.js
@@ -0,0 +1,125 @@
+import { config } from '../product.rhods';
+import { generateChartSettings } from '../../components/graphCard/graphCardHelpers';
+import { parseRowCellsListData } from '../../components/inventoryCard/inventoryCardHelpers';
+import {
+  RHSM_API_QUERY_INVENTORY_SORT_DIRECTION_TYPES as SORT_DIRECTION_TYPES,
+  RHSM_API_RESPONSE_INSTANCES_DATA_TYPES as INVENTORY_TYPES,
+  RHSM_API_RESPONSE_SUBSCRIPTIONS_DATA_TYPES as SUBSCRIPTIONS_INVENTORY_TYPES,
+  RHSM_API_RESPONSE_SUBSCRIPTIONS_META_TYPES as SUBSCRIPTIONS_INVENTORY_META_TYPES,
+  RHSM_API_QUERY_SET_TYPES,
+  RHSM_API_PATH_METRIC_TYPES
+} from '../../services/rhsm/rhsmConstants';
+
+describe('Product RHODS config', () => {
+  it('should apply graph configuration', () => {
+    const { initialGraphFilters, initialGraphSettings } = config;
+
+    expect(generateChartSettings(initialGraphFilters)).toMatchSnapshot('filters');
+    expect(initialGraphSettings).toMatchSnapshot('settings');
+  });
+
+  it('should handle a custom yAxisTickFormat for floating points', () => {
+    const generateTicks = (method = config.initialGraphSettings.yAxisTickFormat) => {
+      const ticks = {};
+
+      for (let i = 0.00001345; i < 1; i++) {
+        for (let k = 1; k < 26; k++) {
+          const incrementMultiplier = k * i;
+          ticks[incrementMultiplier] = method({ tick: incrementMultiplier });
+        }
+      }
+
+      for (let i = 0.001; i < 1; i++) {
+        for (let k = 1; k < 26; k++) {
+          const incrementMultiplier = k * i;
+          ticks[incrementMultiplier] = method({ tick: incrementMultiplier });
+        }
+      }
+
+      for (let i = 0; i < 13; i++) {
+        const multiplier = Math.pow(10, i);
+        for (let k = 1; k < 16; k++) {
+          const incrementMultiplier = k * multiplier;
+          ticks[incrementMultiplier] = method({ tick: incrementMultiplier });
+        }
+      }
+      return ticks;
+    };
+
+    expect(generateTicks()).toMatchSnapshot('yAxisTickFormat');
+  });
+
+  /**
+   * FixMe: this test needs to be updated as part of the refactor towards instances vs hosts
+   */
+  it('should apply an instances inventory configuration under hosts', () => {
+    const { initialInventoryFilters: initialFilters, inventoryHostsQuery: inventoryQuery } = config;
+
+    const inventoryData = {
+      [INVENTORY_TYPES.DISPLAY_NAME]: 'lorem ipsum',
+      [RHSM_API_PATH_METRIC_TYPES.CORES]: 200,
+      [INVENTORY_TYPES.LAST_SEEN]: '2022-01-01T00:00:00.000Z'
+    };
+
+    const filteredInventoryData = parseRowCellsListData({
+      filters: initialFilters,
+      cellData: inventoryData
+    });
+
+    expect(filteredInventoryData).toMatchSnapshot('filtered');
+
+    const filteredInventoryDataAuthorized = parseRowCellsListData({
+      filters: initialFilters,
+      cellData: {
+        ...inventoryData,
+        [INVENTORY_TYPES.INVENTORY_ID]: 'XXXX-XXXX-XXXXX-XXXXX'
+      },
+      session: { authorized: { inventory: true } }
+    });
+
+    expect(filteredInventoryDataAuthorized).toMatchSnapshot('filtered, authorized');
+
+    expect(inventoryQuery[RHSM_API_QUERY_SET_TYPES.DIRECTION] === SORT_DIRECTION_TYPES.DESCENDING).toBe(true);
+  });
+
+  it('should apply subscriptions inventory configuration', () => {
+    const { initialSubscriptionsInventoryFilters: initialFilters, inventorySubscriptionsQuery: inventoryQuery } =
+      config;
+
+    const inventoryData = {
+      [SUBSCRIPTIONS_INVENTORY_TYPES.PRODUCT_NAME]: 'lorem',
+      [SUBSCRIPTIONS_INVENTORY_TYPES.SERVICE_LEVEL]: 'hello world',
+      [SUBSCRIPTIONS_INVENTORY_TYPES.NEXT_EVENT_DATE]: '2022-01-01T00:00:00.000Z',
+      [SUBSCRIPTIONS_INVENTORY_TYPES.TOTAL_CAPACITY]: 2000,
+      [SUBSCRIPTIONS_INVENTORY_TYPES.HAS_INFINITE_QUANTITY]: true
+    };
+
+    const inventoryMeta = {
+      [SUBSCRIPTIONS_INVENTORY_META_TYPES.SUBSCRIPTION_TYPE]: 'dolor'
+    };
+
+    const filteredInventoryData = parseRowCellsListData({
+      filters: initialFilters,
+      cellData: inventoryData,
+      meta: inventoryMeta
+    });
+
+    expect(filteredInventoryData).toMatchSnapshot('filtered');
+
+    const fallbackInventoryData = {
+      ...inventoryData,
+      [SUBSCRIPTIONS_INVENTORY_TYPES.SERVICE_LEVEL]: null,
+      [SUBSCRIPTIONS_INVENTORY_TYPES.NEXT_EVENT_DATE]: null
+    };
+
+    const fallbackFilteredInventoryData = parseRowCellsListData({
+      filters: initialFilters,
+      cellData: fallbackInventoryData,
+      meta: {}
+    });
+
+    expect(fallbackFilteredInventoryData).toMatchSnapshot('filtered, fallback display');
+
+    expect(inventoryQuery[RHSM_API_QUERY_SET_TYPES.DIRECTION] === SORT_DIRECTION_TYPES.DESCENDING).toBe(true);
+  });
+});
---
+++ src/config/product.rhods.js
@@ -0,0 +1,208 @@
+import React from 'react';
+import {
+  chart_color_blue_100 as chartColorBlueLight,
+  chart_color_blue_300 as chartColorBlueDark
+} from '@patternfly/react-tokens';
+import { Button } from '@patternfly/react-core';
+import { DateFormat } from '@redhat-cloud-services/frontend-components/DateFormat';
+import moment from 'moment';
+import {
+  RHSM_API_QUERY_INVENTORY_SORT_DIRECTION_TYPES as SORT_DIRECTION_TYPES,
+  RHSM_API_QUERY_INVENTORY_SORT_TYPES,
+  RHSM_API_QUERY_INVENTORY_SUBSCRIPTIONS_SORT_TYPES,
+  RHSM_API_RESPONSE_SUBSCRIPTIONS_DATA_TYPES as SUBSCRIPTIONS_INVENTORY_TYPES,
+  RHSM_API_RESPONSE_SUBSCRIPTIONS_META_TYPES as SUBSCRIPTIONS_INVENTORY_META_TYPES,
+  RHSM_API_RESPONSE_INSTANCES_DATA_TYPES as INVENTORY_TYPES,
+  RHSM_API_QUERY_GRANULARITY_TYPES as GRANULARITY_TYPES,
+  RHSM_API_QUERY_SET_TYPES,
+  RHSM_API_PATH_PRODUCT_TYPES,
+  RHSM_API_PATH_METRIC_TYPES,
+  RHSM_INTERNAL_PRODUCT_DISPLAY_TYPES as DISPLAY_TYPES
+} from '../services/rhsm/rhsmConstants';
+import { dateHelpers, helpers } from '../common';
+import { translate, EMPTY_CONTEXT } from '../components/i18n/i18n';
+
+const productGroup = RHSM_API_PATH_PRODUCT_TYPES.RHODS;
+
+const productId = RHSM_API_PATH_PRODUCT_TYPES.RHODS;
+
+const productLabel = RHSM_API_PATH_PRODUCT_TYPES.RHODS;
+
+const config = {
+  productGroup,
+  productId,
+  productLabel,
+  productDisplay: DISPLAY_TYPES.HOURLY,
+  viewId: `view${productGroup}`,
+  query: {
+    [RHSM_API_QUERY_SET_TYPES.START_DATE]: dateHelpers.getRangedMonthDateTime('current').value.startDate.toISOString(),
+    [RHSM_API_QUERY_SET_TYPES.END_DATE]: dateHelpers.getRangedMonthDateTime('current').value.endDate.toISOString()
+  },
+  graphTallyQuery: {
+    [RHSM_API_QUERY_SET_TYPES.GRANULARITY]: GRANULARITY_TYPES.DAILY
+  },
+  inventoryHostsQuery: {
+    [RHSM_API_QUERY_SET_TYPES.SORT]: RHSM_API_QUERY_INVENTORY_SORT_TYPES.LAST_SEEN,
+    [RHSM_API_QUERY_SET_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
+    [RHSM_API_QUERY_SET_TYPES.LIMIT]: 100,
+    [RHSM_API_QUERY_SET_TYPES.OFFSET]: 0
+  },
+  inventorySubscriptionsQuery: {
+    [RHSM_API_QUERY_SET_TYPES.SORT]: RHSM_API_QUERY_INVENTORY_SUBSCRIPTIONS_SORT_TYPES.NEXT_EVENT_DATE,
+    [RHSM_API_QUERY_SET_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
+    [RHSM_API_QUERY_SET_TYPES.LIMIT]: 100,
+    [RHSM_API_QUERY_SET_TYPES.OFFSET]: 0
+  },
+  initialGraphFilters: [
+    {
+      id: RHSM_API_PATH_METRIC_TYPES.CORES,
+      fill: chartColorBlueLight.value,
+      stroke: chartColorBlueDark.value,
+      color: chartColorBlueDark.value,
+      chartType: 'line',
+      isStacked: false,
+      isStandalone: true,
+      yAxisChartLabel: ({ id }) => translate('curiosity-graph.label_axisY', { context: id })
+    }
+  ],
+  initialGraphSettings: {
+    isCardTitleDescription: true,
+    xAxisChartLabel: () => translate('curiosity-graph.label_axisX', { context: GRANULARITY_TYPES.DAILY }),
+    yAxisTickFormat: ({ tick }) => {
+      if (tick > 1) {
+        return helpers
+          .numberDisplay(tick)
+          ?.format({ average: true, mantissa: 1, trimMantissa: true, lowPrecision: false })
+          ?.toUpperCase();
+      }
+      return helpers
+        .numberDisplay(tick)
+        ?.format({ average: true, mantissa: 5, trimMantissa: true, lowPrecision: true })
+        ?.toUpperCase();
+    }
+  },
+  initialInventoryFilters: [
+    {
+      id: INVENTORY_TYPES.DISPLAY_NAME,
+      cell: (
+        { [INVENTORY_TYPES.DISPLAY_NAME]: displayName = {}, [INVENTORY_TYPES.INVENTORY_ID]: inventoryId = {} },
+        session
+      ) => {
+        const { inventory: authorized } = session?.authorized || {};
+
+        if (!inventoryId.value) {
+          return displayName.value;
+        }
+
+        let updatedDisplayName = displayName.value || inventoryId.value;
+
+        if (authorized) {
+          updatedDisplayName = (
+            <Button
+              isInline
+              component="a"
+              variant="link"
+              href={`${helpers.UI_DEPLOY_PATH_PREFIX}/insights/inventory/${inventoryId.value}/`}
+            >
+              {displayName.value || inventoryId.value}
+            </Button>
+          );
+        }
+
+        return updatedDisplayName;
+      },
+      isSortable: true
+    },
+    {
+      id: INVENTORY_TYPES.BILLING_PROVIDER,
+      cell: ({ [INVENTORY_TYPES.BILLING_PROVIDER]: provider }) =>
+        translate(`curiosity-inventory.measurement_${INVENTORY_TYPES.BILLING_PROVIDER}`, {
+          context: provider?.value || 'none'
+        }),
+      isSortable: true,
+      isWrappable: false,
+      cellWidth: 15
+    },
+    {
+      id: RHSM_API_PATH_METRIC_TYPES.CORES,
+      cell: ({ [RHSM_API_PATH_METRIC_TYPES.CORES]: total }) =>
+        translate('curiosity-inventory.measurement', {
+          context: RHSM_API_PATH_METRIC_TYPES.CORES,
+          total: helpers.numberDisplay(total?.value)?.format({ mantissa: 5, trimMantissa: true }) || 0
+        }),
+      isSortable: true,
+      isWrappable: true,
+      cellWidth: 15
+    },
+    {
+      id: INVENTORY_TYPES.LAST_SEEN,
+      cell: ({ [INVENTORY_TYPES.LAST_SEEN]: lastSeen }) =>
+        (lastSeen?.value && <DateFormat date={lastSeen?.value} />) || '',
+      isSortable: true,
+      isWrappable: true,
+      cellWidth: 15
+    }
+  ],
+  initialSubscriptionsInventoryFilters: [
+    {
+      id: SUBSCRIPTIONS_INVENTORY_TYPES.PRODUCT_NAME,
+      isSortable: true,
+      isWrappable: true
+    },
+    {
+      id: SUBSCRIPTIONS_INVENTORY_TYPES.BILLING_PROVIDER,
+      cell: ({ [SUBSCRIPTIONS_INVENTORY_TYPES.BILLING_PROVIDER]: provider }) =>
+        translate(`curiosity-inventory.measurement_${SUBSCRIPTIONS_INVENTORY_TYPES.BILLING_PROVIDER}`, {
+          context: provider?.value || 'none'
+        }),
+      isSortable: true,
+      isWrappable: false,
+      cellWidth: 15
+    },
+    {
+      id: SUBSCRIPTIONS_INVENTORY_TYPES.SERVICE_LEVEL,
+      isSortable: true,
+      isWrappable: true,
+      cellWidth: 15
+    },
+    {
+      id: SUBSCRIPTIONS_INVENTORY_TYPES.QUANTITY,
+      isSortable: true,
+      cellWidth: 10,
+      isWrappable: true
+    },
+    {
+      id: SUBSCRIPTIONS_INVENTORY_META_TYPES.SUBSCRIPTION_TYPE,
+      cell: (data, session, { [SUBSCRIPTIONS_INVENTORY_META_TYPES.SUBSCRIPTION_TYPE]: subscriptionType } = {}) =>
+        translate(`curiosity-inventory.label_${SUBSCRIPTIONS_INVENTORY_META_TYPES.SUBSCRIPTION_TYPE}`, {
+          context: subscriptionType || EMPTY_CONTEXT
+        }),
+      isSortable: false,
+      cellWidth: 15,
+      isWrappable: true
+    },
+    {
+      id: SUBSCRIPTIONS_INVENTORY_TYPES.NEXT_EVENT_DATE,
+      cell: ({ [SUBSCRIPTIONS_INVENTORY_TYPES.NEXT_EVENT_DATE]: nextEventDate } = {}) =>
+        (nextEventDate?.value &&
+          helpers.isDate(nextEventDate?.value) &&
+          moment.utc(nextEventDate?.value).format('YYYY-MM-DD')) ||
+        '',
+      isSortable: true,
+      isWrappable: true,
+      cellWidth: 15
+    }
+  ],
+  initialToolbarFilters: [
+    {
+      id: RHSM_API_QUERY_SET_TYPES.BILLING_PROVIDER
+    }
+  ],
+  initialSecondaryToolbarFilters: [
+    {
+      id: 'rangedMonthly'
+    }
+  ]
+};
+
+export { config as default, config, productGroup, productId };
