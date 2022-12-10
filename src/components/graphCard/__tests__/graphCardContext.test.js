4f8bbf98d9100c840d4d9546a754e063d7e7bf23
CD Cabrera <ccabrera@redhat.com>
2022-12-09 19:02:41 -0500

    fix(graphCard,Context): sw-690 graph card loading (#1023)
    * graphCard, lifecycle to context
    * graphCardChartLegend, use parsed context
    * graphCardContext, useParseFiltersSettings hook
    * graphCardContext, rhsmActions, align promise all

--- src/components/graphCard/__tests__/graphCardContext.test.js
+++ src/components/graphCard/__tests__/graphCardContext.test.js
@@ -1,8 +1,47 @@
-import { context, useMetricsSelector } from '../graphCardContext';
+import { context, useMetricsSelector, useParseFiltersSettings } from '../graphCardContext';
+import { rhsmConstants } from '../../../services/rhsm/rhsmConstants';
 
 describe('GraphCardContext', () => {
   it('should return specific properties', () => {
     expect(context).toMatchSnapshot('specific properties');
+  });
+
+  it('should parse configuration', async () => {
+    const { result: basicConfig } = await mountHook(() => useParseFiltersSettings());
+    expect(basicConfig).toMatchSnapshot('configuration, basic');
+
+    const { unmount: groupedUnmount, result: groupedConfig } = await mountHook(() =>
+      useParseFiltersSettings({
+        useProduct: () => ({ productId: 'loremIpsum' }),
+        useProductGraphConfig: () => ({
+          filters: [
+            {
+              metric: rhsmConstants.RHSM_API_PATH_METRIC_TYPES.CORE_SECONDS
+            }
+          ]
+        })
+      })
+    );
+
+    await groupedUnmount();
+    expect(groupedConfig).toMatchSnapshot('configuration, grouped');
+
+    const { unmount: standaloneUnmount, result: standaloneConfig } = await mountHook(() =>
+      useParseFiltersSettings({
+        useProduct: () => ({ productId: 'loremIpsum' }),
+        useProductGraphConfig: () => ({
+          filters: [
+            {
+              metric: rhsmConstants.RHSM_API_PATH_METRIC_TYPES.CORE_SECONDS,
+              isStandalone: true
+            }
+          ]
+        })
+      })
+    );
+
+    await standaloneUnmount();
+    expect(standaloneConfig).toMatchSnapshot('configuration, standalone');
   });
 
   it('should aggregate metric API calls', () => {
