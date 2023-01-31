375a39e2a0882695c0b6fe46cb3bc11020742f67
CD Cabrera <ccabrera@redhat.com>
2023-01-31 16:18:40 -0500

    refactor(redux): sw-625 remove connectRouter (#1045)

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { store } from './store';
 import { reduxActions } from './actions';
 import { reduxHelpers } from './common';
@@ -7,14 +6,4 @@ import { storeHooks } from './hooks';
 import { reduxReducers } from './reducers';
 import { reduxTypes } from './types';
 
-/**
- * Wrapper for applying Router Dom withRouter and Redux connect.
- *
- * @param {Function} mapStateToProps
- * @param {Function} mapDispatchToProps
- * @returns {Function}
- */
-const connectRouter = (mapStateToProps, mapDispatchToProps) => component =>
-  withRouter(connect(mapStateToProps, mapDispatchToProps)(component));
-
-export { connect, connectRouter, reduxActions, reduxHelpers, reduxReducers, reduxTypes, store, storeHooks };
+export { connect, reduxActions, reduxHelpers, reduxReducers, reduxTypes, store, storeHooks };
