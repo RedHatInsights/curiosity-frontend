1d41724d52dc5516dcf337d1e94cb3be118bd862
CD Cabrera <cabrera.code@gmail.com>
2023-03-26 23:53:57 -0400

    fix(chartHelpers): sw-1030 expand x axis unit test (#1082)

--- src/components/chart/__tests__/chartHelpers.test.js
+++ src/components/chart/__tests__/chartHelpers.test.js
@@ -228,21 +228,263 @@ describe('ChartHelpers', () => {
       dataSet: {
         id: 'lorem',
         data: [
-          { x: 0, y: 10 },
-          { x: 1, y: 10 },
-          { x: 2, y: 10 },
-          { x: 3, y: 10 },
-          { x: 4, y: 10 },
-          { x: 5, y: 10 }
+          {
+            x: 0,
+            y: 0,
+            date: '2023-02-12T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 1,
+            y: 0,
+            date: '2023-02-13T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 2,
+            y: 0,
+            date: '2023-02-14T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 3,
+            y: 0,
+            date: '2023-02-15T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 4,
+            y: 0,
+            date: '2023-02-16T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 5,
+            y: 0,
+            date: '2023-02-17T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 6,
+            y: 0,
+            date: '2023-02-18T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 7,
+            y: 0,
+            date: '2023-02-19T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 8,
+            y: 0,
+            date: '2023-02-20T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 9,
+            y: 0,
+            date: '2023-02-21T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 10,
+            y: 0,
+            date: '2023-02-22T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 11,
+            y: 0,
+            date: '2023-02-23T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 12,
+            y: 0,
+            date: '2023-02-24T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 13,
+            y: 0,
+            date: '2023-02-25T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 14,
+            y: 0,
+            date: '2023-02-26T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 15,
+            y: 0,
+            date: '2023-02-27T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 16,
+            y: 0,
+            date: '2023-02-28T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 17,
+            y: 0,
+            date: '2023-03-01T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 18,
+            y: 0,
+            date: '2023-03-02T00:00:00.000Z',
+            hasData: false,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 19,
+            y: 0,
+            date: '2023-03-03T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 20,
+            y: 0,
+            date: '2023-03-04T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 21,
+            y: 0,
+            date: '2023-03-05T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 22,
+            y: 1,
+            date: '2023-03-06T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 23,
+            y: 1,
+            date: '2023-03-07T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 24,
+            y: 1,
+            date: '2023-03-08T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 25,
+            y: 1,
+            date: '2023-03-09T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 26,
+            y: 1,
+            date: '2023-03-10T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 27,
+            y: 0,
+            date: '2023-03-11T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 28,
+            y: 0,
+            date: '2023-03-12T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 29,
+            y: 0,
+            date: '2023-03-13T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: false,
+            isFutureDate: false
+          },
+          {
+            x: 30,
+            y: 0,
+            date: '2023-03-14T00:00:00.000Z',
+            hasData: true,
+            isCurrentDate: true,
+            isFutureDate: false
+          }
         ]
       }
     };
 
     const generatedProps = chartHelpers.generateXAxisProps(options);
     expect(generatedProps).toMatchSnapshot('props');
+    generatedProps.tickValues.forEach((value, index) => generatedProps.tickFormat(index));
 
-    generatedProps.tickFormat(1);
-    expect(mockFormat.mock.calls[0]).toMatchSnapshot('format callback');
+    expect(mockFormat.mock.calls).toMatchSnapshot('format callback');
   });
 
   it('should generate y axis props', () => {
