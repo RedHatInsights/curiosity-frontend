70b7ada3cb927bafaf22769b1a52999c8340839e
CD Cabrera <cabrera.code@gmail.com>
2023-05-30 15:05:42 -0400

    fix(config): sw-1294 rhacs, rhods inventory links
    * rhacs, updated inventory path for links
    * rhods, disabled inventory paths, annotation

--- src/config/product.rhacs.js
+++ src/config/product.rhacs.js
@@ -167,7 +167,7 @@ const config = {
               isInline
               component="a"
               variant="link"
-              href={`${helpers.UI_DEPLOY_PATH_LINK_PREFIX}/insights/inventory/${instanceId.value}/`}
+              href={`${helpers.UI_DEPLOY_PATH_LINK_PREFIX}/application-services/acs/instances/instance/${instanceId.value}`}
             >
               {displayName.value || instanceId.value}
             </Button>
