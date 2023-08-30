681ea38668581af7fc63d90f535952c932d84e49
CD Cabrera <cabrera.code@gmail.com>
2023-08-30 05:37:16 -0400

    chore(build): npm updates

--- src/components/chart/chartAxisLabel.js
+++ src/components/chart/chartAxisLabel.js
@@ -19,19 +19,19 @@ import React from 'react';
  */
 const chartAxisLabel =
   ({ axis, index = 0 } = {}) =>
-  ({ x, y, text }) => // eslint-disable-line
-    (
-      <g>
-        <foreignObject x={0} y={0} width="100%" height="100%">
-          <div
-            className={`curiosity-chartarea__axis-label-container curiosity-chartarea__axis-label-container-${axis} curiosity-chartarea__axis-label-container-${axis}-${index}`}
-            style={{ top: `${y ?? 0}px` }}
-            xmlns="http://www.w3.org/1999/xhtml"
-          >
-            <div className="curiosity-chartarea__axis-label-container-content">{text}</div>
-          </div>
-        </foreignObject>
-      </g>
-    );
+  // eslint-disable-next-line
+  ({ x, y, text }) => (
+    <g>
+      <foreignObject x={0} y={0} width="100%" height="100%">
+        <div
+          className={`curiosity-chartarea__axis-label-container curiosity-chartarea__axis-label-container-${axis} curiosity-chartarea__axis-label-container-${axis}-${index}`}
+          style={{ top: `${y ?? 0}px` }}
+          xmlns="http://www.w3.org/1999/xhtml"
+        >
+          <div className="curiosity-chartarea__axis-label-container-content">{text}</div>
+        </div>
+      </foreignObject>
+    </g>
+  );
 
 export { chartAxisLabel as default, chartAxisLabel };
