edea1271264c8b00cd6083fb2ff5cca01e1197c1
CD Cabrera <cabrera.code@gmail.com>
2023-09-29 10:11:25 -0400

    build(proxy): sw-1689 locale file paths (#1203)

--- config/webpack.proxy.config.js
+++ config/webpack.proxy.config.js
@@ -1,36 +1,47 @@
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { setReplacePlugin, setCommonPlugins } = require('./build.plugins');
 const { setupDotenvFilesForEnv } = require('./build.dotenv');
-const { setProxyRoutes } = require('./spandx.config');
 
-const { _BUILD_RELATIVE_DIRNAME, DEV_BRANCH, DEV_PORT } = setupDotenvFilesForEnv({
+const {
+  _BUILD_RELATIVE_DIRNAME,
+  REACT_APP_UI_DEPLOY_PATH_PREFIX: BETA_PREFIX,
+  DEV_BRANCH,
+  DEV_PORT
+} = setupDotenvFilesForEnv({
   env: 'proxy'
 });
 
-let BETA_PREFIX = '';
+const UPDATED_BETA_PREFIX = [BETA_PREFIX];
 
-if (/(prod|stage|qa|ci)-beta/.test(DEV_BRANCH)) {
-  BETA_PREFIX = '/beta';
+switch (BETA_PREFIX) {
+  case '/preview':
+    UPDATED_BETA_PREFIX.push('/beta');
+    break;
+  case '/beta':
+  default:
+    UPDATED_BETA_PREFIX.push('/preview');
+    break;
 }
 
 const { config: webpackConfig, plugins } = config({
-  appUrl: [
-    `${BETA_PREFIX}/insights/subscriptions`,
-    `${BETA_PREFIX}/openshift/subscriptions`,
-    `${BETA_PREFIX}/application-services/subscriptions`,
-    `${BETA_PREFIX}/subscriptions/usage`,
-    `/preview/insights/subscriptions`,
-    `/preview/openshift/subscriptions`,
-    `/preview/application-services/subscriptions`,
-    `/preview/subscriptions/usage`
-  ],
+  appUrl: (() => {
+    const urls = [];
+    UPDATED_BETA_PREFIX.forEach(path => {
+      urls.push(
+        `${path}/insights/subscriptions`,
+        `${path}/openshift/subscriptions`,
+        `${path}/application-services/subscriptions`,
+        `${path}/subscriptions/usage`
+      );
+    });
+    return urls;
+  })(),
   client: { overlay: false },
   debug: true,
-  deployment: (/beta/.test(BETA_PREFIX) && 'beta/apps') || 'apps',
+  deployment: (/beta/.test(BETA_PREFIX) && 'beta/apps') || (/preview/.test(BETA_PREFIX) && 'preview/apps') || 'apps',
   env: (/(prod|stage|qa|ci)(-stable|-beta)$/.test(DEV_BRANCH) && DEV_BRANCH) || 'stage-stable',
   port: Number.parseInt(DEV_PORT, 10),
   rootFolder: _BUILD_RELATIVE_DIRNAME,
-  routes: setProxyRoutes({ DEV_PORT, BETA_PREFIX }),
   standalone: false,
   useProxy: true,
   replacePlugin: setReplacePlugin()
