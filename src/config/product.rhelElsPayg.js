2ca7797a44320311fdca54f211636b310b492f25
CD Cabrera <cabrera.code@gmail.com>
2023-11-01 15:09:30 -0400

    feat(config): sw-1889 add rhel for x86 els payg (#1224)
    * config, use vcpu metric, base off rhosak
    * locale, adjust rhel desc, subtitle for on-demand
    * services, generic mock response, allow vcpus

---
+++ src/config/product.rhelElsPayg.js
@@ -0,0 +1,313 @@
+import React from 'react';
+import {
+  chart_color_blue_100 as chartColorBlueLight,
+  chart_color_blue_300 as chartColorBlueDark
+} from '@patternfly/react-tokens';
+import { Button } from '@patternfly/react-core';
+import { DateFormat } from '@redhat-cloud-services/frontend-components/DateFormat';
+import moment from 'moment';
+import {
+  RHSM_API_PATH_METRIC_TYPES,
+  RHSM_API_PATH_PRODUCT_TYPES,
+  RHSM_API_QUERY_GRANULARITY_TYPES as GRANULARITY_TYPES,
+  RHSM_API_QUERY_INVENTORY_SORT_DIRECTION_TYPES as SORT_DIRECTION_TYPES,
+  RHSM_API_QUERY_INVENTORY_SORT_TYPES as INVENTORY_SORT_TYPES,
+  RHSM_API_QUERY_INVENTORY_SUBSCRIPTIONS_SORT_TYPES as SUBSCRIPTIONS_SORT_TYPES,
+  RHSM_API_QUERY_SET_TYPES,
+  RHSM_API_RESPONSE_SUBSCRIPTIONS_DATA_TYPES as SUBSCRIPTIONS_INVENTORY_TYPES,
+  RHSM_API_RESPONSE_SUBSCRIPTIONS_META_TYPES as SUBSCRIPTIONS_INVENTORY_META_TYPES,
+  RHSM_API_RESPONSE_INSTANCES_DATA_TYPES as INVENTORY_TYPES,
+  RHSM_INTERNAL_PRODUCT_DISPLAY_TYPES as DISPLAY_TYPES
+} from '../services/rhsm/rhsmConstants';
+import { ChartTypeVariant } from '../components/chart/chart';
+import { dateHelpers, helpers } from '../common';
+import { SelectPosition } from '../components/form/select';
+import { translate, EMPTY_CONTEXT } from '../components/i18n/i18n';
+
+/**
+ * RHEL X86 ELS On-Demand
+ *
+ * @memberof Products
+ * @module RHEL-X86-ELS-PAYG
+ */
+
+/**
+ * Product group. A variant and dissimilar product configuration grouping identifier.
+ *
+ * @type {string}
+ */
+const productGroup = 'rhel';
+
+/**
+ * Product ID. The identifier used when querying the API.
+ *
+ * @type {string}
+ */
+const productId = RHSM_API_PATH_PRODUCT_TYPES.RHEL_X86_ELS_PAYG;
+
+/**
+ * Product label. An identifier used for display strings.
+ *
+ * @type {string}
+ */
+const productLabel = RHSM_API_PATH_PRODUCT_TYPES.RHEL_X86_ELS_PAYG;
+
+/**
+ * Product configuration
+ *
+ * @type {{productLabel: string, productPath: string, aliases: string[], productId: string, inventorySubscriptionsQuery:
+ *     object, query: object, initialSubscriptionsInventoryFilters: Array, initialInventorySettings: object, viewId:
+ *     string, initialToolbarFilters: Array, productGroup: string, graphTallyQuery: object, inventoryHostsQuery:
+ *     object, productDisplay: string, initialGraphFilters: Array, initialGraphSettings: object,
+ *     initialInventoryFilters: Array}}
+ */
+const config = {
+  aliases: [],
+  productGroup,
+  productId,
+  productLabel,
+  productPath: productGroup.toLowerCase(),
+  productDisplay: DISPLAY_TYPES.HOURLY,
+  viewId: `view${productGroup}-${productId}`,
+  query: {
+    [RHSM_API_QUERY_SET_TYPES.START_DATE]: dateHelpers.getRangedMonthDateTime('current').value.startDate.toISOString(),
+    [RHSM_API_QUERY_SET_TYPES.END_DATE]: dateHelpers.getRangedMonthDateTime('current').value.endDate.toISOString()
+  },
+  graphTallyQuery: {
+    [RHSM_API_QUERY_SET_TYPES.GRANULARITY]: GRANULARITY_TYPES.DAILY
+  },
+  inventoryHostsQuery: {
+    [RHSM_API_QUERY_SET_TYPES.SORT]: INVENTORY_SORT_TYPES.LAST_SEEN,
+    [RHSM_API_QUERY_SET_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
+    [RHSM_API_QUERY_SET_TYPES.LIMIT]: 100,
+    [RHSM_API_QUERY_SET_TYPES.OFFSET]: 0
+  },
+  inventorySubscriptionsQuery: {
+    [RHSM_API_QUERY_SET_TYPES.SORT]: SUBSCRIPTIONS_SORT_TYPES.NEXT_EVENT_DATE,
+    [RHSM_API_QUERY_SET_TYPES.DIRECTION]: SORT_DIRECTION_TYPES.DESCENDING,
+    [RHSM_API_QUERY_SET_TYPES.LIMIT]: 100,
+    [RHSM_API_QUERY_SET_TYPES.OFFSET]: 0
+  },
+  initialGraphFilters: [
+    {
+      metric: RHSM_API_PATH_METRIC_TYPES.VCPUS,
+      fill: chartColorBlueLight.value,
+      stroke: chartColorBlueDark.value,
+      color: chartColorBlueDark.value,
+      chartType: ChartTypeVariant.line,
+      isStacked: false,
+      yAxisChartLabel: ({ id } = {}) => translate('curiosity-graph.label_axisY', { context: id })
+    }
+  ],
+  initialGraphSettings: {
+    cards: [
+      {
+        header: ({ dataSets = [] } = {}) =>
+          translate('curiosity-graph.cardHeadingMetric', {
+            context: ['dailyTotal', dataSets?.[0]?.display?.chartId],
+            testId: 'graphDailyTotalCard-header'
+          }),
+        body: ({ dataSets = [] } = {}) =>
+          translate(
+            'curiosity-graph.cardBodyMetric',
+            {
+              context: ['total', dataSets?.[0]?.display?.dailyHasData && dataSets?.[0]?.display?.chartId],
+              testId: 'graphDailyTotalCard-body',
+              total: helpers
+                .numberDisplay(dataSets?.[0]?.display?.dailyValue)
+                ?.format({
+                  average: true,
+                  mantissa: 2,
+                  trimMantissa: true,
+                  lowPrecision: false
+                })
+                ?.toUpperCase()
+            },
+            [<strong title={dataSets?.[0]?.display?.dailyValue} aria-label={dataSets?.[0]?.display?.dailyValue} />]
+          ),
+        footer: ({ dataSets = [] } = {}) =>
+          translate('curiosity-graph.cardFooterMetric', {
+            date: moment
+              .utc(dataSets?.[0]?.display?.dailyDate)
+              .format(dateHelpers.timestampUTCTimeFormats.yearTimeShort),
+            testId: 'graphDailyTotalCard-footer'
+          })
+      },
+      {
+        header: ({ dataSets = [] } = {}) =>
+          translate('curiosity-graph.cardHeadingMetric', {
+            context: ['monthlyTotal', dataSets?.[0]?.display?.chartId],
+            testId: 'graphMonthlyTotalCard-header'
+          }),
+        body: ({ dataSets = [] } = {}) =>
+          translate(
+            'curiosity-graph.cardBodyMetric',
+            {
+              context: ['total', dataSets?.[0]?.display?.monthlyHasData && dataSets?.[0]?.display?.chartId],
+              testId: 'graphMonthlyTotalCard-body',
+              total: helpers
+                .numberDisplay(dataSets?.[0]?.display?.monthlyValue)
+                ?.format({
+                  average: true,
+                  mantissa: 2,
+                  trimMantissa: true,
+                  lowPrecision: false
+                })
+                ?.toUpperCase()
+            },
+            [<strong title={dataSets?.[0]?.display?.monthlyValue} aria-label={dataSets?.[0]?.display?.monthlyValue} />]
+          ),
+        footer: ({ dataSets = [] } = {}) =>
+          translate('curiosity-graph.cardFooterMetric', {
+            date: moment
+              .utc(dataSets?.[0]?.display?.monthlyDate)
+              .format(dateHelpers.timestampUTCTimeFormats.yearTimeShort),
+            testId: 'graphMonthlyTotalCard-footer'
+          })
+      }
+    ],
+    isCardTitleDescription: true,
+    xAxisChartLabel: () => translate('curiosity-graph.label_axisX', { context: GRANULARITY_TYPES.DAILY }),
+    yAxisTickFormat: ({ tick } = {}) => {
+      if (tick > 1) {
+        return helpers
+          .numberDisplay(tick)
+          ?.format({ average: true, mantissa: 1, trimMantissa: true, lowPrecision: false })
+          ?.toUpperCase();
+      }
+      return helpers
+        .numberDisplay(tick)
+        ?.format({ average: true, mantissa: 5, trimMantissa: true, lowPrecision: true })
+        ?.toUpperCase();
+    }
+  },
+  initialInventoryFilters: [
+    {
+      metric: INVENTORY_TYPES.DISPLAY_NAME,
+      cell: ({ [INVENTORY_TYPES.DISPLAY_NAME]: displayName, [INVENTORY_TYPES.INSTANCE_ID]: instanceId } = {}) => {
+        // FixMe: Disabled, see SWATCH-1209 for resolution
+        const { inventory: authorized = false } = {};
+
+        if (!instanceId) {
+          return displayName;
+        }
+
+        let updatedDisplayName = displayName || instanceId;
+
+        if (authorized) {
+          updatedDisplayName = (
+            <Button
+              isInline
+              component="a"
+              variant="link"
+              href={`${helpers.UI_DEPLOY_PATH_LINK_PREFIX}/insights/inventory/${instanceId}/`}
+            >
+              {updatedDisplayName}
+            </Button>
+          );
+        }
+
+        return updatedDisplayName;
+      },
+      isSort: true
+    },
+    {
+      metric: INVENTORY_TYPES.BILLING_PROVIDER,
+      cell: ({ [INVENTORY_TYPES.BILLING_PROVIDER]: provider } = {}) =>
+        translate('curiosity-inventory.label', {
+          context: [INVENTORY_TYPES.BILLING_PROVIDER, provider || 'none']
+        }),
+      isSort: true,
+      width: 20
+    },
+    {
+      metric: RHSM_API_PATH_METRIC_TYPES.VCPUS,
+      cell: ({ [RHSM_API_PATH_METRIC_TYPES.VCPUS]: total } = {}) =>
+        (!total && '--') ||
+        translate('curiosity-inventory.measurement', {
+          context: RHSM_API_PATH_METRIC_TYPES.VCPUS,
+          total: helpers.numberDisplay(total)?.format({ mantissa: 5, trimMantissa: true }) || 0
+        }),
+      isSort: true,
+      isWrap: true,
+      width: 15
+    },
+    {
+      metric: INVENTORY_TYPES.LAST_SEEN,
+      cell: ({ [INVENTORY_TYPES.LAST_SEEN]: lastSeen } = {}) => (lastSeen && <DateFormat date={lastSeen} />) || '',
+      isSort: true,
+      isWrap: true,
+      width: 15
+    }
+  ],
+  initialInventorySettings: {
+    actions: [
+      {
+        id: RHSM_API_QUERY_SET_TYPES.DISPLAY_NAME
+      }
+    ],
+    guestContent: ({
+      [INVENTORY_TYPES.NUMBER_OF_GUESTS]: numberOfGuests = {},
+      [INVENTORY_TYPES.INSTANCE_ID]: id
+    } = {}) => (numberOfGuests > 0 && id && { id, numberOfGuests }) || undefined
+  },
+  initialSubscriptionsInventoryFilters: [
+    {
+      metric: SUBSCRIPTIONS_INVENTORY_TYPES.PRODUCT_NAME,
+      isSort: true,
+      isWrap: true
+    },
+    {
+      metric: SUBSCRIPTIONS_INVENTORY_TYPES.BILLING_PROVIDER,
+      cell: ({ [SUBSCRIPTIONS_INVENTORY_TYPES.BILLING_PROVIDER]: provider } = {}) =>
+        translate(`curiosity-inventory.label`, {
+          context: [SUBSCRIPTIONS_INVENTORY_TYPES.BILLING_PROVIDER, provider || 'none']
+        }),
+      isSort: true,
+      isWrap: false,
+      width: 15
+    },
+    {
+      metric: SUBSCRIPTIONS_INVENTORY_TYPES.SERVICE_LEVEL,
+      isSort: true,
+      isWrap: true,
+      width: 15
+    },
+    {
+      metric: SUBSCRIPTIONS_INVENTORY_TYPES.QUANTITY,
+      isSort: true,
+      isWrap: true,
+      width: 10
+    },
+    {
+      metric: SUBSCRIPTIONS_INVENTORY_META_TYPES.SUBSCRIPTION_TYPE,
+      cell: (data, session, { [SUBSCRIPTIONS_INVENTORY_META_TYPES.SUBSCRIPTION_TYPE]: subscriptionType } = {}) =>
+        translate(`curiosity-inventory.label`, {
+          context: [SUBSCRIPTIONS_INVENTORY_META_TYPES.SUBSCRIPTION_TYPE, subscriptionType || EMPTY_CONTEXT]
+        }),
+      isSort: false,
+      isWrap: true,
+      width: 15
+    },
+    {
+      metric: SUBSCRIPTIONS_INVENTORY_TYPES.NEXT_EVENT_DATE,
+      cell: ({ [SUBSCRIPTIONS_INVENTORY_TYPES.NEXT_EVENT_DATE]: nextEventDate } = {}) =>
+        (nextEventDate && helpers.isDate(nextEventDate) && moment.utc(nextEventDate).format('YYYY-MM-DD')) || '',
+      isSort: true,
+      isWrap: true,
+      width: 15
+    }
+  ],
+  initialToolbarFilters: [
+    {
+      id: RHSM_API_QUERY_SET_TYPES.BILLING_PROVIDER
+    },
+    {
+      id: 'rangedMonthly',
+      isSecondary: true,
+      position: SelectPosition.right
+    }
+  ]
+};
+
+export { config as default, config, productGroup, productId };
