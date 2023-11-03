801d081a8761a6e2184b866012faa94438f25bd0
Vanessa Busch <vbusch@redhat.com>
2023-11-03 09:56:53 -0400

    refactor(dateHelpers): sw-260 remove moment.js (#1220)

--- src/common/__tests__/dateHelpers.test.js
+++ src/common/__tests__/dateHelpers.test.js
@@ -31,12 +31,39 @@ describe('DateHelpers', () => {
 
   it('should return a predictable range of time', () => {
     const currentDate = getCurrentDate();
-    const rangeDateTime = setRangedDateTime({ date: getCurrentDate(), subtract: 5, measurement: 'days' });
+
+    expect(dateHelpers.setRangedDateTime()).toMatchSnapshot('range of time for today');
+
+    const rangeDateTime = setRangedDateTime({ subtract: 5, measurement: 'days' });
 
     expect({
       currentDate,
       rangeDateTime
     }).toMatchSnapshot('range of time');
+
+    const expectedProvidedDate = new Date('2023-9-24');
+    const rangeDateTimeWithProvidedDate = setRangedDateTime({
+      date: expectedProvidedDate,
+      subtract: 0,
+      measurement: 'days'
+    });
+
+    expect(rangeDateTimeWithProvidedDate).toEqual({
+      endDate: new Date(expectedProvidedDate.setUTCHours(23, 59, 59, 999)),
+      startDate: new Date(expectedProvidedDate.setUTCHours(0, 0, 0, 0))
+    });
+
+    const nonSundayStartDate = new Date(Date.UTC(2023, 9, 24, 18, 6, 8, 3));
+    const expectedSundayStartDate = new Date(Date.UTC(2023, 6, 30, 0, 0, 0, 0));
+    const expectedSaturdayEndDate = new Date(Date.UTC(2023, 9, 22, 23, 59, 59, 999));
+
+    expect(
+      dateHelpers.setRangedDateTime({
+        date: nonSundayStartDate,
+        subtract: 12,
+        measurement: 'weeks'
+      })
+    ).toEqual({ startDate: expectedSundayStartDate, endDate: expectedSaturdayEndDate });
   });
 
   it('should return a predictable range based on granularity', () => {
@@ -52,5 +79,26 @@ describe('DateHelpers', () => {
     expect(getRangedMonthDateTime()).toMatchSnapshot('getRangedYearDateTime');
     expect(getRangedMonthDateTime('april')).toMatchSnapshot('get a specific month by name');
     expect(getRangedMonthDateTime(3)).toMatchSnapshot('get a specific month by number');
+  });
+
+  it('should change date to end of day', () => {
+    const startDate = new Date(Date.UTC(2020, 7, 15, 3, 6, 8, 3));
+    const expectedDate = new Date(Date.UTC(2020, 7, 15, 23, 59, 59, 999));
+
+    expect(dateHelpers.setEndOfDay(startDate).getTime()).toEqual(expectedDate.getTime());
+  });
+
+  it('should change date to start of day', () => {
+    const startDate = new Date(Date.UTC(2020, 7, 15, 3, 6, 8, 3));
+    const expectedDate = new Date(Date.UTC(2020, 7, 15, 0, 0, 0, 0));
+
+    expect(dateHelpers.setStartOfDay(startDate).getTime()).toEqual(expectedDate.getTime());
+  });
+
+  it('should change date to end of Month', () => {
+    const startDate = new Date(Date.UTC(2020, 7, 15, 3, 6, 8, 3));
+    const expectedDate = new Date(Date.UTC(2020, 7, 31, 23, 59, 59, 999));
+
+    expect(dateHelpers.setEndOfMonth(startDate).getTime()).toEqual(expectedDate.getTime());
   });
 });
