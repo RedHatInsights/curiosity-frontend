2a998887a24129937b67567f502e153be7bdb2f3
CD Cabrera <cabrera.code@gmail.com>
2024-03-27 14:17:50 -0400

    fix(tabs): sw-2371 avoid redundant tab loads (#1297)

--- src/components/tabs/__tests__/tabs.test.js
+++ src/components/tabs/__tests__/tabs.test.js
@@ -14,12 +14,38 @@ describe('Tabs Component', () => {
     expect(component).toMatchSnapshot('basic');
   });
 
-  it('should handle no tabs', () => {
+  it('should allow config activation of a tab', async () => {
+    const props = {
+      tabs: [
+        { title: 'lorem', content: 'ipsum' },
+        { active: true, title: 'dolor', content: 'sit' }
+      ]
+    };
+
+    const component = await shallowComponent(<Tabs {...props} />);
+    expect(component).toMatchSnapshot('config');
+  });
+
+  it('should allow specific activation of a tab', async () => {
+    const props = {
+      activeTab: 2,
+      tabs: [
+        { title: 'lorem', content: 'ipsum' },
+        { active: true, title: 'dolor', content: 'sit' },
+        { title: 'hello', content: 'world' }
+      ]
+    };
+
+    const component = await shallowComponent(<Tabs {...props} />);
+    expect(component).toMatchSnapshot('specific');
+  });
+
+  it('should handle no tabs', async () => {
     const props = {
       tabs: []
     };
 
-    const component = renderComponent(<Tabs {...props} />);
+    const component = await shallowComponent(<Tabs {...props} />);
     expect(component).toMatchSnapshot('no tabs');
   });
 });
