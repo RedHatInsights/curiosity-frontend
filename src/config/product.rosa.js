b895614c952dd443de27d618e7f4932227d0ac0c
CD Cabrera <cabrera.code@gmail.com>
2024-04-24 11:49:38 -0400

    fix(config): sw-2480 link rosa to openshift details (#1315)

--- src/config/product.rosa.js
+++ src/config/product.rosa.js
@@ -268,7 +268,7 @@ const config = {
               isInline
               component="a"
               variant="link"
-              href={`${helpers.UI_DEPLOY_PATH_LINK_PREFIX}/insights/inventory/${instanceId}/`}
+              href={`${helpers.UI_DEPLOY_PATH_LINK_PREFIX}/openshift/details/${instanceId}/`}
             >
               {updatedDisplayName}
             </Button>
