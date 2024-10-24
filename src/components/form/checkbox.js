c6039888323c845f2e2253b404c8d8464ec35791
CD Cabrera <ccabrera@redhat.com>
2024-10-24 15:08:33 -0400

    refactor(checkbox): sw-2707 remove deprecated react (#1458)

--- src/components/form/checkbox.js
+++ src/components/form/checkbox.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Checkbox as PfCheckbox } from '@patternfly/react-core/dist/js/components/Checkbox';
 import { createMockEvent } from './formHelpers';
 import { helpers } from '../../common';
@@ -16,35 +15,34 @@ import { helpers } from '../../common';
  *
  * @fires onCheckboxChange
  * @param {object} props
- * @param {string} props.ariaLabel
- * @param {React.ReactNode} props.children
- * @param {string} props.id
- * @param {*} props.isChecked
- * @param {boolean} props.isDisabled
- * @param {boolean} props.isReadOnly
- * @param {React.ReactNode} props.label
- * @param {string} props.name
- * @param {Function} props.onChange
- * @param {*} props.value
- * @returns {React.ReactNode}
+ * @param {string} [props.ariaLabel]
+ * @param {React.ReactNode} [props.children]
+ * @param {string} [props.id]
+ * @param {boolean} [props.isChecked=false]
+ * @param {boolean} [props.isDisabled=false]
+ * @param {boolean} [props.isReadOnly=false]
+ * @param {React.ReactNode} [props.label]
+ * @param {string} [props.name]
+ * @param {Function} [props.onChange=helpers.noop]
+ * @param {string} [props.value]
+ * @returns {JSX.Element}
  */
 const Checkbox = ({
   ariaLabel,
   children,
   id,
-  isChecked,
-  isDisabled,
-  isReadOnly,
-  label,
-  name,
-  onChange,
+  isChecked = false,
+  isDisabled = false,
+  isReadOnly = false,
+  label = '',
+  name = helpers.generateId(),
+  onChange = helpers.noop,
   value,
   ...props
 }) => {
   const [check, setCheck] = React.useState();
   const updatedChecked = check ?? isChecked ?? false;
-  const updatedName = name || helpers.generateId();
-  const updatedId = id || updatedName;
+  const updatedId = id || name;
 
   /**
    * onChange event, provide restructured event.
@@ -57,7 +55,7 @@ const Checkbox = ({
     const mockEvent = {
       ...createMockEvent(event),
       id: updatedId,
-      name: updatedName,
+      name,
       value,
       checked
     };
@@ -72,53 +70,15 @@ const Checkbox = ({
       checked={updatedChecked}
       id={updatedId}
       isChecked={updatedChecked}
-      isDisabled={isDisabled || false}
+      isDisabled={isDisabled}
       label={children || label}
-      name={updatedName}
+      name={name}
       onChange={(event, checked) => onCheckboxChange(checked, event)}
       value={value}
-      readOnly={isReadOnly || false}
+      readOnly={isReadOnly}
       {...props}
     />
   );
-};
-
-/**
- * Prop types.
- *
- * @type {{isReadOnly: boolean, onChange: Function, children: React.ReactNode, name: string, id: string,
- *     isDisabled: boolean, label: string, isChecked: boolean, value: *, ariaLabel: string}}
- */
-Checkbox.propTypes = {
-  ariaLabel: PropTypes.string,
-  children: PropTypes.node,
-  id: PropTypes.string,
-  isChecked: PropTypes.any,
-  isDisabled: PropTypes.bool,
-  isReadOnly: PropTypes.bool,
-  label: PropTypes.node,
-  name: PropTypes.string,
-  onChange: PropTypes.func,
-  value: PropTypes.any
-};
-
-/**
- * Default props.
- *
- * @type {{isReadOnly: boolean, onChange: Function, children: React.ReactNode, name: string, id: string,
- *     isDisabled: boolean, label: string, isChecked: boolean, value: *, ariaLabel: string}}
- */
-Checkbox.defaultProps = {
-  ariaLabel: null,
-  children: null,
-  id: null,
-  isChecked: false,
-  isDisabled: false,
-  isReadOnly: false,
-  label: '',
-  name: null,
-  onChange: helpers.noop,
-  value: undefined
 };
 
 export { Checkbox as default, Checkbox };
