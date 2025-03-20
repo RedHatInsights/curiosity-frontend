a9f8372a6a592de3c59884e0f472186b75842a0f
CD Cabrera <ccabrera@redhat.com>
2025-03-20 09:51:29 -0400

    fix(inventoryGuests): sw-3216 infinite loader (#1542)
    * inventoryGuests, apply key for loading consistency
    * inventoryGuestsContext, correct dispatch types, add row state

--- src/components/inventoryGuests/inventoryGuests.js
+++ src/components/inventoryGuests/inventoryGuests.js
@@ -43,7 +43,6 @@ const InventoryGuests = ({
     resultsColumnCountAndWidths = { count: 1, widths: [] },
     resultsOffset
   } = useAliasGetInventory(id);
-
   const onScroll = useAliasOnScroll({ id, numberOfGuests });
 
   // ToDo: Review having the height be a calc value. Remember to include the table header
@@ -51,40 +50,42 @@ const InventoryGuests = ({
   updatedHeight = (updatedHeight < 275 && updatedHeight) || 275;
 
   return (
-    <div className="fadein">
-      <div className="curiosity-table-scroll" style={{ height: `${updatedHeight}px` }}>
-        <div
-          className={`curiosity-table-scroll-list${(updatedHeight < 275 && '__no-scroll') || ''}`}
-          onScroll={onScroll}
-        >
-          {(error && <ErrorMessage message={message} title={t('curiosity-inventory.error_title')} />) ||
-            (pending && (
-              <div className="curiosity-table-scroll-loader__custom">
-                <Loader
-                  variant="table"
-                  tableProps={{
-                    borders: false,
-                    className: 'curiosity-guests-list',
-                    colCount: resultsColumnCountAndWidths.count,
-                    colWidth: resultsColumnCountAndWidths.widths,
-                    rowCount: (resultsOffset === 0 && numberOfGuests < defaultPerPage && numberOfGuests) || 1,
-                    variant: TableVariant.compact,
-                    isHeader: false
-                  }}
-                />
-              </div>
-            ))}
-          {(dataSetRows?.length && (
-            <Table
-              isBorders={false}
-              isHeader
-              className="curiosity-guests-list"
-              columnHeaders={dataSetColumnHeaders}
-              rows={dataSetRows}
-            />
-          )) ||
-            null}
-        </div>
+    <div className="curiosity-table-scroll" style={{ height: `${updatedHeight}px` }}>
+      <div
+        className={`curiosity-table-scroll-list${(pending && '__no-scroll') || (updatedHeight < 275 && '__no-scroll') || ''}`}
+        onScroll={onScroll}
+      >
+        {(error && <ErrorMessage message={message} title={t('curiosity-inventory.error_title')} />) ||
+          (pending && (
+            <div className="curiosity-table-scroll-loader__custom">
+              <Loader
+                variant="table"
+                tableProps={{
+                  borders: false,
+                  className: 'curiosity-guests-list',
+                  colCount: resultsColumnCountAndWidths.count,
+                  colWidth: resultsColumnCountAndWidths.widths,
+                  rowCount:
+                    (resultsOffset === 0 && numberOfGuests < defaultPerPage && numberOfGuests) ||
+                    (resultsOffset === 0 && numberOfGuests) ||
+                    1,
+                  variant: TableVariant.compact,
+                  isHeader: false
+                }}
+              />
+            </div>
+          ))}
+        {(dataSetRows?.length && (
+          <Table
+            key={id}
+            isBorders={false}
+            isHeader
+            className="curiosity-guests-list fadein__fast"
+            columnHeaders={dataSetColumnHeaders}
+            rows={dataSetRows}
+          />
+        )) ||
+          null}
       </div>
     </div>
   );
