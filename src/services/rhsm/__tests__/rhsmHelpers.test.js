5c3ed1328cabc3ef269ffac61f76179f7f1f10f9
CD Cabrera <cabrera.code@gmail.com>
2025-05-01 10:18:08 -0400

    test: expand metric tests for unique providers

--- src/services/rhsm/__tests__/rhsmHelpers.test.js
+++ src/services/rhsm/__tests__/rhsmHelpers.test.js
@@ -11,7 +11,7 @@ describe('RhsmHelpers', () => {
       data: undefined
     },
     {
-      description: 'multiple providers, multiple accounts',
+      description: 'multiple unique providers, multiple accounts',
       data: {
         loremApi: [
           { id: '123', provider: 'ipsum', type: 'loremApi' },
@@ -35,6 +35,18 @@ describe('RhsmHelpers', () => {
       }
     },
     {
+      description: 'single unique provider, multiple accounts',
+      data: {
+        loremApi: [
+          { id: '123', provider: 'lorem', type: 'loremApi' },
+          { id: '456', provider: 'lorem', type: 'loremApi' },
+          { id: '789', provider: 'dolor', type: 'loremApi' },
+          { id: '012', provider: 'lorem', type: 'loremApi' }
+        ],
+        dolorApi: [{ id: '789', provider: 'dolor', type: 'dolorApi' }]
+      }
+    },
+    {
       description: 'single provider, single account',
       data: {
         loremApi: [
@@ -42,6 +54,16 @@ describe('RhsmHelpers', () => {
           { id: '789', provider: 'lorem', type: 'loremApi' }
         ],
         dolorApi: [{ id: '789', provider: 'lorem', type: 'dolorApi' }]
+      }
+    },
+    {
+      description: 'single unique provider, single account',
+      data: {
+        loremApi: [
+          { id: '123', provider: 'lorem', type: 'loremApi' },
+          { id: '789', provider: 'dolor', type: 'loremApi' }
+        ],
+        dolorApi: [{ id: '789', provider: 'dolor', type: 'dolorApi' }]
       }
     }
   ])('billingMetrics should return parsed API responses in a consumable metric format, $description', ({ data }) => {
