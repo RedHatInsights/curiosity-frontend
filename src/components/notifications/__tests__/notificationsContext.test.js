c9056eb50b6dd08d7f16fcb768ccb3fc44154d38
CD Cabrera <cabrera.code@gmail.com>
2025-06-30 14:09:49 -0400

    test: review updates

---
+++ src/components/notifications/__tests__/notificationsContext.test.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { context, useNotifications } from '../notificationsContext';
+
+describe('NotificationsContext', () => {
+  it('should return specific properties', () => {
+    expect(context).toMatchSnapshot('specific properties');
+  });
+
+  it('should apply a hook wrapper for notifications', async () => {
+    const { result } = await renderHook(() => useNotifications());
+    expect(result).toMatchSnapshot('notifications hook');
+  });
+
+  it.each([
+    {
+      description: 'swatchId',
+      notification: {
+        swatchId: 'loremIpsum',
+        title: 'Lorem ipsum'
+      }
+    },
+    {
+      description: 'swatchid',
+      notification: {
+        swatchid: 'dolorSitAmet',
+        title: 'Lorem ipsum'
+      }
+    },
+    {
+      description: 'id',
+      notification: {
+        id: 'ametConsectetur',
+        title: 'Lorem ipsum'
+      }
+    }
+  ])('should attempt addNotification with a custom ID: $description', async ({ notification }) => {
+    const mockAddNotification = jest.fn();
+    const mockRemoveNotification = jest.fn();
+    const mockGetNotifications = jest.fn(() => []);
+    const mockClearNotifications = jest.fn();
+
+    const mockUseContext = jest.spyOn(React, 'useContext').mockReturnValue({
+      addNotification: mockAddNotification,
+      removeNotification: mockRemoveNotification,
+      getNotifications: mockGetNotifications,
+      clearNotifications: mockClearNotifications
+    });
+
+    const { result } = await renderHook(() => useNotifications());
+    result.addNotification({
+      ...notification
+    });
+
+    expect(mockAddNotification.mock.calls).toMatchSnapshot();
+    mockUseContext.mockClear();
+  });
+
+  it.each([
+    {
+      description: 'swatchId',
+      removeId: 'loremIpsum',
+      notifications: [
+        {
+          id: 'generated-id-1',
+          swatchid: 'loremIpsum',
+          title: 'Lorem ipsum'
+        }
+      ]
+    },
+    {
+      description: 'id',
+      removeId: 'generated-id-2',
+      notifications: [
+        {
+          id: 'generated-id-2',
+          swatchid: 'dolorSitAmet',
+          title: 'Lorem ipsum'
+        }
+      ]
+    },
+    {
+      description: 'incorrect id',
+      removeId: 'incorrectId',
+      notifications: [
+        {
+          id: 'generated-id-3',
+          swatchid: 'ametConsectetur',
+          title: 'Lorem ipsum'
+        }
+      ]
+    }
+  ])('should attempt removeNotification with a custom ID: $description', async ({ notifications, removeId }) => {
+    const mockAddNotification = jest.fn();
+    const mockRemoveNotification = jest.fn();
+    const mockGetNotifications = jest.fn(() => [...notifications]);
+    const mockClearNotifications = jest.fn();
+
+    const mockUseContext = jest.spyOn(React, 'useContext').mockReturnValue({
+      addNotification: mockAddNotification,
+      removeNotification: mockRemoveNotification,
+      getNotifications: mockGetNotifications,
+      clearNotifications: mockClearNotifications
+    });
+
+    const { result } = await renderHook(() => useNotifications());
+    result.removeNotification(removeId);
+
+    expect(mockRemoveNotification.mock.calls).toMatchSnapshot();
+    mockUseContext.mockClear();
+  });
+});
--- src/components/notifications/notifications.js
+++ src/components/notifications/notifications.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { AlertVariant } from '@patternfly/react-core';
 import { NotificationsProvider } from '@redhat-cloud-services/frontend-components-notifications';
+import { context as NotificationsContext } from './notificationsContext';
 import { helpers } from '../../common';
 
 /**
@@ -11,18 +13,27 @@ import { helpers } from '../../common';
  */
 
 /**
+ * Toast notification, or Alert, variants.
+ *
+ * @type {{success: AlertVariant.success, danger: AlertVariant.danger, warning: AlertVariant.warning,
+ *     info: AlertVariant.info, custom: AlertVariant.custom}}
+ */
+const NotificationVariant = { ...AlertVariant };
+
+/**
  * Expose consoledot toast notifications.
  *
- * @param {object} props - The prop object for the component.
- * @param {React.ReactNode} props.children - The child components or elements to be rendered.
- * @returns {React.ReactNode} The rendered output, either wrapped with NotificationsProvider or plain children.
+ * @param {object} props
+ * @param {React.ReactNode} props.children
+ * @param {boolean} [props.isDisabled=helpers.UI_DISABLED_NOTIFICATIONS] - Disable the notification provider.
+ * @returns {React.ReactNode} Rendered output, either wrapped with NotificationsProvider or plain children.
  */
-const Notifications = ({ children }) => {
-  if (!helpers.UI_DISABLED_NOTIFICATIONS) {
-    return <NotificationsProvider>{children}</NotificationsProvider>;
+const Notifications = ({ children, isDisabled = helpers.UI_DISABLED_NOTIFICATIONS }) => {
+  if (isDisabled) {
+    return children;
   }
 
-  return children;
+  return <NotificationsProvider>{children}</NotificationsProvider>;
 };
 
-export { Notifications as default, Notifications };
+export { Notifications as default, Notifications, NotificationsContext, NotificationVariant };
