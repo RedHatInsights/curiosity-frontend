698a0b4ee0ef7b2724f7edaf2890221bf51a9d44
CD Cabrera <cabrera.code@gmail.com>
2026-03-18 11:25:55 -0400

    feat: sw-175 activate notification banner

--- src/config/banners.js
+++ src/config/banners.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { AlertVariant } from '@patternfly/react-core';
+import { translate } from '../components/i18n/i18n';
+
 /**
  * Global banner configurations.
  *
@@ -45,6 +49,27 @@
  *     productIds: string[], condition: Function, actions: Array<{title: string, href: string,
  *     onClick: Function, isExternal: boolean}>}>}
  */
-const banners = [];
+const banners = [
+  {
+    id: 'global-notifications-banner',
+    title: () => translate('curiosity-banner.notifications_title'),
+    message: () => translate('curiosity-banner.notifications_description', {}, [<strong />]),
+    variant: AlertVariant.custom,
+    dataTest: 'bannerNotifications',
+    condition: () => new Date().getTime() <= new Date('May 2, 2026').getTime(),
+    actions: [
+      {
+        title: () => translate('curiosity-banner.notifications_action_setup'),
+        href: 'https://console.redhat.com/settings/notifications/user-preferences?bundle=subscription-services&app=subscriptions',
+        isExternal: false
+      },
+      {
+        title: () => translate('curiosity-banner.action_learn_more'),
+        href: 'https://docs.redhat.com/en/documentation/subscription_central/1-latest/html-single/getting_started_with_the_subscriptions_service/index#proc-notifications-subscription-usage',
+        isExternal: true
+      }
+    ]
+  }
+];
 
 export { banners as default, banners };
