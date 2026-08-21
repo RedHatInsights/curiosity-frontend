dabf513dc328a89d4e589773a4994fea6c4cd89a
Diego Maranhao <diegomaranhao@outlook.com>
2026-08-20 22:55:40 -0400

    fix(app): resolve AppEntry TDZ from Kessel circular init

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux';
-import App from './app';
 import './styles/index.scss';
 
 /**
@@ -9,19 +8,25 @@ import './styles/index.scss';
  * @module AppEntry
  */
 
-/**
- * ToDo: review consolidating between this and app.js
- */
+
+const App = React.lazy(() => import('./app'));
+
 /**
  * Application entry.
  * - A platform required file, including how it's cased.
+ * Function declaration is hoisted so Chrome can resolve this export during circular init.
+ * App is lazy-loaded so Kessel/auth are not evaluated in the same module pass.
  *
  * @returns {JSX.Element}
  */
-const AppEntry = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+function AppEntry() {
+  return (
+    <Provider store={store}>
+      <Suspense fallback={null}>
+        <App />
+      </Suspense>
+    </Provider>
+  );
+}
 
 export { AppEntry as default, AppEntry };
